f0271eff3f7c91ecd3c1ae24977345e155966f3a
ATrrs <32263149+ATrrs@users.noreply.github.com>
2017-11-22 12:22:55 +0100

    Revert "General pull"

--- Topology Optimization/TopOpt_Problem_Compliance_st_Volume.m
+++ Topology Optimization/TopOpt_Problem_Compliance_st_Volume.m
@@ -3,10 +3,14 @@ classdef TopOpt_Problem_Compliance_st_Volume < TopOpt_Problem
     end
     methods
         function obj=TopOpt_Problem_Compliance_st_Volume(settings)
-            obj@TopOpt_Problem(settings);
+            obj.TOL=settings.TOL;
             obj.cost=ShFunc_Compliance;
-            obj.constraint=ShFunc_Volume(settings.volume);
-             
+            obj.constraint=ShFunc_Volume(settings.Vfrac);
+            obj.settings=settings;
+            obj.filter=Filter_SLERP;
+            %wip
+            obj.physicalProblem=Physical_Problem(settings.filename);
+            %wip
         end
     end
 end
