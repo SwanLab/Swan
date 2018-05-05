8da6b9aac9ae626e4e2706b577195388bde30293
marcnunezc <marcnc94@gmail.com>
2018-05-05 10:36:09 +0200

    Added several Benchmarks: Bridge, Cantilever Quad, Cantilever Tetrahedra, Gripping

--- FEM/Material/Material_Elastic_ISO_3D.m
+++ FEM/Material/Material_Elastic_ISO_3D.m
@@ -15,19 +15,22 @@ classdef Material_Elastic_ISO_3D < Material_Elastic_ISO
         function obj = computeC(obj)
             C = zeros(6,6,obj.nelem);
             
-            a = obj.kappa+2*obj.mu;
-            C(1,1,:) = a;
-            C(1,2,:) = obj.kappa;
-            C(2,1,:) = obj.kappa;
-            C(1,3,:) = obj.kappa;
-            C(3,1,:) = obj.kappa;
-            C(3,2,:) = obj.kappa;
-            C(2,3,:) = obj.kappa;
-            C(2,2,:) = a;
-            C(3,3,:) = a;
-            C(4,4,:) = obj.mu;
-            C(5,5,:) = obj.mu;
-            C(6,6,:) = obj.mu;
+            epoiss = (obj.kappa - obj.mu)./(obj.kappa + obj.mu);
+            eyoung = 4*obj.kappa.*obj.mu./(obj.kappa + obj.mu);
+            
+            a = sparse(eyoung)/sparse(((ones(size(epoiss,1),1)+epoiss).*(ones(size(epoiss,1),1)-2*epoiss)));
+            C(1,1,:) = a*(1-epoiss);
+            C(1,2,:) = a*epoiss;
+            C(2,1,:) = a*epoiss;
+            C(1,3,:) = a*epoiss;
+            C(3,1,:) = a*epoiss;
+            C(3,2,:) = a*epoiss;
+            C(2,3,:) = a*epoiss;
+            C(2,2,:) = a*(1-epoiss);
+            C(3,3,:) = a*(1-epoiss);
+            C(4,4,:) = a*(1-2*epoiss)/2;
+            C(5,5,:) = a*(1-2*epoiss)/2;
+            C(6,6,:) = a*(1-2*epoiss)/2;
             
             obj.C = C;
         end
