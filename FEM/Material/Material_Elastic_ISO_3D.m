ee0e9a9565a2a9212a7f432584b11ecaadb22510
marcnunezc <marcnc94@gmail.com>
2018-05-24 19:56:25 +0200

    Modifiied 3D Cmat computation, which was slowing the code. Added alternative to compute Kmatrix.

--- FEM/Material/Material_Elastic_ISO_3D.m
+++ FEM/Material/Material_Elastic_ISO_3D.m
@@ -18,19 +18,19 @@ classdef Material_Elastic_ISO_3D < Material_Elastic_ISO
             epoiss = (obj.kappa - obj.mu)./(obj.kappa + obj.mu);
             eyoung = 4*obj.kappa.*obj.mu./(obj.kappa + obj.mu);
             
-            a = sparse(eyoung)/sparse(((ones(size(epoiss,1),1)+epoiss).*(ones(size(epoiss,1),1)-2*epoiss)));
-            C(1,1,:) = a*(1-epoiss);
-            C(1,2,:) = a*epoiss;
-            C(2,1,:) = a*epoiss;
-            C(1,3,:) = a*epoiss;
-            C(3,1,:) = a*epoiss;
-            C(3,2,:) = a*epoiss;
-            C(2,3,:) = a*epoiss;
-            C(2,2,:) = a*(1-epoiss);
-            C(3,3,:) = a*(1-epoiss);
-            C(4,4,:) = a*(1-2*epoiss)/2;
-            C(5,5,:) = a*(1-2*epoiss)/2;
-            C(6,6,:) = a*(1-2*epoiss)/2;
+            a = eyoung./((1+epoiss).*(1-2*epoiss));
+            C(1,1,:) = a.*(1-epoiss);
+            C(1,2,:) = a.*epoiss;
+            C(2,1,:) = a.*epoiss;
+            C(1,3,:) = a.*epoiss;
+            C(3,1,:) = a.*epoiss;
+            C(3,2,:) = a.*epoiss;
+            C(2,3,:) = a.*epoiss;
+            C(2,2,:) = a.*(1-epoiss);
+            C(3,3,:) = a.*(1-epoiss);
+            C(4,4,:) = a.*(1-2*epoiss)/2;
+            C(5,5,:) = a.*(1-2*epoiss)/2;
+            C(6,6,:) = a.*(1-2*epoiss)/2;
             
             obj.C = C;
         end
