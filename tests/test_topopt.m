15371e2204696ab4f4cb88dee3a58d2ebf4ab4b0
Trujillo94 <trujillo.oriol@gmail.com>
2018-08-30 17:28:33 +0200

    Added Shape Opt. 3D test: test_bridge2.

--- tests/test_topopt.m
+++ tests/test_topopt.m
@@ -6,7 +6,7 @@ fprintf('Running TopOpt tests...\n')
 
 %% Test Declaration -------------------------------------------------------
 
-tests_topopt = {'test_cantilever','test_cantilever2','test_cantilever3','test_gripping','test_bridge','test_micro','test_micro2'};
+tests_topopt = {'test_cantilever','test_cantilever2','test_cantilever3','test_gripping','test_bridge','test_bridge2','test_micro','test_micro2'};
 
 %% Run Top Opt Tests ------------------------------------------------------
 for i = 1:length(tests_topopt)
@@ -20,8 +20,13 @@ for i = 1:length(tests_topopt)
     
     % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DELETE !!!!!!!!!!!!!!!!!!!!!!!!!!!!! %
     if contains(file_name,'bridge')
-        dim = [2 1]; div = [120 60];
-        [A1,b1,A0,b0] = conversionTensors(settings.filename,dim,div);
+        if contains(file_name,'2')
+            dim = [60 20 20]; div = [24 8 8];
+            [A1,b1,A0,b0] = conversionTensors3D(settings.filename,dim,div);
+        else
+            dim = [2 1]; div = [120 60];
+            [A1,b1,A0,b0] = conversionTensors(settings.filename,dim,div);
+        end
         save(fullfile(pwd,'Allaire_ShapeOpt','conversion'),'A0','A1','b0','b1','dim','div');
     end
     % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! %
