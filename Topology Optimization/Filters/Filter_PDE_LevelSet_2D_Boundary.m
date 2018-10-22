04c6f91919574cfd6df0669c4369228558661b76
Trujillo94 <trujillo.oriol@gmail.com>
2018-10-22 11:13:16 +0200

    Fixed computeFacets for Mesh_Unfitted_2D_Boundary for the 4-cut-points case.

--- Topology Optimization/Filters/Filter_PDE_LevelSet_2D_Boundary.m
+++ Topology Optimization/Filters/Filter_PDE_LevelSet_2D_Boundary.m
@@ -1,9 +1,5 @@
 classdef Filter_PDE_LevelSet_2D_Boundary < Filter_PDE_LevelSet & Filter_LevelSet_2D_Boundary
-    methods
-        function obj = Filter_PDE_LevelSet_2D_Boundary(problemID,scale)
-            obj@Filter_PDE_LevelSet(problemID,scale);
-        end
-        
+    methods        
          function preProcess(obj)
             preProcess@Filter_PDE(obj)
             preProcess@Filter_LevelSet_2D_Boundary(obj)
