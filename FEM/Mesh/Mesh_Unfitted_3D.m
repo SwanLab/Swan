915d5845e5dd92b710f100d3b58a47facfc0be1c
marcnunezc <marcnc94@gmail.com>
2018-11-11 20:21:52 +0100

    created new objects to separate the unfitted mesh algorithm in delaunay and marching cubes, pending to add hexahedra case

--- FEM/Mesh/Mesh_Unfitted_3D.m
+++ FEM/Mesh/Mesh_Unfitted_3D.m
@@ -5,24 +5,7 @@ classdef Mesh_Unfitted_3D < Mesh_Unfitted
             obj.max_subcells = 20;
             obj.nnodes_subcell = 4;
         end
-        
-        function [P,active_nodes]=findCutPoints_Iso(obj)
-            pos_nodes = obj.fitted_geom_interpolation.pos_nodes;
-            
-            iteration_1 = obj.fitted_geom_interpolation.iteration(1,:);
-            iteration_2 = obj.fitted_geom_interpolation.iteration(2,:);
-            
-            gamma_1 = permute(obj.x_fitted(obj.fitted_mesh.connec(obj.cut_cells,iteration_1)),[2 3 1]);
-            gamma_2 = permute(obj.x_fitted(obj.fitted_mesh.connec(obj.cut_cells,iteration_2)),[2 3 1]);
-            
-            P1 = repmat(pos_nodes(iteration_1,:),[1 1 size(obj.cut_cells)]);
-            % !! There's an specific Matlab function to do this patch!!
-            P2 = repmat(pos_nodes(iteration_2,:),[1 1 size(obj.cut_cells)]);
-            P = P1+gamma_1.*(P2-P1)./(gamma_1-gamma_2);
-            
-            active_nodes = sign(gamma_1.*gamma_2)<=0;
-        end
-        
+
         function [P,active_nodes]=findCutPoints_Global(obj)
             iteration_1=obj.fitted_geom_interpolation.iteration(1,:);
             iteration_2=obj.fitted_geom_interpolation.iteration(2,:);
