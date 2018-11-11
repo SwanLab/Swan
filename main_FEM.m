0b57e70c234e32c017621680b302350b74e22bb7
marcnunezc <marcnc94@gmail.com>
2018-11-11 21:26:54 +0100

    updated main_fem to match current FEM_Problem family

--- main_FEM.m
+++ main_FEM.m
@@ -5,58 +5,22 @@ addpath(genpath(fileparts(mfilename('fullpath'))))
 % 
 % %% Steps
 % % 1 - Run 'Main.m'
-% % 2 - Create object --> obj = Physical_Problem(filename);
+% % 2 - Create object --> obj = Elastic_Problem(filename);
 % % 3 - Preprocess    --> obj.preProcess;
 % % 4 - Compute       --> obj.computeVariables;
 % % 5 - Postprocess   --> obj.postProcess;
 % %% test
-% run('test_fem.m')
+run('test_fem.m')
 % % test
-% clear variables
+clear variables
 %% Main.m
-% triangle_linear = Physical_Problem('CantileverToy_Triangular');
-triangle_linear = Physical_Problem('Stokes8');
-%triangle_linear = Physical_Problem('CantileverToy_Nonlinear');
+
+physProblem = Elastic_Problem('Cantilever_quad_coarse');
+
 tic
-triangle_linear.preProcess;
-triangle_linear.computeVariables;
-triangle_linear.print;
+physProblem.preProcess;
+physProblem.computeVariables;
+physProblem.print;
 toc
 
-% post = Postprocess_PhysicalProblem;
-% gidPath = 'C:\Program Files\GiD\GiD 13.0.3\'; %write your GiD path
-% files_name = triangle_linear.problemID;
-% files_folder = fullfile(pwd,'Output');
-% iterations = 1:1;
-
-% output_video_name = fullfile(pwd,'StressVideo');
-% post.Print_make_video_stress(gidPath,files_name,files_folder,iterations,output_video_name)
-
-
-
-Micro_Square_Triangle = Physical_Problem_Micro('RVE_Square_Triangle');
-Micro_Square_Triangle.preProcess;
-% Micro_Square_Triangle.computeVariables;
-% Micro_Square_Triangle.postProcess;
-Micro_Square_Triangle.computeChomog;
-fprintf('Ok\n');
-
-
-
- 
-% triangle_quadratic = Physical_Problem('CantileverBeam_Triangle_Quadratic');
-% triangle_quadratic.preProcess;
-% triangle_quadratic.computeVariables;
-% triangle_quadratic.postProcess;
-% 
-% 
-% quadrilateral_bilinear = Physical_Problem('Cantileverbeam_Quadrilateral_Bilinear');
-% quadrilateral_bilinear.preProcess;
-% quadrilateral_bilinear.computeVariables;
-% quadrilateral_bilinear.postProcess;
-% 
-% quadrilateral_serendipity = Physical_Problem('Cantileverbeam_Quadrilateral_Serendipity');
-% quadrilateral_serendipity.preProcess
-% quadrilateral_serendipity.computeVariables;
-% quadrilateral_serendipity.postProcess;
 
