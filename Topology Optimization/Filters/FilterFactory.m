3179fa65bdf4e1e4180da6ab8ac701fe74c9f62c
marcnunezc <marcnc94@gmail.com>
2019-03-12 06:50:52 +0100

    Implementing cParamas to filter factory

--- Topology Optimization/Filters/FilterFactory.m
+++ Topology Optimization/Filters/FilterFactory.m
@@ -2,17 +2,17 @@ classdef FilterFactory < handle
     
     methods (Access = public, Static)
         
-        function obj = create(type,optimizer)
-            switch type
+        function obj = create(cParams)
+            switch cParams.filter
                 case 'P1'
-                    switch optimizer
+                    switch cParams.optimizer
                         case {'MMA','PROJECTED GRADIENT','IPOPT'}
                             obj = Filter_P1_Density();
                         case {'SLERP','HAMILTON-JACOBI','PROJECTED SLERP'}
                             obj = Filter_P1_LevelSet();
                     end
                 case 'PDE'
-                    switch optimizer
+                    switch cParams.optimizer
                         case {'MMA','PROJECTED GRADIENT','IPOPT'}
                             obj = Filter_PDE_Density();
                         case {'SLERP','HAMILTON-JACOBI','PROJECTED SLERP'}
