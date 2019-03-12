a37a0771220cd45287faab00d0b5819a7543b5de
marcnunezc <marcnc94@gmail.com>
2019-03-12 07:18:21 +0100

    Adding ElementalDensCreator settings

---
+++ ExploringSettings/SettingsElementalDensity.m
@@ -0,0 +1,8 @@
+classdef SettingsElementalDensity < DefaultSettings
+    properties
+        filterParams = SettingsFilterP0()
+        levelSetCreatorParams = SettingsLevelSetCreator()
+    end
+    methods
+    end
+end
