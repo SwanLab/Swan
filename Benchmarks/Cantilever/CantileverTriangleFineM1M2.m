0f6a3aa6b2423e2fa8fbb3ecad7ad65e51eebb1c
aferrer <aferrer@cimne.upc.edu>
2019-05-17 16:26:51 +0200

    * Remeshed BridgeArch

--- Benchmarks/Cantilever/CantileverTriangleFineM1M2.m
+++ Benchmarks/Cantilever/CantileverTriangleFineM1M2.m
@@ -1,5 +1,5 @@
-%filename='CantileverSquare';
-filename = 'BridgeArch';
+filename='CantileverSquare';
+%filename = 'BridgeArch';
 %filename = 'Cantilever_triangle_fine';
 ptype = 'MACRO';
 initial_case = 'full';
@@ -35,8 +35,7 @@ switch option
         nsteps = 50;
         homegenizedVariablesComputer = 'ByVademecum';
         vademecumFileName = 'SmoothRectangle';
-        maxiter = 250;    
-
+        maxiter = 250;          
     case 'A'
         Vfrac_initial = 0.3;        
         optimizer = 'DualNestedInPrimal';
@@ -47,8 +46,12 @@ switch option
         kfrac = 2;%1.05;
         nsteps = 1;
         homegenizedVariablesComputer = 'ByVademecum';
-        vademecumFileName = 'SmoothRectangle';
-        maxiter = 500;
+        vademecumFileName = 'Rectangle';%'SmoothRectangle';
+        maxiter = 500;  
+        filterType = 'P1';
+        epsilon_initial = 0.01;
+        epsilon_final = 0.01;
+        optimality_final = 1e-5;
     case 'B'
         Vfrac_initial = 0.3;                
         optimizer = 'IPOPT';
@@ -62,6 +65,19 @@ switch option
         TOL.nu_minus = 1/3;
         method = 'SIMPALL';
         materialType = 'ISOTROPIC'; 
+    case 'B2'
+        Vfrac_initial = 0.3;                
+        optimizer = 'MMA';
+        designVariable = 'Density';
+        nsteps = 1;
+        TOL.rho_plus = 1;
+        TOL.rho_minus = 0;
+        TOL.E_plus = 1;
+        TOL.E_minus = 1e-3;
+        TOL.nu_plus = 1/3;
+        TOL.nu_minus = 1/3;
+        method = 'SIMPALL';
+        materialType = 'ISOTROPIC';         
     case 'C'
         Vfrac_initial = 1;                        
         optimizer = 'IPOPT';
@@ -96,6 +112,36 @@ switch option
         homegenizedVariablesComputer = 'ByVademecum';
         vademecumFileName = 'SmoothRectangle';  
         maxiter = 1000;        
+    case 'E2'
+        Vfrac_initial = 1;                                
+        optimizer = 'AlternatingPrimalDual';
+        optimizerUnconstrained = 'PROJECTED GRADIENT';        
+        designVariable = 'Density';
+        nsteps = 1;
+        TOL.rho_plus = 1;
+        TOL.rho_minus = 0;
+        TOL.E_plus = 1;
+        TOL.E_minus = 1e-3;
+        TOL.nu_plus = 1/3;
+        TOL.nu_minus = 1/3;
+        method = 'SIMPALL';
+        materialType = 'ISOTROPIC'; 
+                maxiter = 1000;        
+
+            case 'E3'
+        optimizer = 'DualNestedInPrimal';
+        optimizerUnconstrained = 'SLERP';        
+        designVariable = 'LevelSet';
+        nsteps = 10;
+        TOL.rho_plus = 1;
+        TOL.rho_minus = 0;
+        TOL.E_plus = 1;
+        TOL.E_minus = 1e-3;
+        TOL.nu_plus = 1/3;
+        TOL.nu_minus = 1/3;
+        method = 'SIMPALL';
+        materialType = 'ISOTROPIC'; 
+        maxiter = 200;     
 end
 
 
