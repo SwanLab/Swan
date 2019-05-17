1000eda9ba491e03b66453ebb7722ee3ced4368c
aferrer <aferrer@cimne.upc.edu>
2019-05-17 17:44:45 +0200

    * refactored HomogVarComputerFromVad

--- FEM/Material/HomogenizedVariablesComputer/HomogenizedVarComputerFromVademecum.m
+++ FEM/Material/HomogenizedVariablesComputer/HomogenizedVarComputerFromVademecum.m
@@ -7,8 +7,10 @@ classdef HomogenizedVarComputerFromVademecum ...
     end
     
     properties (Access = private)
-       Rsymbolic
-       RsymbolicEps       
+       Reps      
+       Rot
+       Cref
+       dCref
     end
     
     methods (Access = public)
@@ -19,96 +21,14 @@ classdef HomogenizedVarComputerFromVademecum ...
             v.load();
             obj.Ctensor = v.Ctensor;
             obj.density = v.density;
-            obj.createSymbolicRotationMatrix();
+            obj.computeSymbolicRotationMatrix();
             obj.designVariable = cParams.designVariable;
         end
         
-        function R = computeRotationMatrix2(obj)
-           b = obj.designVariable.beta;            
-           b1 = -b(1,:);
-           b2 = b(2,:);
-           R(1,1,:) = (1 - b1)/2;
-           R(1,2,:) = (1 + b1)/2;
-           R(1,3,:) = -b2;
-           R(2,1,:) = (1 + b1)/2;
-           R(2,2,:) = (1 - b1)/2;
-           R(2,3,:) = b2;
-           R(3,1,:) = b2/2;
-           R(3,2,:) = -b2/2;
-           R(3,3,:) = -b1;           
-        end
-        
         function computeCtensor(obj,x)
-            
-            Rs = obj.computeRotatorMatrix(obj.Rsymbolic);
-            Rp = obj.computeRotationMatrix2();
-            
-            Re = obj.computeRotatorMatrix(obj.RsymbolicEps);
-            
-            R = Re;
-           % Rs = Rs2;
-
-            mx = x(:,1);
-            my = x(:,2);
-            [c,dc] = obj.Ctensor.compute([mx,my]);
-            dc = permute(dc,[1 2 4 3]);
-
-            
-            Cr  = zeros(size(c));
-            dCr = zeros(size(dc));
-            for i = 1:3
-                for m = 1:3
-                    Rmi  = squeeze(R(m,i,:));  
-                    Rim = squeeze(R(i,m,:));                                        
-                    for n = 1:3
-                        Cmn = squeeze(c(m,n,:));
-                        for j= 1:3
-                            Rnj  = squeeze(R(n,j,:));
-                            Rjn  = squeeze(R(j,n,:));
-                            
-                            Cij  = Rmi.*Cmn.*Rnj;
-                            %Cij  = Rim.*Cmn.*Rjn;
-                            
-                            Cr(i,j,:)  = squeeze(Cr(i,j,:)) + Cij;
-                            for ivar = 1:2
-                                dCmn = squeeze(dc(m,n,ivar,:));
-                                dCij = Rmi.*dCmn.*Rnj;
-                                %dCij = Rim.*dCmn.*Rjn;
-                                dCr(i,j,ivar,:) = squeeze(dCr(i,j,ivar,:)) + dCij;
-                            end
-                        end
-                    end
-                end
-            end
-            
-            obj.C = Cr;
-            obj.dC = dCr;
-        end
-        
-        function R = computeRotatorMatrix(obj,Rs)
-            dir = obj.designVariable.alpha;            
-            nx = squeeze(dir(1,:));
-            ny = squeeze(dir(2,:));
-            angle = squeeze(atan2(ny,nx));
-            R = zeros(3,3,length(angle));
-            for i = 1:3
-                for j = 1:3
-                    r = matlabFunction(Rs(i,j));
-                    R(i,j,:) = r(angle);
-                end
-            end
-        end
-        
-        function a = createSymbolicRotationMatrix(obj)
-            S = StressPlaneStressVoigtTensor();
-            alpha = sym('alpha','real');
-            v = Vector3D();
-            v.setValue([0 0 1]);
-            factory = RotatorFactory();
-            rotator = factory.create(S,alpha,v);
-            a = rotator.rotationMatrix();  
-            obj.Rsymbolic = a;
-            obj.RsymbolicEps = simplify(inv(a)');
+            obj.obtainReferenceConsistutiveTensor(x);            
+            obj.computeRotationMatrix();
+            obj.rotateConstitutiveTensor();
         end
         
         function computeDensity(obj,x)
@@ -117,6 +37,79 @@ classdef HomogenizedVarComputerFromVademecum ...
             [rho,drho] = obj.density.compute([mx,my]);
             obj.rho = rho;
             obj.drho = drho;
+        end        
+        
+    end
+    
+    methods (Access = private)
+                
+        function obtainReferenceConsistutiveTensor(obj,x)
+            mx = x(:,1);
+            my = x(:,2);
+            [c,dc] = obj.Ctensor.compute([mx,my]);
+            obj.Cref  = c;
+            obj.dCref = permute(dc,[1 2 4 3]);            
+        end        
+        
+        function computeRotationMatrix(obj)
+            Rsym = obj.Reps;
+            dir = obj.designVariable.alpha;            
+            nx = squeeze(dir(1,:));
+            ny = squeeze(dir(2,:));
+            angle = squeeze(atan2(ny,nx));
+            R = zeros(3,3,length(angle));
+            for i = 1:3
+                for j = 1:3
+                    rij = matlabFunction(Rsym(i,j));
+                    R(i,j,:) = rij(angle);
+                end
+            end
+            obj.Rot = R;
+        end        
+        
+        function rotateConstitutiveTensor(obj)
+            cr  = obj.Cref;    
+            dCr = obj.dCref;
+            c  = zeros(size(cr));
+            dc = zeros(size(dCr));
+            nstre = size(cr,1);
+            for i = 1:nstre
+                for m = 1:nstre
+                    Rmi  = obj.rotationComponent(m,i);  
+                    for n = 1:nstre
+                        Cmn  = squeeze(cr(m,n,:));
+                        dCmn = squeeze(dCr(m,n,:,:));
+                        for j = 1:nstre
+                            Rnj = obj.rotationComponent(n,j);
+                            Cij = Rmi.*Cmn.*Rnj;
+                            c(i,j,:)  = squeeze(c(i,j,:)) + Cij;
+                            for s = 1:2
+                                dCmns(:,1) = dCmn(s,:);
+                                dCij = Rmi.*dCmns.*Rnj;
+                                dc(i,j,s,:) = squeeze(dc(i,j,s,:)) + dCij;
+                            end
+                        end
+                    end
+                end
+            end      
+            obj.C = c;
+            obj.dC = dc;            
+        end
+        
+        function Rij = rotationComponent(obj,i,j)
+            R = obj.Rot;
+            Rij = squeeze(R(i,j,:));            
+        end        
+        
+        function a = computeSymbolicRotationMatrix(obj)
+            S = StressPlaneStressVoigtTensor();
+            alpha = sym('alpha','real');
+            v = Vector3D();
+            v.setValue([0 0 1]);
+            factory = RotatorFactory();
+            rotator = factory.create(S,alpha,v);
+            a = rotator.rotationMatrix();  
+            obj.Reps = simplify(inv(a)');
         end
         
     end
