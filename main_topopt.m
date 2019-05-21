dc0ee89c424b3272c6ec2498dac4e54bf762475b
Trujillo94 <trujillo.oriol@gmail.com>
2019-05-21 09:50:26 +0200

    Small changes. Need to switch branches.

--- main_topopt.m
+++ main_topopt.m
@@ -21,12 +21,12 @@ clear variables;
 % settings = Settings('Case_RefactoringSettingsMICRO_OLD_2');
 % settingsTopOpt = SettingsTopOptProblem('CaseBenchmark_JSON_A.json',settings);
 
-settings = Settings('Case_RefactoringSettings_OLD');
-settingsTopOpt = SettingsTopOptProblem('CaseBenchmark_JSON_B.json');
+% settings = Settings('Case_RefactoringSettings_OLD');
+% settingsTopOpt = SettingsTopOptProblem('CaseBenchmark_JSON_B.json');
 
-% settings = Settings('CantileverTetrahedra_Case_1_1_1');
-% SettingsTranslator().translate(settings);
-% settingsTopOpt = SettingsTopOptProblem('CantileverTetrahedra_Case_1_1_1.json');
+settings = Settings('CantileverTetrahedra_Case_1_1_1');
+SettingsTranslator().translate(settings);
+settingsTopOpt = SettingsTopOptProblem('CantileverTetrahedra_Case_1_1_1.json');
 
 topOptProblem = TopOpt_Problem(settingsTopOpt);
 topOptProblem.computeVariables;
