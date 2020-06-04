fe1a77745a611662213b45147eb95c3d53e90689
AlexFerrer <aferrer@cimne.upc.edu>
2020-06-04 16:27:01 +0200

    prepared for parallel

--- Topology Optimization/Homogenization/Sources/VadamecumCalculator/VademecumCalculator.m
+++ Topology Optimization/Homogenization/Sources/VadamecumCalculator/VademecumCalculator.m
@@ -1,86 +1,54 @@
 classdef VademecumCalculator < handle
     
-    properties (Access = private)
-        samplePoints
-        optimalExponent
-        iter
-        iMx
-        iMy   
-        cellVariables
-        qOptimal
-        phi
+    properties (Access = public)
+        cellVariables        
     end
     
     properties (Access = private)
-        nMx
-        nMy
-        nPhi
-        phiMin
-        phiMax
+        optimalExponent
+        qOptimal        
+    end
+    
+    properties (Access = private)            
+        rho
+        xi
+        phi 
+        nCase
     end
     
     methods (Access = public)
         
-        function obj = VademecumCalculator()
-            obj.init([])
-            obj.createSamplePoints();
-            obj.computeVademecumData();
+        function obj = VademecumCalculator(cParams)
+            obj.init(cParams)
         end
         
-        function computeVademecumData(obj)
-            for imx = 1:obj.nMx
-                for imy = 1:obj.nMy
-                    obj.storeIndeces(imx,imy);
-                    obj.computeGlobalIteration();
-                    obj.displayPercentatge();
-                    for iphi = 1:obj.nPhi                               
-                        obj.createOptimalExponentComputer(iphi);
-                        obj.computeOptimalExponent();
-                        obj.obtainCellVariables(iphi);
-                    end
-                end
-            end
+        function compute(obj)
+            obj.createOptimalExponentComputer(iphi);
+            obj.computeOptimalExponent();
+            obj.obtainCellVariables(iphi);
         end
-        
         
     end
     
     methods (Access = private)
         
         function init(obj,cParams)
-            obj.nMx    = 10;
-            obj.nMy    = 10;
-            obj.nPhi   = 10;
-            obj.phiMin = 0;
-            obj.phiMax = pi/4;
+            obj.xi  = cParams.xi;
+            obj.rho = cParams.rho;
+            obj.phi = cParams.phi;       
+            obj.nCase = cParams.nCase;
         end
         
-        function createSamplePoints(obj)
-            s.type = 'FromMxMy';
-            s.nMx  = obj.nMx;
-            s.nMy  = obj.nMy;
-            s.nPhi = obj.nPhi;
-            s.phiMin = obj.phiMin;
-            s.phiMax = obj.phiMax;
-            sample = SamplePointsCreatorForOptimalExponentComputer.create(s);
-            sample.compute();
-            obj.samplePoints = sample;
-        end
-        
-        function storeIndeces(obj,imx,imy)
-            obj.iMx = imx;
-            obj.iMy = imy;
-        end
-        
-        function computeGlobalIteration(obj)
-            imx = obj.iMx;
-            imy = obj.iMy;
-            obj.iter = (imy + obj.nMx*(imx -1));
-        end
-
-        function displayPercentatge(obj)
-            disp([num2str(obj.iter/(obj.nMx*obj.nMy)*100),'% done']);
-        end       
+        function createOptimalExponentComputer(obj)
+            s.fileName = ['OptimaSuperEllipseCase',obj.nCase];
+            s.rho   = obj.rho;
+            s.txi   = obj.xi;
+            s.phi   = obj.phi;
+            s.pNorm = 'max';
+            s.hMesh = [];            
+            optimalExp = OneOptimalExponentComputerAndFunctionVariation(s);           
+            obj.optimalExponent = optimalExp;            
+        end        
         
         function q = computeOptimalExponent(obj)
              obj.optimalExponent.computeOptimalExponent();  
@@ -88,31 +56,17 @@ classdef VademecumCalculator < handle
              fMin = obj.optimalExponent.fOptIter();            
              [~,ind] = min(fMin);
              q = qMin(ind);
-             %q = 2;
-             obj.qOptimal(obj.iMx,obj.iMy) = q;
+             obj.qOptimal = q;
         end
         
-        function obtainCellVariables(obj,iphi)
-            imx = obj.iMx;
-            imy = obj.iMy;
-            q = obj.qOptimal(imx,imy);
+        function obtainCellVariables(obj)
+            q = obj.qOptimal;
             cVariables = obj.optimalExponent.obtainCellVariables(q);  
-            cVariables.rho = obj.samplePoints.rhoV(obj.iter);
-            cVariables.xi = obj.samplePoints.txiV(obj.iter);
-            cVariables.phi = obj.samplePoints.phiV(iphi);
+            cVariables.rho = obj.rho;
+            cVariables.xi = obj.xi;
+            cVariables.phi = obj.phi;
             cVariables.q = q;
-            obj.cellVariables{imx,imy} = cVariables;
-        end
-        
-        function createOptimalExponentComputer(obj,iphi)
-            s.fileName = ['OptimaSuperEllipseIter',num2str(obj.iter),'Phi',num2str(iphi)];
-            s.rho   = obj.samplePoints.rhoV(obj.iter);
-            s.txi   = obj.samplePoints.txiV(obj.iter);
-            s.phi   = obj.samplePoints.phiV(iphi);
-            s.pNorm = 'max';
-            s.hMesh = [];            
-            optimalExp = OneOptimalExponentComputerAndFunctionVariation(s);           
-            obj.optimalExponent = optimalExp;            
+            obj.cellVariables = cVariables;
         end
         
     end
---
+++ Topology Optimization/Homogenization/Sources/VadamecumCalculator/WrappingVademecumData.m
@@ -0,0 +1,18 @@
+function WrappingVademecumData
+
+for i = 1:20
+   RunningVademecumInParalel(i); 
+end
+
+cellVar = cell(20,20,10);
+for iMx = 1:20
+    fileName = 'OptimalSuperEllipseDifferentStressSign';
+    d = load([fileName,num2str(iMx),'.mat']);
+    for iMy = 1:20
+        for iPhi = 1:10
+            cellVar{iMx,iMy,iPhi} = d.c{iMx,iMy,iPhi};
+        end
+    end    
+end
+
+end
