81ca6fed72b7bded298ad385b8e8cb6024e8cc5b
tcreusc <toncc3d@gmail.com>
2021-10-12 20:20:47 +0200

    més plottingtests

--- tests/Source/PlottingTests/ZZPlottingTests.m
+++ tests/Source/PlottingTests/ZZPlottingTests.m
@@ -11,13 +11,16 @@ classdef ZZPlottingTests < handle & matlab.unittest.TestCase
         boletTests = {'testPlotLargeCylinderTethaedra'}
     end
 
-    methods (Test, TestTags = {'PlottingTests', 'Passed', 'Classic', 'Displacement'})
+    methods (Test, TestTags = {'PlottingTests', 'Toy', 'Nou'})
 
-        function testDisplacement(testCase, coordTests)
+        function testTriangleToy(testCase, coordTests)
             cd ../../../
             s.testName              = coordTests;
             s.meshType              = 'BOUNDARY';
             s.meshIncludeBoxContour = false;
+            obj.coord  = [0 0;1 0;1 1;0 1;2 0;2 1;0 2;1 2;2 2; 0.5 0.5; 1.5 0.5; 0.5 1.5; 1.5 1.5];
+            obj.connec = [1 2 10; 2 3 10; 10 3 4; 10 4 1; 2 11 3; 2 5 11; 5 6 11; 11 6 3; 3 8 12; 4 3 12; 12 8 7; 12 7 4; 3 6 13; 6 9 13; 13 9 8; 3 13 8];
+            obj.levelSet = [-0.05 0.2 -0.5 0.1 0.1 -1 1 -0.2 -0.5 -0.05 -0.05 0.05 -0.5]';
             test = ZZTestPlotting(s);
             err = test.computeError();
             tol = 1e-6;
@@ -26,4 +29,20 @@ classdef ZZPlottingTests < handle & matlab.unittest.TestCase
         end
 
     end
+
+%     methods (Test, TestTags = {'PlottingTests', 'Passed', 'Classic', 'Displacement'})
+% 
+%         function testDisplacement(testCase, coordTests)
+%             cd ../../../
+%             s.testName              = coordTests;
+%             s.meshType              = 'BOUNDARY';
+%             s.meshIncludeBoxContour = false;
+%             test = ZZTestPlotting(s);
+%             err = test.computeError();
+%             tol = 1e-6;
+%             testCase.verifyLessThanOrEqual(err, tol)
+%             cd ./tests/Source/PlottingTests/
+%         end
+% 
+%     end
 end
