d7188226afe51df4f0c797b79ebfb50c92197819
tcreusc <toncc3d@gmail.com>
2022-03-12 00:11:30 +0100

    updating test .mat results

--- tests/Source/TopOptTests/TopOptTestsSuite.m
+++ tests/Source/TopOptTests/TopOptTestsSuite.m
@@ -5,7 +5,7 @@ classdef TopOptTestsSuite < handle
         function obj = TopOptTestsSuite()
             warning('off', 'MATLAB:structOnObject')
             % testFastDisplacement, testMacro
-            results = runtests("TopOptTests","ProcedureName","testMicro", 'Debug', true);
+            results = runtests("TopOptTests","ProcedureName","testMacro", 'Debug', true);
             table(results)
             warning('on', 'MATLAB:structOnObject')
         end
