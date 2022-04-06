6b8edacb621e92bce75718dc247fb1dcbecdb878
tcreusc <toncc3d@gmail.com>
2022-04-06 15:02:02 +0200

    organizing files

--- FEM/FEM.m
+++ FEM/FEM.m
@@ -1,24 +1,7 @@
 classdef FEM < handle
     
-    properties (GetAccess = public, SetAccess = public)
-        problemData
-        geometry
-        mesh
-        dof
-        element
-        Fext
-        variables
-    end
-    
     properties (Access = protected)
-        solver
-        iter = 0;
         inputReader
-        fileName
-    end
-
-    properties (Access = private)
-        femData
     end
     
     methods (Access = public)
@@ -55,79 +38,13 @@ classdef FEM < handle
     
     methods (Access = public)
         
-        function sol = solve_steady_nonlinear_problem(obj,free_dof,tol)
-            total_free_dof = sum(free_dof);
-            dr = obj.element.computedr;
-            x0 = zeros(total_free_dof,1);
-            
-            r = obj.element.computeResidual(x0,dr);
-            x = x0;
-            while dot(r,r) > tol
-                inc_x = obj.solver.solve(dr,-r);
-                x = x0 + inc_x;
-                % Compute r
-                r = obj.element.computeResidual(x,dr);
-                x0 = x;
-            end
-            sol = x;
-        end
-        
-        function sol = solve_transient_nonlinear_problem(obj,free_dof,tol,dt,final_time)
-            total_free_dof = sum(free_dof);
-            x_n(:,1) = zeros(total_free_dof,1);
-            x0 = zeros(total_free_dof,1);
-            
-            dr = obj.element.computedr(dt);
-            
-            for istep = 2: final_time/dt
-                u_previous_step = x_n(1:free_dof(1),istep-1);
-                
-                r = obj.element.computeResidual(x0,dr,u_previous_step);
-                while dot(r,r) > tol
-                    inc_x = obj.solver.solve(dr,-r);
-                    x = x0 + inc_x;
-                    % Compute r
-                    r = obj.element.computeResidual(x,dr,u_previous_step);
-                    x0 = x;
-                end
-                x_n(:,istep) = x;
-            end
-            sol = x_n;
-        end
-        
-        function setDof(obj,dof)
-            obj.dof = dof;
-        end
-        
-        function setMatProps(obj,s)
-           obj.element.material.compute(s);
-        end
-        
-        function setC(obj,C)
-            obj.element.material.C = C;
-        end
-        
         function print(obj,fileName)
             dI = obj.createPostProcessDataBase(fileName);
             postprocess = Postprocess('Elasticity',dI);
-            q = obj.element.quadrature;
+            q = obj.getQuadrature();
             d.fields = obj.variables;
             d.quad = q;
             postprocess.print(obj.iter,d);
-        end
-        
-        function print_slave(obj,~,evnt)
-            postprocess = Postprocess_PhysicalProblem;
-            res_file = evnt.AffectedObject.res_file;
-            postprocess.print_slave(obj,res_file,obj.variables);
-        end
-        
-       function syncPostProcess(obj,evtobj)
-            addlistener(evtobj,'res_file','PostSet',@obj.print_slave);
-       end
-        
-        function i = getIter(obj)
-            i = obj.iter;
         end
         
     end
@@ -141,11 +58,6 @@ classdef FEM < handle
             d = ps.getValue();
         end
 
-    end
-    
-    methods (Access = public, Abstract)
-        %preProcess(obj)
-        computeVariables(obj)
     end
 
 end
--- FulldeRuta.m
+++ FulldeRuta.m
@@ -1,71 +1,23 @@
 %% To-do
 % a) CLEANUP
-%           - Stokes_Problem still uses BoundaryConditionsApplier
-%                   - Left for legacy purposes
 %       OK! - Rename New stuff to just the name
-%                   - ElasticProblem, DiffReactProblem
-%                   - ElasticProblemMicro, DiffReactProblemMicro
-%       OK!    - Delete old DOFs and Elements
-%                   - Stokes, Hyperelastic?
+%                - ElasticProblem, DiffReactProblem
+%                - ElasticProblemMicro, DiffReactProblemMicro
+%       OK! - Delete old DOFs and Elements
+%                - Stokes, Hyperelastic?
 %       OK! - Move dofsInElem to LHSintegrator
+%           - Stokes_Problem still uses BoundaryConditionsApplier
+%                - Left for legacy purposes
 
 % b) CLOSING
-%           - Adapt ThermalProblem via NewDiffReactProblem
+%       OK! - Adapt ThermalProblem via NewDiffReactProblem
 %       OK! - Add vars2print for micro
 
 % c) RESULT VISUALIZATIONS
-%           - Set up GiD for ThermalProblem and visualize results
-%           - Set up GiD for Micro problem and visualize results
+%       OK! - Set up GiD for ThermalProblem and visualize results
+%       OK! - Set up GiD for Micro problem and visualize results
 
-% d) DOFSINELEM
-%           - 
-
-% e) PERFORMANCE
+% d) PERFORMANCE
 %           - See report.mlx
 
 
-% - Stokes encara fa servir bcapplier
-% - Tancar tèrmic i micro
-% - Visualitzar resultats amb gid
-% - Mirar Strain computer
-
-%% Report
-% We are NOT using the sparse method properly. It is way better to generate
-% arrays of indices and values, and then call the sparse function to
-% assemble the matrix.
-
-
-%% Old to-do
-% a) BOUNDARY CONDITIONS
-%     OK!  - Merge BoundaryConditions and BoundaryConditionsApplier
-%     OK!  - Simplify and clean NewBoundaryConditions
-
-% b) PROBLEM CLEANUP
-%     OK!  - Clean NewDiffReactProblem
-%     OK!  - Clean NewElasticProblemMicro
-%     HMM  - Clean Thermal_Problem
-%               - Created test_thermal
-%               - "Material not yet implemented"
-%               - The physics don't really pop up in NewDiffReactProblem
-%     HMM  - Clean Hyperelastic_Problem
-%               - Created test_hyperelastic
-%               - Problem was not working properly
-%               - Uses discontinued methods? Don't really know
-%                 Isotropic2dHyperElasticMaterial
-
-% c) OTHER CLEANUP
-
-% c.1) ASSEMBLER
-%       - Simplify Assembler
-%     OK!  - Use dofsInElem at assembleMatrix()
-%          - Use dofsInElem at assembleBMatrix()
-%               - See c.2 
-%          - Explore alternate ways to assemble B and C
-%           (LHSintegrator_StiffnessElasticStoredB)
-%          - Delete globalConnec from LHSintegrator/Assembler
-
-% c.2) CANTILEVER BEAM
-%       - StrainComputer increase performance
-%             - Through "vectorize"
-%             - Through bsxfun
-%             - Through assembly+product
