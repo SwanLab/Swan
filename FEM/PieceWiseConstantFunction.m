98a1467ca9e26eba016c97f4872a78c47f46edfa
tcreusc <toncc3d@gmail.com>
2022-05-02 15:12:11 +0200

    Integrator_Simple cleanup

--- FEM/PieceWiseConstantFunction.m
+++ FEM/PieceWiseConstantFunction.m
@@ -5,7 +5,6 @@ classdef PieceWiseConstantFunction < handle
     end
     
     properties (Access = private)
-        integrator
         quadrature
         dim
     end
@@ -25,7 +24,6 @@ classdef PieceWiseConstantFunction < handle
         end
         
         function fNodal = projectToLinearNodalFunction(obj)
-            obj.createIntegrator();
             LHS = obj.computeLHS();
             RHS = obj.computeRHS();
             fNodal = (LHS\RHS);
@@ -57,26 +55,6 @@ classdef PieceWiseConstantFunction < handle
             d.compute();
             obj.dim = d;
         end
-
-        function createIntegrator(obj)
-            s.type = 'SIMPLE';
-            s.mesh = obj.mesh;
-            s.npnod = obj.mesh.nnodes;
-            s.globalConnec = obj.mesh.connec;
-            int = Integrator.create(s);
-            obj.integrator = int;
-        end
-        
-        function x = computeXgauss(obj)
-            xG = obj.quadrature.posgp;
-            x = repmat(xG,[1,1,obj.mesh.nelem]);
-        end
-        
-        function f = computeFgauss(obj)
-            ngaus = obj.quadrature.ngaus;
-            fV(1,:) = obj.fValues;
-            f = repmat(fV,[ngaus,1]);
-        end
         
         function LHS = computeLHS(obj)
             s.mesh         = obj.mesh;
@@ -89,9 +67,29 @@ classdef PieceWiseConstantFunction < handle
         end
         
         function RHS = computeRHS(obj)
-            fG = obj.computeFgauss();
-            xG = obj.computeXgauss();
-            RHS = obj.integrator.integrateFgauss(fG,xG,obj.quadOrder);
+            % Untested but should work
+            s.type      = 'ShapeFunction';
+            s.mesh      = obj.mesh;
+            s.meshType  = obj.mesh.type;
+            s.fType     = 'Gauss';
+            s.fGauss    = obj.computeFgauss();
+            s.xGauss    = obj.computeXgauss();
+            s.quadOrder = obj.quadOrder;
+            s.npnod     = obj.mesh.nnodes;
+            s.globalConnec = obj.mesh.connec;
+            rhs = RHSintegrator.create(s);
+            RHS = rhs.compute();
+        end
+        
+        function x = computeXgauss(obj)
+            xG = obj.quadrature.posgp;
+            x = repmat(xG,[1,1,obj.mesh.nelem]);
+        end
+        
+        function f = computeFgauss(obj)
+            ngaus = obj.quadrature.ngaus;
+            fV(1,:) = obj.fValues;
+            f = repmat(fV,[ngaus,1]);
         end
         
     end
--- Topology Optimization/Operators/Integrator/Integrator_Simple.m
+++ Topology Optimization/Operators/Integrator/Integrator_Simple.m
@@ -8,37 +8,16 @@ classdef Integrator_Simple < Integrator
         end
        
         function rhs = integrate(obj,fNodal)
-            quadOrder = 'LINEAR';
-            rhs = obj.integrateFnodal(fNodal,quadOrder);
-        end
-        
-        function rhs = integrateFnodal(obj,fNodal,quadOrder)
             s.type      = 'ShapeFunction';
             s.mesh      = obj.mesh;
             s.meshType  = obj.mesh.type;
             s.fType     = 'Nodal';
             s.fNodal    = fNodal;
-            s.quadOrder = quadOrder;
+            s.quadOrder = 'LINEAR';
             s.npnod     = obj.npnod;
             s.globalConnec = obj.globalConnec;
             RHS = RHSintegrator.create(s);
             rhs = RHS.compute();
-        end
-
-        % integrateFgauss is apparently used by PieceWiseConstantFunction
-        function rhs = integrateFgauss(obj,fGauss,xGauss,quadOrder)
-            type     = obj.mesh.type;
-            rhsCells = obj.computeElementalRHS(fGauss,xGauss,type,quadOrder);
-            rhs = obj.assembleIntegrand(rhsCells);
-        end
-        
-    end
-    
-    methods (Access = private)
-        
-        function xGauss = computeGaussPoints(obj,quadOrder)
-            q = obj.computeQuadrature(quadOrder);
-            xGauss = repmat(q.posgp,[1,1,obj.mesh.nelem]);
         end
         
     end
