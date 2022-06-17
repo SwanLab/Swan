7a154b4270786f34b719ba19b30996af98467d44
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2022-06-17 10:19:57 +0200

    postprocess works

---
+++ Input/test_postprocess.m
@@ -0,0 +1,38 @@
+filename = 'jaCantilever';
+ptype = 'MACRO';
+method = 'SIMPALL';
+materialType = 'ISOTROPIC';
+initial_case = 'full';
+cost = {'compliance'};
+weights = [1];
+constraint = {'volumeConstraint'};
+optimizerUnconstrained = 'PROJECTED GRADIENT';
+optimizer = 'DualNestedInPrimal';
+incrementFactor = 1.5; % Recommended: 1.5; 2.0
+designVariable = 'Density';
+filterType = 'P1';
+
+nsteps = 1; % Recommended slope: 5%/step is OK
+Vfrac_final = 0.8;
+optimality_final =1e-3;
+constr_final =1e-3;
+
+Vfrac_initial = 1;
+optimality_initial = 1e-3;
+constr_initial = 1e-3;
+Perimeter_target = 5;
+
+TOL.rho_plus = 1;
+TOL.rho_minus = 0;
+TOL.E_plus = 1;
+TOL.E_minus = 1e-3;
+TOL.nu_plus = 1/3;
+TOL.nu_minus = 1/3;
+
+% For all tests
+plotting = true;
+printing = true;
+printing_physics = false;
+monitoring = true;
+monitoring_interval = 1;
+maxiter = 2000;
