3f00de2b97e4e3e8b15354e7aacc0a83e4bf41ec
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2022-06-27 08:38:30 +0200

    Test works with swan filter

--- Topology Optimization/Applications/MinLengthScales/MinimumLengthTest.m
+++ Topology Optimization/Applications/MinLengthScales/MinimumLengthTest.m
@@ -5,6 +5,7 @@ clc
 
 N   = 500; % Number of elements
 x   = linspace(0,1,N+1); % Number of nodes
+xc  = 0.5*(x(1:end-1)+x(2:end)); % Elements coordinates
 
 % Density - initial map example
 rho = zeros(1,round(0.25*length(x)));
@@ -18,48 +19,51 @@ R = 0.10*max(x); % Filter radius
 % -----
 % Choice A
 
-rhoe = zeros(1,N);
-
-for i=1:N % Loop over elements
-    xcurr = 0.5*(x(i)+x(i+1));
-    iN = find(abs(x-xcurr)<=R); % Neighbourhood
-    Num = 0;
-    Den = 0;
-    for j=1:length(iN)-1
-        jN = iN(j);
-        Vj = x(jN+1)-x(jN);
-        xj = 0.5*(x(jN)+x(jN+1));
-        rhoj = 0.5*(rho(jN+1)+rho(jN));
-        Num = Num + (R-abs(xcurr-xj))*Vj*rhoj;
-        Den = Den + (R-abs(xcurr-xj))*Vj;
-    end
-    rhoe(i) = Num/Den;
-end
-
-xc = 0.5*(x(1:end-1)+x(2:end));
-rhoe = interp1(xc,rhoe,x);
+% rhoe = zeros(1,N);
+% 
+% for i=1:N % Loop over elements
+%     xcurr = 0.5*(x(i)+x(i+1));
+%     iN = find(abs(x-xcurr)<=R); % Neighbourhood
+%     Num = 0;
+%     Den = 0;
+%     for j=1:length(iN)-1
+%         jN = iN(j);
+%         Vj = x(jN+1)-x(jN);
+%         xj = 0.5*(x(jN)+x(jN+1));
+%         rhoj = 0.5*(rho(jN+1)+rho(jN));
+%         Num = Num + (R-abs(xcurr-xj))*Vj*rhoj;
+%         Den = Den + (R-abs(xcurr-xj))*Vj;
+%     end
+%     rhoe(i) = Num/Den;
+% end
+% 
+% rhoe = interp1(xc,rhoe,x);
 
 % Choice B
-s.quadratureOrder = 'LINEAR';
-s.mesh.nnodeElem  = 2;
-s.mesh.nnodes = N+1;
-s.mesh.type = 'LINE';
-s.mesh.kFace = 0;
-s.mesh.geometryType = 'Line';
-s.mesh.coord = x';
+s.nnodeElem  = 2;
+s.nnodes = N+1;
+s.type = 'LINE';
+s.kFace = 0;
+s.geometryType = 'Line';
+s.coord = x';
 for i=1:N
     connec(i,1) = i;
     connec(i,2) = i+1;
 end
-s.mesh.connec = connec;
-s.mesh.nelem = N;
-s.mesh.ndim = 1;
+s.connec = connec;
+s.nelem = N;
+s.ndim = 1;
 for i=1:N
     coordElem(:,:,i) = [x(i),x(i+1)];
 end
-s.mesh.coordElem = coordElem;
-s.mesh.interpolation = Interpolation.create(s.mesh,'LINEAR');
-Filter = Filter_P1_Density(s);
+s.coordElem = coordElem;
+ss.mesh = Mesh(s);
+ss.femSettings = [];
+ss.femSettings.mesh = ss.mesh;
+
+ss.quadratureOrder = 'LINEAR';
+Filter = Filter_P1_Density(ss);
+rhoe = Filter.getP1fromP0(interp1(x,rho,xc)');
 % -----
 
 beta=120;
@@ -74,3 +78,4 @@ xlabel('x coor')
 ylabel('density')
 legend('Real','Regularized','Projected')
 ylim([-0.1 1.1])
+
