c0374c8d05adbe44e9dd3510d88d0a4f706e506d
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2022-06-29 17:00:19 +0200

    struct func created

--- Topology Optimization/Applications/MinLengthScales/MinimumLengthTest.m
+++ Topology Optimization/Applications/MinLengthScales/MinimumLengthTest.m
@@ -19,55 +19,56 @@ R = 0.10*max(x); % Filter radius
 % -----
 % Choice A
 
-% rhoe = zeros(1,N);
-% 
-% for i=1:N % Loop over elements
-%     xcurr = 0.5*(x(i)+x(i+1));
-%     iN = find(abs(x-xcurr)<=R); % Neighbourhood
-%     Num = 0;
-%     Den = 0;
-%     for j=1:length(iN)-1
-%         jN = iN(j);
-%         Vj = x(jN+1)-x(jN);
-%         xj = 0.5*(x(jN)+x(jN+1));
-%         rhoj = 0.5*(rho(jN+1)+rho(jN));
-%         Num = Num + (R-abs(xcurr-xj))*Vj*rhoj;
-%         Den = Den + (R-abs(xcurr-xj))*Vj;
-%     end
-%     rhoe(i) = Num/Den;
-% end
-% 
-% rhoe = interp1(xc,rhoe,x);
+rhoe = zeros(1,N);
+
+for i=1:N % Loop over elements
+    xcurr = 0.5*(x(i)+x(i+1));
+    iN = find(abs(x-xcurr)<=R); % Neighbourhood
+    Num = 0;
+    Den = 0;
+    for j=1:length(iN)-1
+        jN = iN(j);
+        Vj = x(jN+1)-x(jN);
+        xj = 0.5*(x(jN)+x(jN+1));
+        rhoj = 0.5*(rho(jN+1)+rho(jN));
+        Num = Num + (R-abs(xcurr-xj))*Vj*rhoj;
+        Den = Den + (R-abs(xcurr-xj))*Vj;
+    end
+    rhoe(i) = Num/Den;
+end
+
+rhoe = interp1(xc,rhoe,x);
 
 % Choice B
-s.nnodeElem  = 2;
-s.nnodes = N+1;
-s.type = 'LINE';
-s.kFace = 0;
-s.geometryType = 'Line';
-s.coord = x';
-for i=1:N
-    connec(i,1) = i;
-    connec(i,2) = i+1;
-end
-s.connec = connec;
-s.nelem = N;
-s.ndim = 1;
-for i=1:N
-    coordElem(:,:,i) = [x(i),x(i+1)];
-end
-s.coordElem = coordElem;
-ss.mesh = Mesh(s);
-ss.femSettings = [];
-ss.femSettings.mesh = ss.mesh;
-
-ss.quadratureOrder = 'LINEAR';
-Filter = Filter_P1_Density(ss);
-rhoe = Filter.getP1fromP0(interp1(x,rho,xc)');
+% s.nnodeElem  = 2;
+% s.nnodes = N+1;
+% s.type = 'LINE';
+% s.kFace = 0;
+% s.geometryType = 'Line';
+% s.coord = x';
+% for i=1:N
+%     connec(i,1) = i;
+%     connec(i,2) = i+1;
+% end
+% s.connec = connec;
+% s.nelem = N;
+% s.ndim = 1;
+% for i=1:N
+%     coordElem(:,:,i) = [x(i),x(i+1)];
+% end
+% s.coordElem = coordElem;
+% ss.mesh = Mesh(s);
+% ss.femSettings = [];
+% ss.femSettings.mesh = ss.mesh;
+% 
+% ss.quadratureOrder = 'LINEAR';
+% Filter = Filter_P1_Density(ss);
+% rhoe = Filter.getP1fromP0(interp1(x,rho,xc)');
 % -----
 
+c = (R/(x(2)-x(1)))^4;
 beta=120;
-eta=0;
+eta=0.03;
 rhoP=(tanh(beta*eta)+tanh(beta*(rhoe-eta)))./(tanh(beta*eta)+tanh(beta*(1-eta)));
 
 figure
@@ -79,3 +80,16 @@ ylabel('density')
 legend('Real','Regularized','Projected')
 ylim([-0.1 1.1])
 
+etaMin = 0.1;
+etaMax = 0.9;
+
+gradRhoe = diff(rhoe)./diff(x);
+gradRhoe = interp1(xc,gradRhoe,x);
+Isolid = rhoP.*exp(-c*gradRhoe.^2);
+Ivoid = (1-rhoP).*exp(-c*gradRhoe.^2);
+
+Isolid(isnan(Isolid))=0;
+Ivoid(isnan(Ivoid))=0;
+
+gSolid = sum(Isolid.*min((rhoe-etaMax),0).^2)/(N+1);
+gVoid = sum(Ivoid.*min((etaMin-rhoe),0).^2)/(N+1);
