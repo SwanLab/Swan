de428b9c10109c3783a07ca99da876e92eb20201
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2022-09-28 15:25:16 +0200

    testing charFun

--- Topology Optimization/testingCharacteristicFunction.m
+++ Topology Optimization/testingCharacteristicFunction.m
@@ -17,3 +17,14 @@ x.connec = m.mesh.connec;
 projP1 = Projector_toP1(x);
 resCharFunInP1 = projP1.project(circleFun);
 resCharFunInP1.plot(m.mesh);
+view(0,90)
+
+% CharFun to P0 Function
+projP0 = Projector_toP0(x);
+resCharFunInP0 = projP0.project(circleFun);
+resCharFunInP0.plot(m.mesh);
+
+% CharFun to P1 Discontinuous Function
+projP1D = Projector_toP1Discontinuous(x);
+resCharFunInP1D = projP1D.project(circleFun);
+resCharFunInP1D.plot(m.mesh);
