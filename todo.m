a225c3f6ab8f45b44776bb5a06fcf8f1941cbd9a
tcreusc <toncc3d@gmail.com>
2023-01-27 13:05:55 +0100

    Update todo.m

--- todo.m
+++ todo.m
@@ -4,16 +4,16 @@
 
 % - {{fet}} Netejar FullInnerMeshCreators
 % - {{fet}} Update a la master
-% - Prints de P1Functions, P1DiscontinuousFunction i FGaussFunctions
+% - {{fet}} Prints de P1Functions, P1DiscontinuousFunction i FGaussFunctions
 
 
 % - composite function printing passant n funcions
-%       - caldra nom de la funcio a printejar...
-%       - exemple de stokes per imprimir els dos
-%           alhora/tensions+desplacaments
-%       - mantenir funcionalitat per printejar una funcio
-%       - paraview
-%       - uml de tot plegat
+%       - {{fet}} caldra nom de la funcio a printejar...
+%       - {{fet}} exemple de stokes per imprimir els dos
+%                   alhora/tensions+desplacaments
+%       - {{fet}} mantenir funcionalitat per printejar una funcio
+%       - {{fet}} paraview
+%       - {{no fet}} uml de tot plegat (es una classe)
 %       - recuperar gid unfitted mesh photo GiDimagecapturer
 %           density --(project)--> unfittedmesh -> innermesh/photo
 
