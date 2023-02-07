372a1eecf7dde439542a6b61d3f82333d66007b1
tcreusc <toncc3d@gmail.com>
2023-02-07 12:38:19 +0100

    Update todo.m

--- todo.m
+++ todo.m
@@ -1,50 +1,29 @@
 %% To-do
-% - GiDImageCapturer -> some variables should be "user variables", defined
-%   once in a centralized file
+% (0) Mesh refining P1Function (RemeshingTests)
+% (1) Macro printing with FeFunctions
+% (2) Micro printing with FeFunctions
+% (3) Micro TopOpt printing
 
+% EXTRAS
+%  - Investigate: converting data to binary format to save read'n'write
+%                 resources for paraview
+%  - Tutorial for printing
+%  - Study file ouptut size vs time (GiD/Paraview) to see which is better
+%    for printing (test + graph)
+%  - Check XY component of fgaussfunctions
 
+%% Mid-term
+% - Use FeFunctions in TopOpt_Problem
+% - PDE belongs to Optimizer, not ShapeFunctional
+% - Micro as three elasticity problems
 
-%%
-
-% investigate: converting data to binary format to save read'n'write
-% resources for paraview
-% wtf is rho @ DesignVariable
-
-% fem.print
-%   - use functions
-% topopt.print
-%   - use functions
-%       - check printing for various time steps in gid
-%       - check printing for various time steps in paraview
-%   - optimizer: designVar, pde, CC (which have functionals)
-%           - designvar pde and functionals should print
-%           - create fefunctions in each of these classes for printing
-%           *only*
-%           - later on we will eventually do more stuff
-
-
-% 
-% - check XY componennt of fgaussfunctions
-% - study file output size vs time (gid/paraview) to see which is better
-%   for printing
-%       - instructions at the wiki (running the test + graph)
-% - update to master after cleanup
-
-% opengl, llicencies, gid 
-% https://www.youtube.com/watch?v=zikDxtlvbUA
-
-% ppt -> add gifs, not pictures
-
-%% Comments
-
-% Cantileverbeam_Hexahedra_Linear_Structured_SYM
-% refining in P1DiscontinuousFunction vs P1Function (see RemeshingTests)
-
-%% per mes endavant
-% - moure input a repositori a part per alleugerir
-% - geometry only in mesh
+%% Long-term
+% - Move Input folder to a separat repository
+% - Geometry only in Mesh
 % - kill Mesh_Total (UnfittedMesh). still used somewhere but should be
 %   removed
-
-% - recuperar gid unfitted mesh photo GiDimagecapturer
+% - Recuperar gid unfitted mesh photo GiDimagecapturer
 %      density --(project)--> unfittedmesh -> innermesh/photo
+
+% - GiDImageCapturer -> some variables should be "user variables", defined
+%   once in a centralized file
