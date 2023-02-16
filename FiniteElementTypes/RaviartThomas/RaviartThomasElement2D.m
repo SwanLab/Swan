37607b386713cce691e987c3f791295252b1b7e6
tarrut <tarrut.code@gmail.com>
2023-02-16 15:10:13 +0100

    Update RaviartThomasElement2D
    RaviartThomasElement2D now deduces the shape functions.

--- FiniteElementTypes/RaviartThomas/RaviartThomasElement2D.m
+++ FiniteElementTypes/RaviartThomas/RaviartThomasElement2D.m
@@ -9,6 +9,7 @@ classdef RaviartThomasElement2D < handle
         n_vertices
         vertices
         measure
+        normalVectors
         shapeFunctions
         fig
     end
@@ -49,26 +50,65 @@ classdef RaviartThomasElement2D < handle
        
         function init(obj)
             obj.computeVertices()
-            obj.computeMeasure()
+            obj.computeNormalVectors()
             obj.computeShapeFunctions()
         end
         
         function computeVertices(obj)
             obj.n_vertices = 3;
-            obj.vertices = [0,0;0,1;1,0];
+            obj.vertices = [0,0;1,0;0,1];
         end
         
-        function computeMeasure(obj)
-            obj.measure = (obj.vertices(2,2)*obj.vertices(3,1))/2;
+        function computeNormalVectors(obj)
+            obj.normalVectors = [1,1;-1,0;0,-1];
+        end
+        
+        function F = integral_func(~,f,A,B)
+            syms x y a1 b1 a2 b2 real
+            F = f;
+            if A(1) ~= B(1)
+                f_int = F;
+                b = max(A(1),B(1));
+                a = min(A(1),B(1));
+                F = int(f_int,x,a,b);
+            else
+                f_int = F;
+                F = subs(f_int,x,A(1));
+            end
+            if A(2) ~= B(2)
+                f_int = F;
+                b = max(A(2),B(2));
+                a = min(A(2),B(2));
+                F = int(f_int,y,a,b);
+            else
+                f_int = F;
+                F = subs(f_int,y,A(2));
+            end
         end
         
         function computeShapeFunctions(obj)
+            syms x y a1 a2 b1 real
+            
+            p = [a1+b1*x,a2+b1*y];
+            for j = 1:obj.n_vertices
+                pn(j) = dot(p,obj.normalVectors(j,:));
+            end
+            
+            A(1) = obj.integral_func(pn(1),obj.vertices(2,:),obj.vertices(3,:));
+            A(2) = obj.integral_func(pn(2),obj.vertices(3,:),obj.vertices(1,:));
+            A(3) = obj.integral_func(pn(3),obj.vertices(1,:),obj.vertices(2,:));
+            
             for i = 1:obj.n_vertices
-                syms x y
-                obj.shapeFunctions{i} = 1/(2*obj.measure)*([x,y]-obj.vertices(i,:));
+                b = zeros(1,obj.n_vertices);
+                b(i) = 1;
+                
+                eq = A == b;
+                s = solve(eq,[a1 a2 b1]);
+                obj.shapeFunctions{i} = [s.a1+s.b1*x,s.a2+s.b1*y];
             end
         end
         
     end
     
 end
+
