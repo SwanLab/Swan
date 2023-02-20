0ea7addb056ce0c431b350e40b40e3b1f8f582d2
tarrut <tarrut.code@gmail.com>
2023-02-21 00:28:29 +0100

    Update NedelecElement2D.m
    Now NedelecElement2D deduces the shape functions

--- FiniteElementTypes/Nédélec/NedelecElement2D.m
+++ FiniteElementTypes/Nédélec/NedelecElement2D.m
@@ -51,38 +51,59 @@ classdef NedelecElement2D < handle
         function init(obj)
             obj.computeVertices()
             obj.computeEdges()
-            obj.computeMeasure()
             obj.computeShapeFunctions()
         end
         
         function computeVertices(obj)
             obj.n_vertices = 3;
-            obj.vertices = [0,0;0,1;1,0];
+            obj.vertices = [0,0;1,0;0,1];
         end
         
         function computeEdges(obj)
-            obj.edges.vect = [sqrt(2)/2,-sqrt(2)/2;-1,0;0,1];
+            obj.edges.vect = [-1,1;0,-1;1,0];
             obj.edges.measure = [sqrt(2),1,1];
         end
         
-        function computeMeasure(obj)
-            obj.measure = (obj.vertices(2,2)*obj.vertices(3,1))/2;
-        end
-        
-        function [b] = Rmap(obj,a)
-            b(1) = -a(2);
-            b(2) = a(1);
+        function F = integral_func(~,f,A,B)
+            syms x y a1 b1 a2 b2 t real
+            F = f;
+            
+            if A(1) == B(1)
+                F = subs(F,x,A(1));
+                F = subs(F,y,t);
+            elseif A(2) == B(2)
+                F = subs(F,y,A(2));
+                F = subs(F,x,t);
+            else
+                m = (A(2)-B(2))/(A(1)-B(1));
+                n = A(2)-m*A(1);
+                F = subs(F,y,m*x+n);
+                F = subs(F,x,t);
+            end
+            
+            F = int(F,t,0,1);
         end
         
         function computeShapeFunctions(obj)
-                syms x y
-                obj.shapeFunctions{1} = obj.Rmap([x y]-obj.vertices(1,:))/(obj.edges.measure(1)*...
-                    dot(obj.edges.vect(1,:),obj.Rmap((obj.vertices(2,:)+obj.vertices(3,:))/2-obj.vertices(1,:))));
-                obj.shapeFunctions{2} = obj.Rmap([x y]-obj.vertices(2,:))/(obj.edges.measure(2)*...
-                    dot(obj.edges.vect(2,:),obj.Rmap((obj.vertices(1,:)+obj.vertices(3,:))/2-obj.vertices(2,:))));
-                obj.shapeFunctions{3} = obj.Rmap([x y]-obj.vertices(3,:))/(obj.edges.measure(3)*...
-                    dot(obj.edges.vect(3,:),obj.Rmap((obj.vertices(2,:)+obj.vertices(1,:))/2-obj.vertices(3,:))));
-
+            syms x y a1 a2 b1 real
+            
+            p = [a1-b1*y,a2+b1*x];
+            for j = 1:obj.n_vertices
+                pn(j) = dot(p,obj.edges.vect(j,:));
+            end
+            
+            A(1) = obj.integral_func(pn(1),obj.vertices(2,:),obj.vertices(3,:));
+            A(2) = obj.integral_func(pn(2),obj.vertices(3,:),obj.vertices(1,:));
+            A(3) = obj.integral_func(pn(3),obj.vertices(1,:),obj.vertices(2,:));
+            
+            for i = 1:obj.n_vertices
+                b = zeros(1,obj.n_vertices);
+                b(i) = 1;
+                
+                eq = A == b;
+                s = solve(eq,[a1 a2 b1]);
+                obj.shapeFunctions{i} = [s.a1-s.b1*y,s.a2+s.b1*x];
+            end
         end
         
     end
