9ecf59a78656eb3ca74079fa341a580d254d2d7e
tarrut <tarrut.code@gmail.com>
2023-02-27 10:04:46 +0100

    Refactor

--- FiniteElementTypes/Lagrange/LagrangeSimplicial2D.m
+++ FiniteElementTypes/Lagrange/LagrangeSimplicial2D.m
@@ -23,27 +23,19 @@ classdef LagrangeSimplicial2D < handle
             s.coord = obj.vertices;
             s.connec = [1 2 3];
             m = Mesh(s);
-            m.plot()
-
-            a = trisurf(m.connec,s.coord(:,1),s.coord(:,2),obj.shapeFunctions{1,1});
-
+            
             obj.fig = figure();
-            syms x y
-            hold on
-            for i=1:obj.k+1
-                for j = 1:obj.k+1
-                    if (j+i)<=(obj.k+2)
-                        subplot(obj.k+1,obj.k+1,(i-1)*(obj.k+1)+j)
-                        func = piecewise(x+y<=1,obj.shapeFunctions{i,j});
-                        fsurf(func,[0 1])
-                        zlim([-0.5 1])
-                        xlabel('x')
-                        ylabel('y')
-                        zlabel('z')
-                        title("i:"+string(i-1)+", j:"+string(j-1))
-                        grid on
-                    end
-                end
+            for i=1:obj.n_nodes
+                subplot(obj.k+1,obj.k+1,I)
+                m.plot()
+                
+                points = evaluateFunction(m,I);
+                surf(points(:,1),points(:,2),points(:,3));
+                
+                zlim([-0.5 1])
+                xlabel('x'); ylabel('y'); zlabel('z')
+                title("I:"+string(I-1))
+                grid on
             end
         end
     
@@ -57,6 +49,7 @@ classdef LagrangeSimplicial2D < handle
             obj.computeVertices()
             obj.computeNodes()
             obj.computeShapeFunctions()
+            obj.storeShapeFunctions()
         end
         
         function computeVertices(obj)
@@ -80,52 +73,84 @@ classdef LagrangeSimplicial2D < handle
         
         function computeShapeFunctions(obj)
             syms x y
+            X = obj.assemblyBasis();
             
             for i = 1:(obj.k+1)
                 for j = 1:(obj.k+1)
                     if (i+j)<=(obj.k+2)
-                        n = -(obj.k+2);
-                        for m = 1:i
-                            n = n + obj.k + 1 - (m-2);
-                        end
-                        I = n+j;
+                        s = obj.computeShapeFunctionCoefficients(X,i,j);
+                        I = computeSimplexIndeces(i,j);
+                        obj.shapeFunctions{I} = X*s;
+                    end
+                end
+            end
+        end
+        
+        function s = computeShapeFunctionCoefficients(obj,X,i,j)
+            A = obj.assemblyShapeFunctionCoefficientsLHS(X);
+            B = obj.assemblyShapeFunctionCoefficientsRHS(i,j);
+            s = A\B;
+        end
+        
+        function storeShapeFunctions(obj)
+            syms x y
+            for i = 1:(obj.k+1)
+                for j = 1:(obj.k+1)
+                    if (i+j)<=(obj.k+2)
+                        obj.shapeFunctions{i,j} = matlabFunction(obj.shapeFunctions{i,j});
+                    end
+                end
+            end
+        end
+        
+        function X = assemblyBasis(obj)
+            syms x y
+            for i = 1:(obj.k+1)
+                for j = 1:(obj.k+1)
+                    if (i+j)<=(obj.k+2)
+                        I = computeSimplexIndeces(i,j);
                         X(I) = x^(i-1)*y^(j-1);
                     end
                 end
             end
-                      
+        end
+        
+        function B = assemblyShapeFunctionCoefficientsRHS(obj,i,j)
+            I = computeSimplexIndeces(i,j);
+            B = zeros(obj.n_nodes,1);
+            B(I) = 1;
+        end
+        
+        function A = assemblyShapeFunctionCoefficientsLHS(obj,X)
+            syms x y
+            A = zeros(obj.n_nodes);
             for i = 1:(obj.k+1)
                 for j = 1:(obj.k+1)
                     if (i+j)<=(obj.k+2)
-                        n = -(obj.k+2);
-                        for m = 1:i
-                            n = n + obj.k + 1 - (m-2);
-                        end
-                        I = n+j;
-
-                        b = zeros(obj.n_nodes,1);
-                        b(I) = 1;
-                        
-                        A = zeros(obj.n_nodes);
-                        for ii = 1:(obj.k+1)
-                            for jj = 1:(obj.k+1)
-                                if (ii+jj)<=(obj.k+2)
-                                    nn = -(obj.k+2);
-                                    for m = 1:ii
-                                        nn = nn + obj.k + 1 - (m-2);
-                                    end
-                                    II = nn+jj;
-                                    node(:) = obj.nodes(ii,jj,:);
-                                    A(II,:) = subs(X,[x y],node);
-                                end
-                            end
-                        end
-                        
-                        s = A\b;
-                        obj.shapeFunctions{i,j} = X*s;
+                        I = computeSimplexIndeces(i,j);
+                        A(I,:) = subs(X,[x y],node);
                     end
                 end
             end
+        end
+        
+        function I = computeSimplexIndeces(obj,i,j)
+            n = -(obj.k+2);
+            for m = 1:i
+                n = n + obj.k + 1 - (m-2);
+            end
+            I = n+j;
+        end
+        
+        function points = obj.evaluateFunction(obj,m,I)
+            p1 = m.coord(1,:);
+            p2 = m.coord(2,:);
+            p3 = m.coord(3,:);
+
+            b = rand(1000,2);
+            b(:,3) = 1 - b(:,1) - b(:,2);
+            points = b(:,1) .* p1 + b(:,2) .* p2 + b(:,3) .* p3;
+            points(:,3) = obj.shapeFunctions{I}(points(:,1),points(:,2));
         end
         
     end
