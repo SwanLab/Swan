32bb3f637064784f1c920227c08e445d130ac234
ariadnasb01 <ariadna.sb01@gmail.com>
2023-02-28 19:41:49 +0100

    MasterSlave initial micro3d cube

--- FEM/Preprocess/MasterSlaveRelator.m
+++ FEM/Preprocess/MasterSlaveRelator.m
@@ -3,6 +3,7 @@ classdef MasterSlaveRelator < handle
     properties (Access = private)
         x
         y
+        z
         nodesInXmin
         nodesInXmax
         nodesInYmin
@@ -10,14 +11,22 @@ classdef MasterSlaveRelator < handle
         cellDescriptor
         msRelation
         allNodes
+        vertices
     end
     
     methods (Access = public)
         
         function obj = MasterSlaveRelator(coord)
-            obj.init(coord);
-            obj.computeNodesInFaces(coord)
-            obj.computeMasterSlaveRelation();
+            switch size(coord,2)
+                case 2
+                    obj.init2(coord);
+                    obj.computeNodesInFaces(coord);
+                    obj.computeMasterSlaveRelation();
+                case 3
+                    obj.init3(coord);
+                    obj.get_vertices(coord);
+                    obj.get_MasterSlave(coord)
+            end
         end
         
         function r = getRelation(obj)
@@ -28,7 +37,7 @@ classdef MasterSlaveRelator < handle
     
     methods (Access = private)
         
-        function init(obj,coord)
+        function init2(obj,coord)
             obj.x = coord(:,1);
             obj.y = coord(:,2);
             obj.allNodes(:,1) = 1:size(obj.x,1);
@@ -73,7 +82,70 @@ classdef MasterSlaveRelator < handle
                 closestNodesUF(inode) = obj.obtainClosestNode(nodeLF,nodesUF,pos);
             end
         end
-        
+
+        function init3(obj,coord)
+            obj.x = coord(:,1);
+            obj.y = coord(:,2);
+            obj.z = coord(:,3);
+%             obj.allNodes(:,1) = 1:size(obj.x,1);
+        end
+
+        function get_vertices(obj,coord)
+            v = [];
+            for i = 1:length(coord) %   find ????
+                nodeGID = coord(i,:);
+                if (isequal(nodeGID, [0,0,0]) || isequal(nodeGID, [0,0,1]) || ...
+                        isequal(nodeGID, [0,1,0]) || isequal(nodeGID, [1,0,0]) || ...
+                        isequal(nodeGID, [0,1,1]) || isequal(nodeGID, [1,0,1]) || ...
+                        isequal(nodeGID, [1,1,0]) || isequal(nodeGID, [1,1,1]))
+                    v = [v; nodeGID];
+                end
+            end
+            obj.vertices = v;
+        end 
+
+        function get_MasterSlave(obj,gidcoord)
+            MS = [ ];
+            v = obj.vertices;
+            for i = 1:length(gidcoord)
+                if ismember(gidcoord(i,:),v,'rows')
+                else % if node is not a vertice
+                    if any(gidcoord==0)
+                        xCoord = gidcoord(i,1);
+                        yCoord = gidcoord(i,2);
+                        zCoord = gidcoord(i,3);
+                        if xCoord==0
+                            for j = 1:length(gidcoord)
+                                nodeCompared = gidcoord(j,1:3);
+                                nodeSuposed = [xCoord+1,yCoord,zCoord];
+                                if (isequal(nodeCompared,nodeSuposed))
+                                    MS = [MS; [i j]];
+                                end
+                            end
+                        end
+                        if yCoord==0
+                            for j = 1:length(gidcoord)
+                                nodeCompared = gidcoord(j,1:3);
+                                nodeSuposed = [xCoord,yCoord+1,zCoord];
+                                if (isequal(nodeCompared,nodeSuposed))
+                                    MS = [MS; [i j]];
+                                end
+                            end
+                        end
+                        if zCoord==0
+                            for j = 1:length(gidcoord)
+                                nodeCompared = gidcoord(j,1:3);
+                                nodeSuposed = [xCoord,yCoord,zCoord+1];
+                                if (isequal(nodeCompared,nodeSuposed))
+                                    MS = [MS; [i j]];
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            obj.msRelation = MS;
+        end
     end
     
     methods (Access = private, Static)
