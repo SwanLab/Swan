a12e5f0b401d4e0766110863bdcc91e56039de9a
tcreusc <toncc3d@gmail.com>
2023-03-07 18:29:25 +0100

    hole inclusion for 3d

--- generatingHoleInclusionMesh.m
+++ generatingHoleInclusionMesh.m
@@ -30,3 +30,31 @@ fem.computeChomog();
 fem.uFun{1}.plot
 fem.uFun{2}.plot
 fem.uFun{3}.plot
+
+%% Generating a 3D mesh with a hole inclusion
+% Using functions!
+clc; clear; close all
+
+% Create the data container for the FEM problem
+a.fileName = 'test3d_micro_cube';
+m = FemDataContainer(a);
+
+% Create the characteristic function (1 inside circle, 0 outside)
+s.mesh    = m.mesh;
+s.fxy     = @(x,y,z) (x-0.5).^2+(y-0.5).^2+(z-0.5).^2 -0.3.^2;
+circleFun = CharacteristicFunction(s);
+
+% Project the function to P0. Useful later on
+x.mesh   = m.mesh;
+x.connec = m.mesh.connec;
+projP0 = Projector_toP0(x);
+p0c = projP0.project(circleFun);
+
+% Generate the hole in the material using the values we just found
+fV = squeeze(p0c.fValues);
+holeNodes = find(fV==1);
+m.material.C(:,:,holeNodes) = zeros(6,6, length(holeNodes));
+
+% Solve the problem
+% fem = ElasticProblemMicro(m);
+% fem.computeChomog();
