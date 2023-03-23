9e577c5339c052f0a97070aa3fc0dc317fab55de
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2023-03-23 15:27:26 +0100

    square filtering

--- shortcuts.m
+++ shortcuts.m
@@ -44,3 +44,22 @@ xlabel('Iterations','Interpreter','latex')
 ylabel('Nullspace coefficients','Interpreter','latex')
 legend('$a_J$','$a_G$','Interpreter','latex')
 ylim([-0.1 1])
+
+
+
+a.fileName = 'SquareForAniTests';
+s = FemDataContainer(a);
+d.type = 'squareInclusion';
+d.coord = s.mesh.coord;
+d.ndim = s.mesh.ndim;
+d.widthSquare = sqrt(0.15);
+LS = LevelSetFactory.create(d);
+psi = LS.getValue();
+chi = 1-heaviside(psi);
+ss.mesh = s.mesh;
+ss.fValues = chi;
+Square = P1Function(ss);
+ss.filename = 'seminarExample';
+% Square.print(ss);
+SquareRelIso = Square.project('H1P1');
+SquareRelIso.print(ss)
