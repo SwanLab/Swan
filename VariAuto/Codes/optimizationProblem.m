d1e6fe1e001d533e3a0dd55f1047a4c3d5b7114c
Joel Campo Moyà <joel.joel.campo@gmail.com>
2023-03-30 11:33:21 +0200

    Not optimizing

--- VariAuto/Codes/optimizationProblem.m
+++ VariAuto/Codes/optimizationProblem.m
@@ -19,90 +19,91 @@ classdef optimizationProblem < handle
 
    methods (Access = public)
 
-       function self = optimizationProblem(varargin)
-           self.init(varargin);
-           self.thetavec = self.network.thetavec;
+       function obj = optimizationProblem(varargin)
+           obj.init(varargin);
+           obj.thetavec = obj.network.thetavec;
        end
        
-       function computeCost(self,theta,Xb,Yb)
-           self.thetavec = theta;
-           [J,grad] = self.propagate(self.network.layer,Xb,Yb); 
-           self.loss = self.loss;
-           l = self.network.lambda;
-           self.regularization = l*self.regularization;
-           self.cost = J; 
-           self.gradient = grad;
+       function computeCost(obj,theta,Xb,Yb)
+           obj.network.thetavec = theta;
+           obj.thetavec = theta;
+           [J,grad] = obj.propagate(obj.network.layer,Xb,Yb); 
+           obj.loss = obj.loss;
+           l = obj.network.lambda;
+           obj.regularization = l*obj.regularization;
+           obj.cost = J; 
+           obj.gradient = grad;
        end 
               
-       function h = getOutput(self,X)
-            h = self.compute_last_H(X);
+       function h = getOutput(obj,X)
+            h = obj.compute_last_H(X);
        end
 
-       function plotBoundary(self,type) 
-           self.plotter.plotBoundary(type);
+       function plotBoundary(obj,type) 
+           obj.plotter.plotBoundary(type);
        end
 
-       function plotConections(self)
-           self.plotter.plotNetworkStatus();
+       function plotConections(obj)
+           obj.plotter.plotNetworkStatus();
        end
 
-       function plotConfusionMatrix(self)
-           self.plotter.drawConfusionMat();
+       function plotConfusionMatrix(obj)
+           obj.plotter.drawConfusionMat();
        end
    end
 
    methods (Access = private)
 
-       function init(self,s)
-           self.data = s{1};
-           self.network = s{2};
-           self.lambda = self.network.lambda;
-           self.plotter = Plotter(self);
+       function init(obj,s)
+           obj.data = s{1};
+           obj.network = s{2};
+           obj.lambda = obj.network.lambda;
+           obj.plotter = Plotter(obj);
        end  
 
-       function [J,gradient] = propagate(self,layer,Xb,Yb)
-            self.forwardprop(layer,Xb,Yb);
-            J = self.cost;
-            gradient = self.backprop(layer,Yb);
+       function [J,gradient] = propagate(obj,layer,Xb,Yb)
+            obj.forwardprop(layer,Xb,Yb);
+            J = obj.cost;
+            gradient = obj.backprop(layer,Yb);
        end  
 
-       function g = compute_last_H(self,X)
-           nLy = self.network.nLayers;
-           layer = self.network.layer;
-           h = self.hypothesisfunction(X,layer{1}.W,layer{1}.b);
-            [g,~] = self.actFCN(h,2);
+       function g = compute_last_H(obj,X)
+           nLy = obj.network.nLayers;
+           layer = obj.network.layer;
+           h = obj.hypothesisfunction(X,layer{1}.W,layer{1}.b);
+            [g,~] = obj.actFCN(h,2);
             for i = 2:nLy-1
-                h = self.hypothesisfunction(g,layer{i}.W,layer{i}.b);
-                [g,~] = self.actFCN(h,i+1);
+                h = obj.hypothesisfunction(g,layer{i}.W,layer{i}.b);
+                [g,~] = obj.actFCN(h,i+1);
             end
        end   
 
-       function forwardprop(self,layer,Xb,Yb)
-           self.computeLoss(layer,Xb,Yb);
-           self.computeRegularization(layer);
-           c = self.loss;
-           r = self.regularization;
-           l = self.lambda;
-           self.cost = c + l*r;
+       function forwardprop(obj,layer,Xb,Yb)
+           obj.computeLoss(layer,Xb,Yb);
+           obj.computeRegularization(layer);
+           c = obj.loss;
+           r = obj.regularization;
+           l = obj.lambda;
+           obj.cost = c + l*r;
        end
 
-       function computeLoss(self,layer,Xb,Yb)          
-           nLy = self.network.nLayers;
+       function computeLoss(obj,layer,Xb,Yb)          
+           nLy = obj.network.nLayers;
            a = cell(nLy,1);
            a{1} = Xb;
            for i = 2:nLy
                g_prev = a{i-1};
-               h = self.hypothesisfunction(g_prev,layer{i-1}.W,layer{i-1}.b);
-               [g,~] = self.actFCN(h,i);
+               h = obj.hypothesisfunction(g_prev,layer{i-1}.W,layer{i-1}.b);
+               [g,~] = obj.actFCN(h,i);
                a{i} = g;
            end
-           self.a_fcn = a;
-           [J,~] = self.costFunction(Yb,a);
-           self.loss = J;
+           obj.a_fcn = a;
+           [J,~] = obj.costFunction(Yb,a);
+           obj.loss = J;
        end
 
-       function computeRegularization(self,layer)
-           nLy = self.network.nLayers;
+       function computeRegularization(obj,layer)
+           nLy = obj.network.nLayers;
            s = 0;
            nth = 0;
            for i = 2:nLy
@@ -110,29 +111,29 @@ classdef optimizationProblem < handle
                 nth = nth + length(layer{i-1}.theta);
            end
            r = 1/(2)*s;
-           self.regularization = r;
+           obj.regularization = r;
        end
 
-       function grad = backprop(self,layer,Yb)
-           a = self.a_fcn;
-           nPl = self.network.neuronsPerLayer;
-           nLy = self.network.nLayers;
+       function grad = backprop(obj,layer,Yb)
+           a = obj.a_fcn;
+           nPl = obj.network.neuronsPerLayer;
+           nLy = obj.network.nLayers;
            m = length(Yb);
            deltag = cell(nLy,1);
            gradW = cell(nLy-1,1);
            gradb = cell(nLy-1,1);
            for k = nLy:-1:2    
-               [~,a_der] = self.actFCN(a{k},k); 
+               [~,a_der] = obj.actFCN(a{k},k); 
                if k == nLy
-                   [~,t1] = self.costFunction(Yb,a);  
+                   [~,t1] = obj.costFunction(Yb,a);  
                    deltag{k} = t1.*a_der;
                else                    
                    deltag{k} = (layer{k}.W*deltag{k+1}')'.*a_der;
                end
-               gradW{k-1} = (1/m)*(a{k-1}'*deltag{k}) + self.lambda*layer{k-1}.W;
-               gradb{k-1} = (1/m)*(sum(deltag{k},1)) + self.lambda*layer{k-1}.b;
+               gradW{k-1} = (1/m)*(a{k-1}'*deltag{k}) + obj.lambda*layer{k-1}.W;
+               gradb{k-1} = (1/m)*(sum(deltag{k},1)) + obj.lambda*layer{k-1}.b;
            end
-           self.delta = deltag;
+           obj.delta = deltag;
            grad = [];
            for i = 2:nLy
                aux = [reshape(gradW{i-1},[1,nPl(i-1)*nPl(i)]),gradb{i-1}];
@@ -140,8 +141,8 @@ classdef optimizationProblem < handle
            end
        end
 
-       function [J,gc] = costFunction(self,y,a)
-            type = self.network.Costtype;
+       function [J,gc] = costFunction(obj,y,a)
+            type = obj.network.Costtype;
             yp = a{end}-10^-10;
             switch type
                 case '-loglikelihood'
@@ -158,12 +159,12 @@ classdef optimizationProblem < handle
             end
        end
 
-       function [g,g_der] = actFCN(self,z,k)
-            nLy = self.network.nLayers;
+       function [g,g_der] = actFCN(obj,z,k)
+            nLy = obj.network.nLayers;
             if k == nLy
-                type = self.network.OUtype;
+                type = obj.network.OUtype;
             else
-                type = self.network.HUtype;
+                type = obj.network.HUtype;
             end
             switch type 
                 case 'sigmoid'
