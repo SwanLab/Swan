10dbfcd424d583f1f47c8573418afc5854757cab
PolRuiz <polruizc6@gmail.com>
2023-04-06 18:25:19 +0200

    minres x0 = xprev

--- FEM/Solver/MINRES_Pol.m
+++ FEM/Solver/MINRES_Pol.m
@@ -1,124 +1,169 @@
 classdef MINRES_Pol < Solver
 
 
-        
-      methods (Static)
+
+    methods (Static)
 
         function x_k = solve(A,b)
-        n = size(A,1);
-        k = 3000;
-        
-        Alpha = zeros(k+1,1);
-        
-        Beta    = zeros(k+1,1);
-        Beta(1) = norm(b);
-        
-        V       = zeros(n,k);
-        v_0     = 0;
-        V(:,1)  = b/Beta(1);
-        
-        c = -1;
-        s = 0;
-        
-        Gamma   = zeros(k,1);
-        Delta   = zeros(k,1);
-        Epsilon = zeros(k,1);
-        
-        Gamma(1) = 0;
-        
-        phi = Beta(1);
-        
-        d = zeros(n,k);
-        x0   = 0;
-        
-        t = zeros(k,1);
-        
-        
-        %j=1
-            j = 1;
-            x_prev = x0;
-            [Alpha(j), Beta(j+1), V(:,j+1)] = lanczosProcess3(A, V(:,j), v_0, Beta(j));
+            tic
+            n = size(A,1);
+            k = 3000;
+            maxiter = k;
+            tol = 10^(-4);
             
-            deltaj_prev = Delta(j);
-            Delta(j) = c*deltaj_prev + s*Alpha(j);
-            Gamma(j) = s*deltaj_prev - c*Alpha(j);
-            Epsilon(j+1) = s*Beta(j+1);
-            Delta(j+1) = -c*Beta(j+1);
-        
-            shi = Gamma(j)^2 + Beta(j+1)^2 + sign(Gamma(j))*Gamma(j)*sqrt(Gamma(j)^2+Beta(j+1)^2);
-            c = ((Beta(j+1)^2)/shi)-1;
-            s = -(Beta(j+1)*(Gamma(j) + sign(Gamma(j))*sqrt(Gamma(j)^2+Beta(j+1)^2)))/shi;
-            
-            phi_prev = phi;
-            phi  = s*phi_prev;
-            t(j) = c*phi_prev;
-        
-            Gamma(j) = c*Gamma(j) + s*Beta(j+1);
-            % R(:,j) is now completed
-        
-            d(:,j) = V(:,j)/Gamma(j);
-        
-            x1 = x_prev + t(j)*d(:,j);
-        
-        %j=2
-            j = 2;
-            
-            x_prev = x1;
-        
-            [Alpha(j), Beta(j+1), V(:,j+1)] = lanczosProcess3(A, V(:,j), V(:,j-1), Beta(j));
-            
-            deltaj_prev = Delta(j);
-            Delta(j) = c*deltaj_prev + s*Alpha(j);
-            Gamma(j) = s*deltaj_prev - c*Alpha(j);
-            Epsilon(j+1) = s*Beta(j+1);
-            Delta(j+1) = -c*Beta(j+1);
-        
-            shi = Gamma(j)^2 + Beta(j+1)^2 + sign(Gamma(j))*Gamma(j)*sqrt(Gamma(j)^2+Beta(j+1)^2);
-            c = ((Beta(j+1)^2)/shi)-1;
-            s = -(Beta(j+1)*(Gamma(j) + sign(Gamma(j))*sqrt(Gamma(j)^2+Beta(j+1)^2)))/shi;
-            
-            phi_prev = phi;
-            phi  = s*phi_prev;
-            t(j) = c*phi_prev;
-        
-            Gamma(j) = c*Gamma(j) + s*Beta(j+1);
-            % R(:,j) is now completed
-        
-            d(:,j) = (V(:,j)-Delta(j)*d(:,j-1))/Gamma(j);
-        
-            xj = x_prev + t(j)*d(:,j);
-        
-            for j = 3:k
-        
-            x_prev = xj;
-        
-            [Alpha(j), Beta(j+1), V(:,j+1)] = lanczosProcess3(A, V(:,j), V(:,j-1), Beta(j));
-            
-            deltaj_prev = Delta(j);
-            Delta(j) = c*deltaj_prev + s*Alpha(j);
-            Gamma(j) = s*deltaj_prev - c*Alpha(j);
-            Epsilon(j+1) = s*Beta(j+1);
-            Delta(j+1) = -c*Beta(j+1);
-        
-            shi = Gamma(j)^2 + Beta(j+1)^2 + sign(Gamma(j))*Gamma(j)*sqrt(Gamma(j)^2+Beta(j+1)^2);
-            c = ((Beta(j+1)^2)/shi)-1;
-            s = -(Beta(j+1)*(Gamma(j) + sign(Gamma(j))*sqrt(Gamma(j)^2+Beta(j+1)^2)))/shi;
-            
-            phi_prev = phi;
-            phi  = s*phi_prev;
-            t(j) = c*phi_prev;
-        
-            Gamma(j) = c*Gamma(j) + s*Beta(j+1);
-            % R(:,j) is now completed
-        
-            d(:,j) = (V(:,j)-Delta(j)*d(:,j-1)-Epsilon(j)*d(:,j-2))/Gamma(j);
-        
-            xj = x_prev + t(j)*d(:,j); 
-            end
-        
-            x_k = xj;
 
-            res = norm(A*x_k-b)/norm(b);
+            [Alpha, Beta, v_0, V, c, s, Gamma, Delta, Epsilon, phi, d, x0, t] = MINRES_Pol.init(k, n, b, A);   
+
+            j = 1;
+            
+            x_prev = x0;
+
+            if (norm(A*x_prev-b)>tol)
+
+            [Alpha(j), Beta(j+1), V(:,j+1)] = MINRES_Pol.lanczosProc(A, V(:,j), v_0, Beta(j));
+
+            deltaj_prev = Delta(j); 
+
+            Delta(j)        =  c*deltaj_prev + s*Alpha(j);
+            Gamma(j)        =  s*deltaj_prev - c*Alpha(j);
+            Epsilon(j+1)    =  s*Beta(j+1);
+            Delta(j+1)      = -c*Beta(j+1);
+
+            [c, s, Gamma(j)] = MINRES_Pol.computeH(Beta, Gamma(j), j);
+            % R(:,j) is now completed
+            phi_prev = phi;
+            phi      = s*phi_prev;
+            t(j)     = c*phi_prev;
+            d(:,j)   = V(:,j)/Gamma(j);
+
+            x1 = x_prev + t(j)*d(:,j);
+
+            j = 2;
+            x_prev = x1;
+
+            [Alpha(j), Beta(j+1), V(:,j+1)] = MINRES_Pol.lanczosProc(A, V(:,j), V(:,j-1), Beta(j));
+
+            deltaj_prev = Delta(j); 
+
+            Delta(j)        =  c*deltaj_prev + s*Alpha(j);
+            Gamma(j)        =  s*deltaj_prev - c*Alpha(j);
+            Epsilon(j+1)    =  s*Beta(j+1);
+            Delta(j+1)      = -c*Beta(j+1);
+
+            [c, s, Gamma(j)] = MINRES_Pol.computeH(Beta, Gamma(j), j);
+            % R(:,j) is now completed            
+            phi_prev = phi;
+            phi      = s*phi_prev;
+            t(j)     = c*phi_prev;
+            d(:,j)   = (V(:,j)-Delta(j)*d(:,j-1))/Gamma(j);
+
+            x2 = x_prev + t(j)*d(:,j);          
+            
+            
+            xj = x2;
+            
+
+            j=3;
+            res = abs(phi);
+%             for j = 3:k
+            while (j<maxiter)&&(res>tol)
+                x_prev = xj;
+
+                [Alpha(j), Beta(j+1), V(:,j+1)] = MINRES_Pol.lanczosProc(A, V(:,j), V(:,j-1), Beta(j));
+
+                deltaj_prev = Delta(j); 
+
+                Delta(j)        =  c*deltaj_prev + s*Alpha(j);
+                Gamma(j)        =  s*deltaj_prev - c*Alpha(j);
+                Epsilon(j+1)    =  s*Beta(j+1);
+                Delta(j+1)      = -c*Beta(j+1);
+
+                [c, s, Gamma(j)] = MINRES_Pol.computeH(Beta, Gamma(j), j);            % R(:,j) is now completed    
+
+                phi_prev    = phi;
+                phi         = s*phi_prev;
+                t(j)        = c*phi_prev;
+                d(:,j)      = (V(:,j)-Delta(j)*d(:,j-1)-Epsilon(j)*d(:,j-2))/Gamma(j);
+
+                xj = x_prev + t(j)*d(:,j);
+
+                res = abs(phi);
+                j=j+1;
+            end
+            x_prev = xj;
+            end
+
+            x_k = x_prev;
+            xNew = x_k;
+
+
+            iter = load("CounterPol\iter.mat").iter;
+            neededIter = load("CounterPol\neededIter.mat").neededIter;
+            solutions = load("CounterPol\solutions.mat").solutions; 
+            
+            solutions(:,iter) = xNew;
+            neededIter(iter) = j-1; 
+            iter = iter + 1;
+            
+            save('CounterPol\iter.mat', 'iter');
+            save('CounterPol\neededIter.mat', 'neededIter');
+            save('CounterPol\solutions.mat', 'solutions');
+
+            save('CounterPol\xNew.mat', 'xNew');
+
+            toc
         end
-      end
+
+
+
+
+
+
+
+
+        function [alpha, beta_new, v_new] = lanczosProc(A, vj, v_prev, betaj)
+            z       = A*vj;
+            alpha   = (z - v_prev*betaj)'*vj;
+            w       = z - v_prev*betaj-vj*alpha;
+            beta_new = norm(w);
+            v_new    = w/beta_new;
+        end
+
+        function [Alpha, Beta, v_0, V, c, s, Gamma, Delta, Epsilon, phi, d, x0, t] = init(k, n, b, A)
+            x0  = load('CounterPol\xNew.mat').xNew;
+%             x0 = zeros(20200,1);
+            initialRes = b-A*x0;
+
+            Alpha   = zeros(k+1,1);
+            Beta    = zeros(k+1,1);
+            Beta(1) = norm(initialRes);
+            V       = zeros(n,k);
+            v_0     = 0;
+            
+
+            V(:,1)  = initialRes/Beta(1);
+
+            c       = -1;
+            s       = 0;
+
+            Gamma    = zeros(k,1);
+            Delta    = zeros(k,1);
+            Epsilon  = zeros(k,1);
+            Gamma(1) = 0;
+
+            phi = Beta(1);
+            d   = zeros(n,k);
+            
+
+            t   = zeros(k,1);
+        end
+
+        function  [c, s, Gamma_j] = computeH(Beta, Gamma_j, j)
+            shi     = Gamma_j^2 + Beta(j+1)^2 + sign(Gamma_j)*Gamma_j*sqrt(Gamma_j^2+Beta(j+1)^2);
+            c       = ((Beta(j+1)^2)/shi)-1;
+            s       = -(Beta(j+1)*(Gamma_j + sign(Gamma_j)*sqrt(Gamma_j^2+Beta(j+1)^2)))/shi;
+
+            Gamma_j = c*Gamma_j + s*Beta(j+1);
+        end
+    end
 end
