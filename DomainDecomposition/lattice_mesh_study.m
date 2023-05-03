81be1504c169b7599b2af982fa9b3533af3e6506
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2023-05-03 09:41:50 +0100

    "small change"

--- DomainDecomposition/lattice_mesh_study.m
+++ DomainDecomposition/lattice_mesh_study.m
@@ -3,10 +3,10 @@ close all
 
 filename='lattice_ex1';
 a.fileName=filename;
-m=FemDataContainer(a);
+femD = FemDataContainer(a);
 
-boundary_mesh=m.mesh.createBoundaryMesh();
-figure
+boundary_mesh=femD.mesh.createBoundaryMesh();
+figure(1)
 for ib=1:length(boundary_mesh)
     bm=boundary_mesh{ib};
     bm.mesh.plot
@@ -16,25 +16,30 @@ end
 %% Repeat cells
 
 nrve_dir=[3 3]; %number repetition x and y
-delta= [max(m.mesh.coord(:,1)) max(m.mesh.coord(:,2)) ];
+delta= [max(femD.mesh.coord(:,1)) max(femD.mesh.coord(:,2)) ];
 % dom_mesh(1,1:nrve_dir(1))=m;
 % dom_mesh(2,1:nrve_dir(2))=m;
-coord=m.mesh.coord;
-connec=m.mesh.connec;
-m_aux=m;
-figure
+coord0  = femD.mesh.coord;
+connec0 = femD.mesh.connec;
+figure(2)
 for jdom=1:nrve_dir(2)
     %extrude in x
     for idom=1:nrve_dir(1)
-        aux_coord(:,1)=coord(:,1)+delta(1)*(idom-1);
-        aux_coord(:,2)=coord(:,2)+delta(2)*(jdom-1);
-        aux_connec=connec+m.mesh.nnodes*(nrve_dir(1)*(jdom-1)+idom-1);
-        m_aux.mesh.setCoord(aux_coord)
-        m_aux.mesh.setConnec(aux_connec)
-%         m_aux.mesh.plot
-        plot(aux_coord(:,1),aux_coord(:,2),'.')
+        coordIJ(:,1) = coord0(:,1)+delta(1)*(idom-1);
+        coordIJ(:,2) = coord0(:,2)+delta(2)*(jdom-1);            
+        newConnec    = femD.mesh.nnodes*(nrve_dir(1)*(jdom-1)+idom-1);
+        connecIJ     = connec0 + newConnec;
+        s.coord  = coordIJ;
+        s.connec = connec0;
+        m = Mesh(s);
+        m.plot();
         hold on
-        domain{jdom,idom}=m_aux;
+      %  m_aux.mesh.setCoord(aux_coord)
+      %  m_aux.mesh.setConnec(aux_connec)
+%         m_aux.mesh.plot
+      %  plot(aux_coord(:,1),aux_coord(:,2),'.')
+      %  hold on
+        meshes{jdom,idom}=m;
 %         domain2(nrve_dir(1)*(jdom-1)+idom)=m_aux;
 %         dom_mesh(jdom,idom).coord(:,1)=m_aux.mesh.coord(:,1)+delta(1)*(idom-1);
 %         dom_mesh(jdom,idom).coord(:,2)=m_aux.mesh.coord(:,2)+delta(1)*(jdom-1);
@@ -55,23 +60,23 @@ end
 %% rpeat cells joining domains
 
 nrve_dir=[3 3]; %number repetition x and y
-delta= [max(m.mesh.coord(:,1)) max(m.mesh.coord(:,2)) ];
+delta= [max(femD.mesh.coord(:,1)) max(femD.mesh.coord(:,2)) ];
 % dom_mesh(1,1:nrve_dir(1))=m;
 % dom_mesh(2,1:nrve_dir(2))=m;
-coord=m.mesh.coord;
-connec=m.mesh.connec;
-m_aux=m;
+coord0=femD.mesh.coord;
+connec0=femD.mesh.connec;
+m_aux=femD;
 figure
 for jdom=1:nrve_dir(2)
     %extrude in x
     for idom=1:nrve_dir(1)
-        aux_coord(:,1)=coord(:,1)+delta(1)*(idom-1);
-        aux_coord(:,2)=coord(:,2)+delta(2)*(jdom-1);
-        aux_connec=connec+m.mesh.nnodes*(nrve_dir(1)*(jdom-1)+idom-1);
-        m_aux.mesh.setCoord(aux_coord)
-        m_aux.mesh.setConnec(aux_connec)
+        coordIJ(:,1)=coord0(:,1)+delta(1)*(idom-1);
+        coordIJ(:,2)=coord0(:,2)+delta(2)*(jdom-1);
+        conbecIJ=connec0+femD.mesh.nnodes*(nrve_dir(1)*(jdom-1)+idom-1);
+        m_aux.mesh.setCoord(coordIJ)
+        m_aux.mesh.setConnec(conbecIJ)
 %         m_aux.mesh.plot
-        plot(aux_coord(:,1),aux_coord(:,2),'.')
+        plot(coordIJ(:,1),coordIJ(:,2),'.')
         hold on
         domain{jdom,idom}=m_aux;
 %         domain2(nrve_dir(1)*(jdom-1)+idom)=m_aux;
@@ -86,7 +91,7 @@ end
 
 %extrude in x
 for idom=1:nrve_dir(1)
-    m_aux=m;
+    m_aux=femD;
 %     m_aux.mesh.coord(:,1)=m_aux.mesh.coord(:,1)+delta(1);
 %     m_aux.mesh.conec=m_aux.mesh.connec+nnode;
 %     dom_mesh(idom)=m_aux;
@@ -97,7 +102,7 @@ end
 
 % extrude in y
 for jdom=2:nrve_dir(2)
-    m_aux=m;
+    m_aux=femD;
 %     m_aux.mesh.coord(:,1)=m_aux.mesh.coord(:,1)+delta(1);
 %     m_aux.mesh.conec=m_aux.mesh.connec+nnode;
 %     dom_mesh(idom)=m_aux;
