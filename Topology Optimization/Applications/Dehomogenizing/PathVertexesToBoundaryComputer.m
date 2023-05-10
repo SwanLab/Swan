e20c04719cd1baa193d5608811f7af3482dcf5b8
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2023-05-10 16:50:28 +0100

    "small ref"

--- Topology Optimization/Applications/Dehomogenizing/PathVertexesToBoundaryComputer.m
+++ Topology Optimization/Applications/Dehomogenizing/PathVertexesToBoundaryComputer.m
@@ -66,6 +66,7 @@ classdef PathVertexesToBoundaryComputer < handle
             [~,iM] = min(dist);
             node = nodes(iM);
             cP = obj.mesh.coord(node,:);
+           % obj.boundaryPointCoord = [1.98    1.3500];
            obj.boundaryPointCoord = cP;
             %obj.boundaryPointCoord(:,2)
         end
