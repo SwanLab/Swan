c6974d58c478f19ecd46aa057b31a28878d8e6da
Laura <laura.dominguez.perez@estudiantat.upc.edu>
2023-05-26 20:24:25 +0200

    small changes
    Petites anotacions explicatives del que es fa a cada part

--- ExampleShapeDeriv.m
+++ ExampleShapeDeriv.m
@@ -1,35 +1,42 @@
-function ExampleShapeDeriv
+% function ExampleShapeDeriv
 
+addpath(genpath(fileparts(mfilename('fullpath')))) ;
+
+
+%%%% Trec les propietats de la malla a partir del FemDataContainer %%%%
 file = 'CantileverBeam_Triangle_Linear';
 a.fileName = file;
 s = FemDataContainer(a);
 
 mesh = s.mesh;
-mesh.plot()
+% mesh.plot()
 
-fem = FEM.create(s);
-fem.solve();
-u = fem.uFun;
-u.plot
-sP.filename = 'Example1';
-sP.type = 'GiD';
-u.print(sP)
+% fem = FEM.create(s);
+% fem.solve();
+% u = fem.uFun;
+% u.plot
+% sP.filename = 'Example1';
+% sP.type = 'GiD';
+% u.print(sP)
 
-
+%%%% Defineixo la funció analítica (el funcional F(Omega)) %%%%
 sAF.fHandle = @(x) (x(1,:,:)-1).^2 + (x(2,:,:)-0.5).^2 -0.5;
 sAF.ndimf   = 1;
 sAF.mesh    = mesh;
 xFun = AnalyticalFunction(sAF);
 
-
+%%%% Defineixo la quadratura de la malla %%%%
 quad = Quadrature.set(s.mesh.type);
+% Trec les dades de la quadratura de gauss (ngaus, weigp, posgp)
 quad.computeQuadrature('LINEAR');
 
+% Avaluo la funció analítica a tota la malla, als punts 
 fG  = xFun.evaluate(quad.posgp);
+% Calculo el diferencial de volum de cada element de la malla
 dVG = mesh.computeDvolume(quad);
-for iF = 1:xFun.ndimf
-intF(iF) = sum(squeeze(fG(iF,:,:)).*dVG');
-end
+% Calculo la integral de la funció analítica del funcional F(Omega)
+intF = sum(squeeze(fG(:,:,:)).*dVG(:)) ;
+
 
 sP.mesh = mesh;
 sP.connec = mesh.connec;
@@ -43,4 +50,4 @@ projP1 = Projector_toP1(pp1);
 p1fun = projP1.project(xFun);
 p1fun.plot();
 
-end
+% end
