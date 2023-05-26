be917c868363a3fec50411c9992e7721b82f875a
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2023-05-26 16:24:29 +0100

    "small changes"

--- Input/test_cantilever2.m
+++ Input/test_cantilever2.m
@@ -1,17 +1,17 @@
-filename = 'Cantileverbeam_Quadrilateral_Bilinear';%CantileverArnau2 %Cantilever';%'CantileverBeam_Triangle_Linear';
+filename = 'CantileverBeam_Triangle_Linear_Fine';%CantileverArnau2 %Cantilever';%'CantileverBeam_Triangle_Linear';
 ptype = 'MACRO';
 method = 'SIMP_P3';
 materialType = 'ISOTROPIC';
-initial_case = 'holes';%'holes';
+initial_case = 'full';%'holes';
 cost = {'compliance'};
 weights = [1];
 constraint = {'volumeConstraint'};
 constraint_case = {'EQUALITY'};
-optimizerUnconstrained = 'HAMILTON-JACOBI';%'SLERP';%'PROJECTED GRADIENT';%'PROJECTED GRADIENT'; 
-optimizer = 'AlternatingPrimalDual';%'DualNestedInPrimal';'DualNestedInPrimal';%'AlternatingPrimalDual';%'AlternatingPrimalDual';
+optimizerUnconstrained = 'SLERP';%'SLERP';%'PROJECTED GRADIENT';%'PROJECTED GRADIENT'; 
+optimizer = 'NullSpace';%'DualNestedInPrimal';'DualNestedInPrimal';%'AlternatingPrimalDual';%'AlternatingPrimalDual';
 incrementFactor = 1;
 designVariable = 'LevelSet';%'Density';'LevelSet'
-filterType = 'P1';
+filterType = 'PDE';
 nsteps = 1;
 Vfrac_final = 0.4;
 optimality_final =1e-3;
@@ -30,9 +30,9 @@ TOL.nu_plus = 1/3;
 TOL.nu_minus = 1/3;
 
 % For all tests
-plotting = false;
+plotting = true;
 printing = false;
 printing_physics = false;
 monitoring = false;
 monitoring_interval = 1;
-maxiter = 3;
+maxiter = 300;
