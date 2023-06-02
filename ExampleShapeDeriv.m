32b528df2deb9e44ebb249fab78002691cbc79de
Laura <laura.dominguez.perez@estudiantat.upc.edu>
2023-06-02 19:24:59 +0200

    changes

--- ExampleShapeDeriv.m
+++ ExampleShapeDeriv.m
@@ -22,14 +22,14 @@ sP.type = 'GiD';
 % u.print(sP)
 
 
-%%%% Defineixo la funció analítica (el funcional F(Omega)) %%%%
+%%%% Defineixo la funció analítica (el funcional F'(Omega)(Theta)) %%%%
 % sAF.fHandle = @(x) (x(1,:,:)-1).^2 + (x(2,:,:)-0.5).^2 -0.5;
 % sAF.ndimf   = 1;
 % sAF.mesh    = mesh;
 % xFun = AnalyticalFunction(sAF);
 
 sAF.fHandle = @(x) [2*(x(1,:,:)-1); 2*(x(2,:,:)-0.5)];
-sAF.ndimf   = 1;
+sAF.ndimf   = 2;
 sAF.mesh    = mesh;
 xFun = AnalyticalFunction(sAF);
 
@@ -43,7 +43,6 @@ fG  = xFun.evaluate(quad.posgp);
 % Calculo el diferencial de volum de cada element de la malla
 dVG = mesh.computeDvolume(quad);
 % Calculo la integral de la funció analítica del funcional F(Omega)
-
 intF = sum(squeeze(fG(:,:,:)).*dVG(:)') ;
 
 %%%% Trobo la funció g que fa que gTheta=F'(Omega)(Theta) %%%%
@@ -54,7 +53,7 @@ g = h.project(xFun) ;
 % g.plot() ;
 
 newCoord = zeros(length(mesh.coord),2) ;
-delta = 1e-5;
+delta = 0.001;
 convergeix = 0 ;
 it = 0 ;
 coordAsterisco=sAF.mesh.coord ;
@@ -62,8 +61,8 @@ coordAsterisco=sAF.mesh.coord ;
 while convergeix == 0
     newCoord = coordAsterisco;
     for i = 1:length(mesh.coord)
-    newCoord(i,1) = newCoord(i,1) - g.fValues(i)*0.1 ;
-    newCoord(i,2) = newCoord(i,2) - g.fValues(i)*0.1 ;
+    newCoord(i,1) = newCoord(i,1) - g.fValues(i,1)*0.001 ;
+    newCoord(i,2) = newCoord(i,2) - g.fValues(i,2)*0.001 ;
     end  
     a.connec = mesh.connec ;
     a.coord = newCoord ;
@@ -85,8 +84,8 @@ a.connec = mesh.connec ;
 a.coord = newCoord ;
 m = Mesh(a) ;
 m.plot() ;
-xlim([-10,10])
-ylim([-10,10])
+% xlim([-10,10])
+% ylim([-10,10])
 
 
 
