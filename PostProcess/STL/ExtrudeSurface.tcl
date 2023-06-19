05f78f5ec3ca0af2e071ba2fc074ab15c36ffe5f
tcreusc <toncc3d@gmail.com>
2023-06-19 13:32:57 +0200

    make normals coherent

--- PostProcess/STL/ExtrudeSurface.tcl
+++ PostProcess/STL/ExtrudeSurface.tcl
@@ -4,5 +4,6 @@ proc ExtrudeSurface {arg1} {
     GiD_Process Mescape Files Read $gidProjectName 
     GiD_Process Mescape Utilities Copy Surfaces DoExtrude Surfaces MaintainLayers Translation FNoJoin 0.0,0.0,0.0 FNoJoin 0.0,0.0,0.16 InvertSelection escape
     GiD_Process Mescape Geometry Create volume 1:end escape escape
+    GiD_Process Mescape Utilities SwapNormals Surfaces MakeGroupCoherent 1:end escape Yes escape
     GiD_Process Mescape Files Save -alsoresults:1 -- geoversion:current $gidProjectName
 }
