8070ff64b983205df13001882e9eae42eb1448b7
Laura <laura.dominguez.perez@estudiantat.upc.edu>
2023-06-19 08:03:51 +0200

    intent PDE

--- PDEShapeDerivative.m
+++ PDEShapeDerivative.m
@@ -5,16 +5,27 @@ classdef PDEShapeDerivative < handle
         temperatureTarget
         temperature
         adjoint
+
+        quadrature
+        tolerance
+        cost
+        projector
+
     end
     
     methods (Access = public)
         
         function obj = PDEShapeDerivative()
+            addpath(genpath(fileparts(mfilename('fullpath'))))
             obj.init();  
             obj.createMesh();                        
             obj.createTemperatureTarget();
             obj.computeTemperature();
             obj.computeAdjoint();
+            obj.createFunction() ;
+            obj.createQuadrature();
+            obj.createProjector();
+            obj.compute()
         end
         
     end
@@ -67,9 +78,9 @@ classdef PDEShapeDerivative < handle
             s.rhsFun = obj.createFunctionRHS();
             fem = FEM.create(s);
             fem.solve(); 
-            %fem.print('asa') 
+            fem.print('asa') 
              obj.temperature = fem.temperature;
-             obj.temperature.plot()
+             % obj.temperature.plot()
         end
 
         function computeAdjoint(obj)
@@ -82,7 +93,7 @@ classdef PDEShapeDerivative < handle
             fem.solve();  
             p = fem.temperature;
             obj.adjoint = p;
-            obj.adjoint.plot()
+            % obj.adjoint.plot()
         end
 
         function f = createRHSFunction(obj)
@@ -101,8 +112,105 @@ classdef PDEShapeDerivative < handle
             obj.temperatureTarget = t;
         end
 
+        function f = createFunction(obj)
+            %%PDE%%
+            u = obj.temperature ;
+            u_t = obj.temperatureTarget ;
+            s.fHandle = @(x) (u - u_t).^2 ;
+                        
+            s.ndimf   = 1;
+            s.mesh    = obj.mesh;
+            f = AnalyticalFunction(s);
+        end
 
+        %%%%%%
+        % function dfC = computeDerivative(obj)
+        %     df{1} = @(x) 6*( (x(1,:,:)-1).^2 + (x(2,:,:)-0.5).^2 ).^2.*(x(1,:,:)-1) - 8*(x(1,:,:)-1).*(x(2,:,:)-0.5).^2 ;
+        %     df{2} = @(x) 6*( (x(1,:,:)-1).^2 + (x(2,:,:)-0.5).^2 ).^2.*(x(2,:,:)-0.5) - 8*(x(2,:,:)-0.5).*(x(1,:,:)-1).^2 ;
+        %     for i = 1:length(df)
+        %         s.fHandle = df{i};
+        %         s.ndimf   = 1;
+        %         s.mesh    = obj.mesh;
+        %         dfC{i} = AnalyticalFunction(s);
+        %     end
+        % end
 
+        function c = computeCost(obj)
+            f    = obj.createFunction();
+            fG   = f.evaluate(obj.quadrature.posgp);
+            dVG  = obj.mesh.computeDvolume(obj.quadrature);
+            fG   = squeeze(fG);
+            c    = fG.*dVG;
+            c = sum(c(:));
+        end
+
+        function compute(obj)
+            obj.cost(1) = obj.computeCost();
+            tau = 0.02;
+            hasNotConverged = true;
+            iter = 2;
+            while hasNotConverged 
+                f  = obj.temperature ;
+                df = obj.adjoint;                
+                g = obj.projector.project(obj.mesh,f,df);
+                obj.updateMesh(g,tau);
+                cTrial =  obj.computeCost();                
+                if cTrial < obj.cost(iter-1)
+                    obj.cost(iter) = cTrial;
+                    tau = 1.1*tau;
+                    incC = abs(obj.cost(iter) - obj.cost(iter-1))/abs(obj.cost(iter));
+                    hasNotConverged = incC > obj.tolerance;  
+                    iter   = iter + 1 ;
+                    obj.plotMesh();
+                    obj.plotCost();
+                   % obj.remesh();
+                else 
+                    tau  = tau/2;
+                    if tau < obj.tolerance
+                      hasNotConverged = false;
+                    else
+                      hasNotConverged = true;
+                    end
+                end
+            end
+          end
+
+        function updateMesh(obj,g,tau)
+            coord = obj.mesh.coord;
+            nCoord(:,1) = coord(:,1) - g.fValues(:,1)*tau ;
+            nCoord(:,2) = coord(:,2) - g.fValues(:,2)*tau ;
+            s.connec = obj.mesh.connec;
+            s.coord  = nCoord;
+            m = Mesh(s);
+            obj.mesh =  m;
+        end
+
+        function q = createQuadrature(obj)
+            q = Quadrature.set(obj.mesh.type);
+            q.computeQuadrature('CUBIC');
+            obj.quadrature = q;
+        end     
+
+        function createProjector(obj)
+            p = ShapeDerivProjector();
+            obj.projector = p;
+        end
+        
+        function plotMesh(obj)
+            figure(1)
+            clf
+            obj.mesh.plot()
+            %newMesh = obj.remesh(newMesh);
+            % axis([0 2 -0.3 1.4])
+             axis([-0.25 2.25 -0.25 1.25])
+            %axis([0 2 -0.5 1.3])
+        end
+
+        function plotCost(obj)
+            figure(2)
+            plot(obj.cost)
+        end
+        
     end
     
 end
