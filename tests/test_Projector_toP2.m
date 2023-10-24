3c771f8c16fb156d2910b39ed24347883277f45e
tarrut <tarrut.code@gmail.com>
2023-10-24 17:58:28 +0200

    Validation of the P1 and P2 projections

--- tests/test_Projector_toP2.m
+++ tests/test_Projector_toP2.m
@@ -1,36 +1,85 @@
-sM.coord = [0 0;1 0;0 1];
-sM.connec = [1 2 3];
+% Reset Workspace
+clear
+close all
+clc
 
-m = Mesh(sM);
-mesh = m;
+% Define number of elements
+N = [8 16 32 64];
 
-sAF.fHandle = @(x) x(1,:,:); % f(x) = x
-sAF.ndimf   = 1; % number of dimensions
-sAF.mesh    = mesh;
-xFun = AnalyticalFunction(sAF);
+% Prepare analytical function
+sAF.fHandle = @(x) sin(x(1,:,:)*(2*pi)); % f(x) = sin(2*pi*x)
+sAF.ndimf   = 1;
 
+% Variables to store the error
+e_p1 = zeros(size(N));
+e_p2 = zeros(size(N));
 
-
-
-p2fun = xFun.project('P2');
-p2fun.plot()
-
-
-xFun.evualaute(xG);
-p2.evaluate(xG);
-
-for iH = 1:nH
-for iOrder = 1:2
+for i = 1:length(N)
+    % Create the mesh of N(i) elements
+    sAF.mesh = createMesh(N(i));
     
-        createMesh()
-
-
-(xFun - p2).^2
-
+    % Define the analytical function for the selected mesh
+    xFun = AnalyticalFunction(sAF);
+    
+    % Project to P1 and P2
+    p1fun = xFun.project('P1');
+    p2fun = xFun.project('P2');
+    
+    % Define the quadrature to integrate the norm
+    xG = defineGaussPoints(sAF.mesh);
+    
+    % Evaluate the analytical function, and the projections in the Gauss
+    % points
+    f = xFun.evaluate(xG);
+    p1 = p1fun.evaluate(xG);
+    p2 = p2fun.evaluate(xG);
+    
+    % Compute the error with the norm of L^2
+    e_p1(i) = sum(sum((f - p1).^2));
+    e_p2(i) = sum(sum((f - p2).^2));
 end
 
+% ----------------------------------------------------------------------- %
+% PLOTS
+% ----------------------------------------------------------------------- %
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+set(groot,'defaultAxesTickLabelInterpreter','latex');
+figure()
+loglog(1./N,e_p1,'-x')
+hold on
+loglog(1./N,e_p2,'-x')
+loglog(1./N,1./N.^2,'-x')
+loglog(1./N,1./N.^4,'-x')
+grid on
+legend('P1 error','P2 error','$h^2$','$h^4$','Location','best')
+xlabel('h')
+ylabel('Error')
+title('P1 and P2 Projections Validation')
+
+% ----------------------------------------------------------------------- %
+% FUNCTIONS
+% ----------------------------------------------------------------------- %
+function m = createMesh(N)
+    % Defines a 2D squared mesh of triangles of N nodes by side in a domain
+    % between 0 and 1 in both axes
+    x1 = linspace(0,1,N);
+    x2 = linspace(0,1,N);
+    [xv,yv] = meshgrid(x1,x2);
+    [F,V] = mesh2tri(xv,yv,zeros(size(xv)),'x');
+    sBg.coord  = V(:,1:2);
+    sBg.connec = F;
+    m = Mesh(sBg);
+end
+
+function xG = defineGaussPoints(mesh)
+    % Returns the gauss points for an integration of order4 for the given
+    % mesh 
+    quad = Quadrature.set(mesh.type);
+    quad.computeQuadrature('ORDER4');
+    xG = quad.posgp;
+end
 
 %ToDo
 %1. Merge P1 and P2 by LagrangianFeFunction and check tests (AllTests)
 %using Dofs name
-%2. Convergence analysis for P1 and P2 in Tutorial
