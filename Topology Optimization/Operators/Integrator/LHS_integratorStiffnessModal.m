2ba659806e082c4829b19d10a9a168802372ebf8
FerrerFerreAlex <aferrer@cimne.upc.edu>
2023-10-27 14:30:46 +0200

    Create LHS_integratorStiffnessModal.m

---
+++ Topology Optimization/Operators/Integrator/LHS_integratorStiffnessModal.m
@@ -0,0 +1,32 @@
+classdef LHS_integratorStiffnessModal < handle
+    
+    properties (Access = public)
+        
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    methods (Access = public)
+        
+        function obj = LHS_integratorStiffnessModal(cParams)
+            obj.init(cParams)
+            
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function init(obj,cParams)
+            
+        end
+        
+    end
+    
+end
