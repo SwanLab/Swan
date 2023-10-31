7065e189a201870bb7bf38b9de226be61f14f03a
jose-antonio-torres <jtorreslerma@icloud.com>
2023-10-31 12:54:59 +0100

    creating more geometrical fun examples

--- testingGeometricalFunctions.m
+++ testingGeometricalFunctions.m
@@ -1,6 +1,9 @@
 clear
+close all
 
-file = 'test2d_triangle';
+% Circle
+
+file = 'Circle_Triangle_Linear_Unstructured';
 a.fileName = file;
 ff = FemDataContainer(a);
 mesh = ff.mesh;
@@ -20,3 +23,92 @@ ss.backgroundMesh = mesh;
 ss.boundaryMesh   = bMesh;
 uMesh             = UnfittedMesh(ss);
 uMesh.compute(ls.fValues);
+uMesh.plot();
+
+
+
+
+clear
+
+% Square
+
+file = 'Circle_Triangle_Linear_Unstructured';
+a.fileName = file;
+ff = FemDataContainer(a);
+mesh = ff.mesh;
+bMesh = mesh.createBoundaryMesh();
+
+coor = mesh.coord;
+
+s.type='Square';
+s.length=0.6*(max(coor(:,1))-min(coor(:,1)));
+s.xCoorCenter=0.5*(max(coor(:,1))-min(coor(:,1)));
+s.yCoorCenter=0.5*(max(coor(:,2))-min(coor(:,2)));
+g=GeometricalFunction(s);
+
+ls=g.computeLevelSetFunction(mesh);
+
+ss.backgroundMesh = mesh;
+ss.boundaryMesh   = bMesh;
+uMesh             = UnfittedMesh(ss);
+uMesh.compute(ls.fValues);
+figure
+uMesh.plot();
+
+
+
+clear
+
+% Fiber
+
+file = 'Circle_Triangle_Linear_Unstructured';
+a.fileName = file;
+ff = FemDataContainer(a);
+mesh = ff.mesh;
+bMesh = mesh.createBoundaryMesh();
+
+coor = mesh.coord;
+
+s.type='VerticalFiber';
+s.width=0.3*(max(coor(:,1))-min(coor(:,1)));
+s.xCoorCenter=0.5*(max(coor(:,1))-min(coor(:,1)));
+g=GeometricalFunction(s);
+
+ls=g.computeLevelSetFunction(mesh);
+
+ss.backgroundMesh = mesh;
+ss.boundaryMesh   = bMesh;
+uMesh             = UnfittedMesh(ss);
+uMesh.compute(ls.fValues);
+figure
+uMesh.plot();
+
+
+
+
+clear
+
+% Vertical N-Fibers
+
+file = 'Circle_Triangle_Linear_Unstructured';
+a.fileName = file;
+ff = FemDataContainer(a);
+mesh = ff.mesh;
+bMesh = mesh.createBoundaryMesh();
+
+coor = mesh.coord;
+
+s.type='VerticalNFibers';
+s.nFibers=5;
+s.minxCoor=min(coor(:,1));
+s.maxxCoor=max(coor(:,1));
+g=GeometricalFunction(s);
+
+ls=g.computeLevelSetFunction(mesh);
+
+ss.backgroundMesh = mesh;
+ss.boundaryMesh   = bMesh;
+uMesh             = UnfittedMesh(ss);
+uMesh.compute(ls.fValues);
+figure
+uMesh.plot();
