13830a9d6fa7c1a70f274274addd86941b3f00fc
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2023-11-06 17:55:01 +0100

    generalizing P1

--- Topology Optimization/Filters/FilterP1.m
+++ Topology Optimization/Filters/FilterP1.m
@@ -2,16 +2,19 @@ classdef FilterP1 < handle
 
     properties (Access = private)
         mesh
+        filteredField
         Poper
-        quadrature
+        M
+        I
     end
 
     methods (Access = public)
 
         function obj = FilterP1(cParams)
             obj.init(cParams);
-            obj.createQuadrature(cParams);
             obj.createPoperator();
+            obj.createMassMatrix();
+            obj.createSupportMatrix();
         end
 
         function xReg = getP1Function(obj,fun,quadType)
@@ -38,14 +41,16 @@ classdef FilterP1 < handle
         function xReg = compute(obj,fun,quadType)
             test       = P1Function.create(obj.mesh, 1);
             int        = obj.computeRHSintegrator(quadType);
-            P          = obj.Poper.value;
+            Iki        = obj.I;
+            sM         = sum(obj.M,2);
+            den        = Iki*sM;
+            P          = Iki./den;
             A          = P;
             b          = int.compute(fun,test);
             xR         = A*b;
             p.fValues  = xR;
             p.mesh     = obj.mesh;
-            xRegP0     = P0Function(p);
-            xReg       = xRegP0.project('P1');
+            xReg       = P1Function(p);
         end
 
     end
@@ -53,13 +58,8 @@ classdef FilterP1 < handle
     methods (Access = private)
 
         function init(obj,cParams)
-            obj.mesh = cParams.mesh;
-        end
-
-        function createQuadrature(obj,cParams)
-            q = Quadrature.set(obj.mesh.type);
-            q.computeQuadrature(cParams.quadType);
-            obj.quadrature = q;
+            obj.mesh          = cParams.mesh;
+            obj.filteredField = P1Function.create(obj.mesh,1); % trial will come from outside
         end
 
         function createPoperator(obj)
@@ -67,24 +67,42 @@ classdef FilterP1 < handle
             obj.Poper = Poperator(s);
         end
 
+        function createMassMatrix(obj)
+            s.type  = 'MassMatrix';
+            s.mesh  = obj.mesh;
+            s.test  = P1Function.create(obj.mesh, 1);
+            s.trial = P1Function.create(obj.mesh, 1);
+            s.quadratureOrder = 'QUADRATICMASS';
+            LHS   = LHSintegrator.create(s);
+            obj.M = LHS.compute();
+        end
+
+        function createSupportMatrix(obj)
+            connecField = obj.filteredField.computeDofConnectivity;
+            connecP1    = obj.mesh.connec';
+            nelem       = obj.mesh.nelem;
+            nnodes      = obj.mesh.nnodes;
+            nDofs       = max(connecField, [], 'all');
+            nodesElem   = obj.mesh.nnodeElem;
+            nDofElem    = size(connecField,1);
+            T           = zeros(nDofs,nnodes);
+            for ielem = 1:nelem
+                for kdof = 1:nDofElem
+                    for inode = 1:nodesElem
+                        dofs  = connecField(kdof,ielem);
+                        nodes = connecP1(inode,ielem);
+                        T(dofs,nodes) = 1;
+                    end
+                end
+            end
+            obj.I = T;
+        end
+
         function rhs = computeRHSintegrator(obj,quadType)
             s.type     = 'ShapeFunction';
             s.quadType = quadType;
             s.mesh     = obj.mesh;
             rhs        = RHSintegrator.create(s);
-        end
-
-        function xG = expressInFilterGaussPoints(obj,x)
-            x0 = zeros(length(x),obj.quadrature.ngaus);
-            for igaus = 1:obj.quadrature.ngaus
-                x0(:,igaus) = x;
-            end
-            ngaus        = obj.quadrature.ngaus;
-            nelem        = obj.mesh.nelem;
-            s.fValues    = reshape(x0',[1,ngaus,nelem]);
-            s.mesh       = obj.mesh;
-            s.quadrature = obj.quadrature;
-            xG           = FGaussDiscontinuousFunction(s);
         end
 
     end
