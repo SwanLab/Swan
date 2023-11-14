c56f116909129e43c399b77b2a3818dee95cf5d4
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2023-11-14 20:02:25 +0100

    chair forces

--- TFGAlvaro/BoundaryConditions/BCChair.m
+++ TFGAlvaro/BoundaryConditions/BCChair.m
@@ -44,23 +44,40 @@ lnodes(1:3:end,2) = 1;
 lnodes(2:3:end,2) = 2;
 lnodes(3:3:end,2) = 3;
 
-% External forces
-t         = 0.3; % Hole size (0.3A x 0.3A)
-zTip      = max(z);
-ref       = min(yMax,xMax);
-nForcesz  = find(z==zTip);
-nForcesy  = find(y>=(yMax-t*ref)/2 & y<=(yMax+t*ref)/2);
-nForcesx  = find(x>=(xMax-t*ref)/2 & x<=(xMax+t*ref)/2);
-nForcesyz = nForcesz(ismember(nForcesz,nForcesy,'rows'));
-nForcesxz = nForcesz(ismember(nForcesz,nForcesx,'rows'));
-nForces   = nForcesyz(ismember(nForcesyz,nForcesxz,'rows'));
+% External forces on horizontal surface
+fHS       = 3;
+zHS       = 0.5*max(z);
+nForcesz  = find(z==zHS);
+nForcesx  = find(x<=xMax-8);
+nForces   = nForcesz(ismember(nForcesz,nForcesx,'rows'));
 
-nForcesRepeated               = [nForces';nForces';nForces'];
-pointload_complete            = [pointload_complete,nForcesRepeated(:)];
-pointload_complete(:,[2,3])   = 0;
-pointload_complete(1:3:end,2) = 1;
-pointload_complete(2:3:end,2) = 2;
-pointload_complete(3:3:end,2) = 3;
-pointload_complete(3:3:end,3) = -1;
+pointload_complete1            = [];
+nForcesRepeated                = [nForces';nForces';nForces'];
+pointload_complete1            = [pointload_complete1,nForcesRepeated(:)];
+pointload_complete1(:,[2,3])   = 0;
+pointload_complete1(1:3:end,2) = 1;
+pointload_complete1(2:3:end,2) = 2;
+pointload_complete1(3:3:end,2) = 3;
+pointload_complete1(3:3:end,3) = -fHS;
+
+
+% External forces on vertical surface
+fVS = 1;
+xVS = xMax-8;
+nForcesx = find(x==xVS);
+nForcesz = find(z>=zHS);
+nForces  = nForcesx(ismember(nForcesx,nForcesz,'rows'));
+
+pointload_complete2            = [];
+nForcesRepeated                = [nForces';nForces';nForces'];
+pointload_complete2            = [pointload_complete2,nForcesRepeated(:)];
+pointload_complete2(:,[2,3])   = 0;
+pointload_complete2(1:3:end,2) = 1;
+pointload_complete2(2:3:end,2) = 2;
+pointload_complete2(3:3:end,2) = 3;
+pointload_complete2(1:3:end,3) = fVS;
+
+pointload_complete = [pointload_complete1;pointload_complete2];
+
 
 save('TFGAlvaro/BoundaryConditions/dataChair.mat','lnodes','pointload_complete'); % GIDLNODS! + Mathcha Kernel...
