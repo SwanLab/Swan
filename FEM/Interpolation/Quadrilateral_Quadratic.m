23713be09545bd38c70586112165a13581c98054
tarrut <tarrut.code@gmail.com>
2023-12-17 20:52:36 +0100

    Update Quadrilateral_Quadratic.m

--- FEM/Interpolation/Quadrilateral_Quadratic.m
+++ FEM/Interpolation/Quadrilateral_Quadratic.m
@@ -20,7 +20,7 @@ classdef Quadrilateral_Quadratic < Interpolation
             obj.type = 'QUADRILATERAL_QUADRATIC';
             obj.ndime = 2;
             obj.nnode = 9;
-            obj.pos_nodes = [0,0 ; 1 0 ; 0,1 ; 1,1 ; 0.5,0 ; 1,0.5 ; 0.5,1 ; 0,0.5 ; 0.5,0.5];
+            obj.pos_nodes = [-1,-1 ; 1 -1 ; -1,1 ; 1,1 ; 0,-1 ; 1,0 ; 0,1 ; -1,0 ; 0,0];
         end
         
         function computeShapes(obj,posgp)
