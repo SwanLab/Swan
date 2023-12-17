a996ee1f57b78f9c7c15128f90b44063029cdf7b
tarrut <tarrut.code@gmail.com>
2023-12-17 22:12:16 +0100

    Update testQuadLagrangian.m

--- testQuadLagrangian.m
+++ testQuadLagrangian.m
@@ -4,11 +4,11 @@ clc
 
 mesh = UnitQuadMesh(1,1);
 
-sAF.fHandle = @(x) sin(2*pi*x(1,:,:));
+sAF.fHandle = @(x) x(1,:,:);
 sAF.ndimf   = 1;
 sAF.mesh    = mesh;
 xFun = AnalyticalFunction(sAF);
 
-p1fun = xFun.project('P1');
+p1fun = xFun.project('P2');
 % p2fun = xFun.project('P2');
 % p3fun = xFun.project('P3');
