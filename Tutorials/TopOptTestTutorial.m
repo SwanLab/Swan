f3ea103d85494651386fefdc60f01f93e08670da
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2023-12-26 10:41:19 +0100

    Create TopOptTestTutorial.m

---
+++ Tutorials/TopOptTestTutorial.m
@@ -0,0 +1,24 @@
+classdef TopOptTestTutorial < handle
+    
+    properties (Access = private)
+        
+    end
+    
+    methods (Access = public)
+        
+        function obj = TopOptTestTutorial()
+            obj.init()
+            
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function init(obj)
+            
+        end
+        
+    end
+    
+end
