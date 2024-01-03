d4176893622734d2bf123215b41359a78ea978bb
jose-antonio-torres <jtorreslerma@icloud.com>
2024-01-03 10:28:30 +0100

    small correction

--- TFGAlvaro/RunTest.m
+++ TFGAlvaro/RunTest.m
@@ -2,16 +2,12 @@ s.testName = 'testAlvaro';
 t = TopOptComputer(s);
 t.compute();
 
-
-
 % With the following lines you obtain the result for the last iteration
 % (example: design variable with GiD. Test other results and also ParaView!)
-p1Params.fValues = t.computation.designVariable.value;
-p1Params.mesh    = t.computation.designVariable.mesh;
-Result           = P1Function(p1Params);
-
-
+Result = t.computation.designVariable.fun;
+u      = t.computation.physicalProblem.uFun;
 
 ResultsName = 'Cubechair_newcode';
 type   = 'Paraview'; % GiD/Paraview
 Result.print(['TFGAlvaro/Results/',ResultsName],type);
+u.print(['TFGAlvaro/Results/',ResultsName,'Displacements'],type);
