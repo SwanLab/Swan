94393d2cbc319765366665296919a0b75dd7ecff
jose-antonio-torres <jtorreslerma@icloud.com>
2024-01-18 09:40:55 +0100

    aJ for ls

--- Topology Optimization/Optimizers/OptimizerFactory.m
+++ Topology Optimization/Optimizers/OptimizerFactory.m
@@ -15,7 +15,7 @@ classdef OptimizerFactory < handle
                 case 'fmincon'
                     op = Optimizer_fmincon(cParams);
                 case 'NullSpace'
-                    cParams.optimizerNames.aJmax = 0.4; % 0.45 (only density, ls is 0)
+                    cParams.optimizerNames.aJmax = 0; % 0.45 (only density, ls is 0)
                     cParams.optimizerNames.aGmax = 0.65; % 0.65 (both)
                     op = OptimizerNullSpace(cParams);
                 case 'IPM'
