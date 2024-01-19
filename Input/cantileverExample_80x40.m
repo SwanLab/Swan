980732e22db70fe334750ae1979bd8862b681432
tcreusc <24914481+tcreusc@users.noreply.github.com>
2024-01-19 13:51:16 +0100

    updating test_cantilever_IPM

--- Input/cantileverExample_80x40.m
+++ Input/cantileverExample_80x40.m
@@ -9497,6 +9497,21 @@ pointload_complete = [
 3188 1 0.000000;
 3188 2 -0.062500;
 ];
+
+isLeft   = @(coor) (abs(coor(:,1) - min(coor(:,1)))   < 1e-12);
+isRight  = @(coor) (abs(coor(:,1) - max(coor(:,1)))   < 1e-12);
+isMiddle = @(coor) (abs(coor(:,2) >= 0.3 & abs(coor(:,2)) <= 0.7) );
+
+% Dirichlet
+sDir{1}.domain    = @(coor) isLeft(coor);
+sDir{1}.direction = [1,2];
+sDir{1}.value     = 0;
+
+% Point load
+sPL{1}.domain    = @(coor) isMiddle(coor) & isRight(coor);
+sPL{1}.direction = 2;
+sPL{1}.value     = -0.062500;
+
 %% Nodes solid
 % Node
 nodesolid = unique(pointload_complete(:,1));
