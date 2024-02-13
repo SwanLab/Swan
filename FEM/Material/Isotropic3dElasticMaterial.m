660cc9888a01b9439862f50f3d3958880ec576b8
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2024-02-13 13:37:09 +0100

    3d fem tests

--- FEM/Material/Isotropic3dElasticMaterial.m
+++ FEM/Material/Isotropic3dElasticMaterial.m
@@ -12,23 +12,23 @@ classdef Isotropic3dElasticMaterial < IsotropicElasticMaterial
 
         function C = evaluate(obj,xV)
             [mu,k] = obj.computeShearAndBulk(xV);      
-            l = obj.computeLambdaFromShearAndBulk(mu,k);
-            nGaus = size(m,1);                        
-            nElem = size(m,2);
+            l = obj.computeLambdaFromShearAndBulk(mu,k,obj.ndim);
+            nGaus = size(mu,1);                        
+            nElem = size(mu,2);
             nStre = 6;            
             C = zeros(nStre,nStre,nGaus,nElem);
-            C(1,1,:,:) = 2*m + l;
-            C(2,2,:,:) = 2*m + l;
-            C(3,3,:,:) = 2*m + l;
+            C(1,1,:,:) = 2*mu + l;
+            C(2,2,:,:) = 2*mu + l;
+            C(3,3,:,:) = 2*mu + l;
             C(1,2,:,:) = l;
             C(2,1,:,:) = l;
             C(1,3,:,:) = l;
             C(3,1,:,:) = l;
             C(3,2,:,:) = l;
             C(2,3,:,:) = l;
-            C(4,4,:,:) = m;
-            C(5,5,:,:) = m;
-            C(6,6,:,:) = m;
+            C(4,4,:,:) = mu;
+            C(5,5,:,:) = mu;
+            C(6,6,:,:) = mu;
         end
         
     end
