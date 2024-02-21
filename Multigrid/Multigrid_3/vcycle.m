31b3e177c78d6476037a1bcef7532e30a064e266
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2024-02-21 18:15:00 +0100

    Update vcycle.m

--- Multigrid/Multigrid_3/vcycle.m
+++ Multigrid/Multigrid_3/vcycle.m
@@ -2,40 +2,43 @@ function [u,numero,residuFine,iterRes] = vcycle(u, b, data, vdown, vup, level, b
 
     if level == 1
         %u = data(level).A\b;
-        malla = 'coarse';
+        mesh = 'coarse';
         maxIter = 1000;
         %plotSolution(u, mesh{1,1}, bc{1,1}, malla, numero)
-        [u, res,residuFine,iterRes] = conjugateGradient_Solver(data(level).A,b,u,malla,residuFine,iterRes,maxIter);
+        [u, res,residuFine,iterRes] = conjugateGradient_Solver(data(level).A,b,u,mesh,residuFine,iterRes,maxIter);
         %plotSolution(u, mesh{1,1}, bc{1,1}, malla, numero)
         %plotRes(res,mesh{1,1},bc{1,1},malla,numero)
         numero = numero + 1;
     else 
         %[u,~] = gauss_seidel(data(level).A, b, u,vdown);
-        malla = 'fine';
+        mesh = 'fine';
         maxIter = 20;
-        [u, res,residuFine,iterRes] = conjugateGradient_Solver(data(level).A,b,u,malla,residuFine,iterRes,maxIter);
+        [u, res,residuFine,iterRes] = conjugateGradient_Solver(data(level).A,b,u,mesh,residuFine,iterRes,maxIter);
         %plotSolution(u, mesh{1,2}, bc{1,2}, malla, numero)
         %plotRes(res,mesh{1,2},bc{1,2},malla,numero)
         numero = numero + 1;
         r = b - data(level).A*u;
         [Rr] = interpolate(r,bc,data,level);
         [ur] = interpolate(u,bc,data,level);
-        [e,numero,residuFine,iterRes] = vcycle(ur, Rr, data, vdown, vup, level - 1, bc, numero, mesh,residuFine,iterRes);
-    %    [e,numero,residuFine,iterRes] = vcycle(0*ur, data(level-1).b, data, vdown, vup, level - 1, bc, numero, mesh,residuFine,iterRes);
-        e = bc{level - 1}.reducedToFullVector(e);
-        e = reshape(e,2,[])';
-        e = data(level - 1).T * e; 
-        e = reshape(e,1,[])';
-        e = bc{level}.fullToReducedVector(e);
+        [er,numero,residuFine,iterRes] = vcycle(ur, Rr, data, vdown, vup, level - 1, bc, numero, mesh,residuFine,iterRes);
+        e = restriction(er);
         u = u + e;
         %[u,~] = gauss_seidel(data(level).A, b, u,vup);
-        malla = 'fine';
-        [u, res,residuFine,iterRes] = conjugateGradient_Solver(data(level).A,b,u,malla,residuFine,iterRes,maxIter);
+        mesh = 'fine';
+        [u, res,residuFine,iterRes] = conjugateGradient_Solver(data(level).A,b,u,mesh,residuFine,iterRes,maxIter);
         %plotSolution(u, mesh{1,2}, bc{1,2}, malla, numero)
         %plotRes(res,mesh{1,2},bc{1,2},malla,numero)
         numero = numero + 1;
      end
 
+end
+
+function fFine = restriction(fCoarse,bc,data,level)
+        fFine = bc{level - 1}.reducedToFullVector(fCoarse);
+        fFine = reshape(fFine,2,[])';
+        fFine = data(level - 1).T * fFine; 
+        fFine = reshape(fFine,1,[])';
+        fFine = bc{level}.fullToReducedVector(fFine);
 end
 
 function [fCoarse] = interpolate(fFine,bc,data,level)
