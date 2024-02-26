3c9d61a842b726fe172e0f6187377ef184ad3a7a
jose-antonio-torres <jtorreslerma@icloud.com>
2024-02-26 11:04:17 +0100

    cantilever

--- Topology Optimization/Optimizers/OptimizerConstrained/OptimizerMMA.m
+++ Topology Optimization/Optimizers/OptimizerConstrained/OptimizerMMA.m
@@ -191,8 +191,8 @@ classdef OptimizerMMA < Optimizer
                 obj.upp = ones(length(x0),1);
                 [obj.f0val,obj.df0dx,obj.fval,obj.dfdx] = obj.funmma();
                 obj.m = length(obj.fval);
-                obj.c = 1000*ones(obj.m,1);
-                obj.d = 0*ones(obj.m,1);
+                obj.c = 10*ones(obj.m,1);
+                obj.d = 100*ones(obj.m,1);
                 obj.a0 = 1;
                 obj.a = 0*ones(obj.m,1);
                 obj.n = length(obj.x);
--- Tutorials/TopOptTestTutorial.m
+++ Tutorials/TopOptTestTutorial.m
@@ -42,8 +42,8 @@ classdef TopOptTestTutorial < handle
 
         function createMesh(obj)
             %UnitMesh better
-            x1      = linspace(0,1,50);
-            x2      = linspace(0,1,50);
+            x1      = linspace(0,2,50);
+            x2      = linspace(0,1,100);
             [xv,yv] = meshgrid(x1,x2);
             [F,V]   = mesh2tri(xv,yv,zeros(size(xv)),'x');
             s.coord  = V(:,1:2);
