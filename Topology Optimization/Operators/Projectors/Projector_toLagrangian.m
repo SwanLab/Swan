4ce0a587d91d6a0178d75f84ccdf1411f7979f78
Pau Tarrés <91079521+tarrut@users.noreply.github.com>
2024-02-28 15:31:40 +0100

    Update Projector_toLagrangian.m

--- Topology Optimization/Operators/Projectors/Projector_toLagrangian.m
+++ Topology Optimization/Operators/Projectors/Projector_toLagrangian.m
@@ -29,7 +29,7 @@ classdef Projector_toLagrangian < Projector
             s.mesh  = obj.mesh;
             s.test  = LagrangianFunction.create(obj.mesh, 1, obj.order);
             s.trial = LagrangianFunction.create(obj.mesh, 1, obj.order);
-            s.quadratureOrder = 'CUBIC'; % no
+            s.quadratureOrder = 'ORDER5'; % no
             s.type  = 'MassMatrix';
             lhs = LHSintegrator.create(s);
             LHS = lhs.compute();
@@ -71,7 +71,7 @@ classdef Projector_toLagrangian < Projector
                 ord = fun.getQuadratureOrder();
             else
                 ord = obj.determineQuadratureOrder(fun);
-                ord = 'CUBIC'; % no
+                ord = 'ORDER5'; % no
             end
             q = Quadrature.set(obj.mesh.type);
             q.computeQuadrature(ord);
