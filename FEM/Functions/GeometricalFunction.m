97626b4d548c1889976b29d1a4d3f346df8e24fb
JoelCarrasc0 <joel.carrasco@estudiantat.upc.edu>
2024-03-18 13:19:17 +0100

    TestMicro modification / Superellipse inclusion

--- FEM/Functions/GeometricalFunction.m
+++ FEM/Functions/GeometricalFunction.m
@@ -83,6 +83,29 @@ classdef GeometricalFunction < handle
                     y0 = cParams.yCoorCenter;
                     fH = @(x) (x1(x)-x0).^2+(x2(x)-y0).^2-r^2;
                     obj.fHandle = fH;
+                
+                case 'Ellipse'
+                    a  = cParams.semiHorizontalAxis;
+                    b  = cParams.semiVerticalAxis;
+                    n  = cParams.superEllipseFactor;
+                    x0 = cParams.xCoorCenter;
+                    y0 = cParams.yCoorCenter;
+                    fH = @(x) abs((x1(x)-x0)/a).^n+abs((x2(x)-y0)/b).^n-1;
+                    obj.fHandle = fH;
+                
+                case 'Superform'
+                    a  = cParams.semiHorizontalAxis;
+                    b  = cParams.semiVerticalAxis;
+                    m  = cParams.m;
+                    n1 = cParams.n1;
+                    n2 = cParams.n2;
+                    n3 = cParams.n3;
+                    x0 = cParams.xCoorCenter;
+                    y0 = cParams.yCoorCenter;
+                    th = @(x) atan((x2(x)-y0)./(x1(x)-x0));
+                    r  = @(x) ((x1(x)-x0).^2+(x2(x)-y0).^2).^0.5;
+                    fH = @(x) (abs(cos(m*th(x)/4)/a).^n2+abs(sin(m*th(x)/4)/b).^n3).^(-1/n1)-r(x);
+                    obj.fHandle = fH;
 
                 case 'CircleInclusion'
                     s      = cParams;
