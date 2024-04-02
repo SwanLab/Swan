9cef50a01e9d5fe946be1289860cafb3c12d97ab
ArnauPena <a.penasapena@gmail.com>
2024-04-02 11:48:09 +0200

    fmincon monitoring works, it might not work for academic due to the designVariable definition

--- Tutorials/TopOptTestTutorial.m
+++ Tutorials/TopOptTestTutorial.m
@@ -183,7 +183,7 @@ classdef TopOptTestTutorial < handle
             s.lb             = 0;
             s.volumeTarget   = 0.4;
             s.primal         = 'PROJECTED GRADIENT';
-            opt              = OptimizerBisection(s);
+            opt              = Optimizer_fmincon(s);
             % opt = OptimizerMMA(s);
             opt.solveProblem();
             obj.optimizer = opt;
