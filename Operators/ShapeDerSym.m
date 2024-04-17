517df210fb3037024c7e332768ae8b5b93988e83
tcreusc <24914481+tcreusc@users.noreply.github.com>
2024-04-17 12:47:38 +0200

    starting fake stiffness matrix

--- Operators/ShapeDerSym.m
+++ Operators/ShapeDerSym.m
@@ -1,31 +1,77 @@
-function dom = Grad(u)
+function dom = ShapeDerSym(u)
     s.operation = @(xV) evaluate(u, xV);
     dom = DomainFunction(s);
 end
 
 function fVR = evaluate(u, xV)
     dNdx = u.evaluateCartesianDerivatives(xV);
-    nDofs   = u.nDofs;
-    nDimf   = u.ndimf;
-    nDimG   = size(dNdx, 1);
-    nNodeE  = size(dNdx, 2);
-    nPoints = size(dNdx, 3);
-    nElem   = size(dNdx, 4);
-    connec = u.getConnec();
-    fV = reshape(u.fValues', [1 nDofs]);
-    grad = zeros(nDimG,nDimf, nPoints, nElem);
-    for iDimG = 1:nDimG
-        for jDimf = 1:nDimf
-            for kNodeE = 1:nNodeE
-                dNdxIK = squeezeParticular(dNdx(iDimG, kNodeE,:,:),[1 2]);
-                iDofE = nDimf*(kNodeE-1)+jDimf;
-                dofs = connec(:,iDofE);
-                fKJ = repmat(fV(dofs),[nPoints 1]);
-                gradIJ= dNdxIK.*fKJ;
-                grad(iDimG,jDimf,:,:) = squeezeParticular(grad(iDimG,jDimf,:,:),[1 2]) + gradIJ;
-            end
-        end
-    end
+    B = computeB(u,dNdx);
+    fVR = B;
+end
 
-    fVR = reshape(grad, [nDimG*nDimf,nPoints, nElem]);
+function B = computeB(u,dNdx)
+    ndimf = u.ndimf;
+    switch ndimf
+        case 1
+            B = computeBin1D(u,dNdx);
+        case 2
+            B = computeBin2D(u,dNdx);
+        case 3
+            B = computeBin3D(u,dNdx);
+    end
+end
+
+function [B] = computeBin1D(u,deriv)
+    nVoigt = 2;
+    nDimf = u.ndimf;
+    nNode = size(deriv,2);
+    nElem = size(u,4);
+    nDofs = nDimf*nNode;
+    B = zeros(nVoigt,nDofs,nGaus,nElem);
+    for inode = 1:nNode
+        j = nDimf*(inode-1) + 1;
+        B(1,j,:,:) = deriv(1,inode,:,:);
+        B(2,j,:,:) = deriv(2,inode,:,:);
+    end
+end
+
+function B = computeBin2D(u,deriv)
+    nVoigt = 3;
+    nDimf = u.ndimf;
+    nNodE = size(deriv,2);
+    nDofE = nNodE*nDimf;
+    nGaus = size(deriv,3);
+    nElem = size(deriv,4);
+    B = zeros(nVoigt,nDofE,nGaus,nElem);
+    for iNode = 1:nNodE
+        j = nDimf*(iNode-1)+1;
+        B(1,j,:,:)   = deriv(1,iNode,:,:);
+        B(2,j+1,:,:) = deriv(2,iNode,:,:);
+        B(3,j,:,:)   = deriv(2,iNode,:,:);
+        B(3,j+1,:,:) = deriv(1,iNode,:,:);
+    end
+end
+
+function B = computeBin3D(u,deriv)
+    nVoigt = 6;
+    nNode = size(deriv,2);
+    nGaus = size(deriv,3);
+    nElem = size(deriv,4);
+    B = zeros(nVoigt,nNode,nGaus,nElem);
+    for inode = 1:nNode
+        j = u.ndimf*(inode-1)+1;
+        % associated to normal strains
+        B(1,j,:,:)   = deriv(1,inode,:,:);
+        B(2,j+1,:,:) = deriv(2,inode,:,:);
+        B(3,j+2,:,:) = deriv(3,inode,:,:);
+        % associated to shear strain, gamma12
+        B(4,j,:,:)   = deriv(2,inode,:,:);
+        B(4,j+1,:,:) = deriv(1,inode,:,:);
+        % associated to shear strain, gamma13
+        B(5,j,:,:)   = deriv(3,inode,:,:);
+        B(5,j+2,:,:) = deriv(1,inode,:,:);
+        % associated to shear strain, gamma23
+        B(6,j+1,:,:) = deriv(3,inode,:,:);
+        B(6,j+2,:,:) = deriv(2,inode,:,:);
+    end
 end
--- deletingIntegrators.m
+++ deletingIntegrators.m
@@ -12,10 +12,11 @@ ndimf = 2;
 trial = TrialFunction.create(mesh, ndimf, 'P1');
 test  = TestFunction.create(mesh, ndimf, 'P1');
 
+%% Mass Matrix
 shTr = Shape(trial).evaluate(xV);
 shTe = Shape(test).evaluate(xV);
 dVolu(1,:,:)  = mesh.computeDvolume(quad);
-mult = pagemtimes(shTr,shTe');
+mult = pagemtimes(shTr',shTe);
 integ = bsxfun(@times, mult, dVolu);
 % kron(squeeze(A(:,:,1)),eye(2)) % !!!!
 integ = openprod(integ,eye(ndimf));
@@ -24,6 +25,15 @@ M = massmatrix(quad,mesh,test,trial);
 
 shDerTe = ShapeDer(test).evaluate(xV);
 
+%% Fake stiffness matrix
+
+shTr = ShapeDerSym(trial).evaluate(xV);
+shTe = ShapeDerSym(test).evaluate(xV);
+dVolu(1,:,:)  = mesh.computeDvolume(quad);
+mult = pagemtimes(permute(shTr,[2 1 3 4]),shTe);
+
+
+%% Functions
 function z = openprod(A,B)
     z = bsxfun(@times, permute(A, [4 1 5 2 3]), permute(B, [1 4 2 5 3]));  %// step 1
     z = reshape(z, size(A,1)*size(B,1), size(A,2)*size(B,2), size(A,3)); 
@@ -62,5 +72,37 @@ function lhs = massmatrix(quad,mesh, test, trial)
         end
     end
     lhs = M;
+end
 
+
+function lhs = stiffnessmatrix(quad,mesh, test, trial)
+    xV = quad.posgp;
+    dNdxTs = test.evaluateCartesianDerivatives(xV);
+    dNdxTr = trial.evaluateCartesianDerivatives(xV);
+    dVolu = mesh.computeDvolume(obj.quadrature);
+    nGaus = quad.ngaus;
+    nElem = size(dVolu,2);
+
+    nNodETs = size(dNdxTs,2);
+    nDofETs = nNodETs*test.ndimf;
+    nNodETr = size(dNdxTr,2);
+    nDofETr = nNodETr*trial.ndimf;
+
+    BcompTs = createBComputer(test, dNdxTs);
+    BcompTr = createBComputer(trial, dNdxTr);
+    lhs = zeros(nDofETs,nDofETr,nElem);
+    for igaus = 1:nGaus
+        BmatTs = BcompTs.compute(igaus);
+        BmatTr = BcompTr.compute(igaus);
+        dV(1,1,:) = dVolu(igaus,:)';
+        Bt   = permute(BmatTs,[2 1 3]);
+        BtCB = pagemtimes(Bt, BmatTr);
+        lhs = lhs + bsxfun(@times, BtCB, dV);
+    end
+end
+
+function Bcomp = createBComputer( fun, dNdx)
+    s.fun  = fun;
+    s.dNdx = dNdx;
+    Bcomp = BMatrixComputer(s);
 end
