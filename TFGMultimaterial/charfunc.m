4c8b638a2a997c3928ffcd87f71e946ed69d4b23
rosa-galan <rosa.galan@estudiantat.upc.edu>
2024-04-24 17:08:20 +0200

    charfunc commit

--- TFGMultimaterial/charfunc.m
+++ TFGMultimaterial/charfunc.m
@@ -1,35 +1,65 @@
-function [ fi, tfi ] = charfunc( p,t,psi)
-%charfunc Calculates the characteristic function from the level-set
-%functions
-%auth: Augusto Romero
-%date: 26-06-2018
+classdef CharacteristicFunctionComputer < handle
 
-n = size(psi,2);
-chi = psi<0;
-if n>1
-    for i=1:n-1
-        fi(:,i) = (1 - chi(:,i+1)).*prod(chi(:,1:i),2);
+    properties (Access = public)
+        tfi
+        fi
     end
-    fi(:,n) = prod(chi,2);
-else
-    fi(:,1) = chi*1; %multiplied by 1 in order to convert from logical to double
-end
-fi(:,end+1) = (1 - chi(:,1));
 
-if nargout==2
-    for i=1:n
-    [tXi(i,:),~] = integ_exact(t,p,psi(:,i));
+    properties (Access = private)
+        levelSet
+        t
+        p
     end
-    tXi = 1 - tXi;
-    if n>1
-        for i=1:n-1
-            tfi(i,:) = (1 - tXi(i+1,:)).*prod(tXi(1:i,:),1);
+
+    methods (Access = public)
+
+        function obj = CharacteristicFunctionComputer(cParams)
+            obj.init(cParams)
+            obj.compute();
         end
-        tfi(n,:) = prod(tXi,1);
-    else
-        tfi(1,:) = tXi;
     end
-    tfi(end+1,:) = (1 - tXi(1,:));    
-end
+
+    methods (Access = private)
+
+        function init(obj,cParams)
+            obj.p = cParams.p;
+            obj.t = cParams.t;
+            obj.levelSet = cParams.psi;
+        end
+
+        function compute(obj)
+            psi = obj.levelSet; 
+
+            n = size(psi,2);
+            chi = psi<0;
+            
+            if n>1
+                for i=1:n-1
+                    obj.fi(:,i) = (1 - chi(:,i+1)).*prod(chi(:,1:i),2);
+                end
+                obj.fi(:,n) = prod(chi,2);
+            else
+                obj.fi(:,1) = chi*1; %multiplied by 1 in order to convert from logical to double
+            end
+            obj.fi(:,end+1) = (1 - chi(:,1));
+            
+            if nargout==2
+                for i=1:n
+                [tXi(i,:),~] = integ_exact(t,p,psi(:,i));
+                end
+                tXi = 1 - tXi;
+                if n>1
+                    for i=1:n-1
+                        obj.tfi(i,:) = (1 - tXi(i+1,:)).*prod(tXi(1:i,:),1);
+                    end
+                    obj.tfi(n,:) = prod(tXi,1);
+                else
+                    obj.tfi(1,:) = tXi;
+                end
+                obj.tfi(end+1,:) = (1 - tXi(1,:));    
+            end
+                    end
+                end
+
     
 end
