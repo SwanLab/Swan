5e7cb8bc67facb61e42804f2b1797e991d96af23
jose-antonio-torres <jtorreslerma@icloud.com>
2024-05-16 12:08:00 +0200

    full density case

--- lsEtaMaxStudy.m
+++ lsEtaMaxStudy.m
@@ -3,7 +3,7 @@
 clear;
 close all;
 
-fig = open("lsMagicRatio2.fig");
+fig = open("lsMagicRatio5SuperSmoothLarge.fig");
 % fig = open("lsMagicRatio3.fig");
 
 ch         = fig.Children;
@@ -11,44 +11,17 @@ iters      = ch(1).Children.XData(27:end); % ex 2: 27-end; ex 3.1: 3-end; ex3.2:
 beta       = ch(1).Children.YData(27:end);
 alpha      = ch(2).Children.YData(27:end);
 theta      = ch(3).Children.YData(27:end);
-magicRatio = ch(4).Children.YData(27:end);
+magicRatio = abs(ch(4).Children.YData(27:end));
 eta        = ch(8).Children.YData(27:end);
 k          = ch(10).Children.YData(27:end);
+gV         = abs(ch(13).Children.YData(27:end));
 close all;
 
 etaMax = 1./magicRatio;
 
-bVec = -20:0.1:20;
-for j=1:length(bVec)
-[aVec(j),error(j)]  = comptueKappaExponentFromTheta(etaMax,k,theta,bVec(j));
-end
-
+combo=beta./eta;
 figure
-plot(bVec,aVec)
+semilogy(iters,magicRatio,iters,theta,iters,k,iters,alpha,iters,beta,iters,combo,iters,gV)
 grid on
 grid minor
-xlabel('Theta sinus exponent')
-ylabel('Kappa exponent')
-
-figure
-plot(bVec,error)
-grid on
-grid minor
-xlabel('Theta sinus exponent')
-ylabel('Error')
-
-b=0;
-a=0.87;
-combo = (k.^a).*(sin(theta)).^b;
-
-figure
-semilogy(iters,etaMax,iters,theta,iters,k,iters,alpha,iters,beta,iters,combo)
-grid on
-grid minor
-legend('Real etaMax','theta','kappa','alpha','beta','Proposed combination')
-
-function [aRes,error]=comptueKappaExponentFromTheta(x,k,theta,b)
-fun   = @(a) norm((k.^a).*(sin(theta)).^b-x);
-aRes  = fminbnd(fun,-20,20);
-error = fun(aRes);
-end
+legend('Predicted tau','theta','kappa','alpha','beta','Proposed combination','volume constraint')
