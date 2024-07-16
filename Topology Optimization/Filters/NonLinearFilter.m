8fb5a3403aa22aa120b393cb76aca9830ae62846
jose-antonio-torres <jtorreslerma@icloud.com>
2024-07-16 11:23:39 +0200

    first direction programmed

--- Topology Optimization/Filters/NonLinearFilter.m
+++ Topology Optimization/Filters/NonLinearFilter.m
@@ -3,23 +3,31 @@ classdef NonLinearFilter < handle
     properties (Access = private)
         mesh
         trial
+        q
         epsilon
     end
 
     properties (Access = private)
         % Define new properties here
+        M
+        RHS1
+        Kq
     end
 
     methods (Access = public)
         function obj = NonLinearFilter(cParams)
             obj.init(cParams);
             % Construct non-linear stuff...
+            obj.createMassMatrix();
         end
 
         function xF = compute(obj,fun,quadOrder)
             xF = LagrangianFunction.create(obj.mesh, 1, obj.trial.order);
             % solve non-linear filter...
             % let's start by creating a factory and define circle case (validation)
+            obj.createRHSFirstDirection(fun,quadOrder);
+            obj.createStiffnessWithFunFirstDirection();
+            obj.solveFirstDirection();
 
             %xF.fValues  = ;
         end
@@ -34,9 +42,48 @@ classdef NonLinearFilter < handle
 
     methods (Access = private)
         function init(obj,cParams)
-            obj.trial   = LagrangianFunction.create(cParams.mesh, 1, cParams.trial.order);
+            obj.trial   = LagrangianFunction.create(cParams.mesh, 1, cParams.trial.order); % rho_eps
+            obj.q       = LagrangianFunction.create(cParams.mesh, 1, cParams.trial.order);
             obj.mesh    = cParams.mesh;
             obj.epsilon = cParams.mesh.computeMeanCellSize();
+        end
+
+        function createMassMatrix(obj)
+            s.type  = 'MassMatrix';
+            s.mesh  = obj.mesh;
+            s.test  = obj.trial;
+            s.trial = obj.trial;
+            s.quadratureOrder = 2;
+            LHS     = LHSintegrator.create(s);
+            obj.M   = LHS.compute();
+        end
+
+        function createRHSFirstDirection(obj,fun,quadOrder)
+            s.mesh     = obj.mesh;
+            s.type     = 'ShapeFunction';
+            s.quadType = quadOrder;
+            int        = RHSintegrator.create(s);
+            test       = obj.trial;
+            rhs        = int.compute(fun,test);
+            obj.RHS1   = rhs;
+        end
+
+        function createStiffnessWithFunFirstDirection(obj)
+            s.type = 'StiffnessMatrixWithFunction';
+            s.mesh  = obj.mesh;
+            s.test  = obj.trial;
+            s.trial = obj.trial;
+            s.quadratureOrder = 2;
+            s.function = obj.q;
+            LHS     = LHSintegrator.create(s);
+            obj.Kq  = LHS.compute();
+        end
+
+        function solveFirstDirection(obj)
+            LHS = obj.M + obj.Kq;
+            RHS = obj.RHS1;
+            rhoi = LHS\RHS; % hard coded direct solver
+            obj.trial.fValues = rhoi;
         end
 
         function itHas = hasEpsilonChanged(obj,eps)
