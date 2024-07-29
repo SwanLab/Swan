4171e1a5d7b2a16254512c7803e0b1e8bcb1c06e
tcreusc <24914481+tcreusc@users.noreply.github.com>
2024-07-29 19:28:22 +0200

    final cleanup

--- Tutorials/Tutorial02FEMElasticity.m
+++ Tutorials/Tutorial02FEMElasticity.m
@@ -27,12 +27,12 @@ classdef Tutorial02FEMElasticity < handle
         end
 
         function createMesh(obj)
-            obj.mesh = Mesh.createFromGiD('hole_mesh.m');
+            obj.mesh = UnitTriangleMesh(50,50);
         end
 
         function computeElasticProperties(obj)
-            E1  = 2.909090909090909e+03;
-            nu1 = 0.454545454545455;
+            E1  = 1;
+            nu1 = 1/3;
             E   = AnalyticalFunction.create(@(x) E1*ones(size(squeeze(x(1,:,:)))),1,obj.mesh);
             nu  = AnalyticalFunction.create(@(x) nu1*ones(size(squeeze(x(1,:,:)))),1,obj.mesh);
             obj.young   = E;
@@ -66,77 +66,34 @@ classdef Tutorial02FEMElasticity < handle
         function bc = createBoundaryConditions(obj)
             xMax    = max(obj.mesh.coord(:,1));
             yMax    = max(obj.mesh.coord(:,2));
-            isLeft   = @(coor)  abs(coor(:,1))==0;
-            isRight  = @(coor)  abs(coor(:,1))==xMax;
-            isHalf   = @(coor)  abs(abs(coor(:,1)) - xMax/2) <=10e-2;
-            isTop    = @(coor)  abs(coor(:,2))==yMax;
-            isBottom = @(coor)  abs(coor(:,2))==0;
-            isMiddle = @(coor)  abs(abs(coor(:,2))-yMax/2) <= 10e-2;
-            
-            % 2D N ELEMENTS
-            sDir2.domain    = @(coor) isBottom(coor);
-            sDir2.direction = [1,2];
-            sDir2.value     = 0;
-            dir2 =  DirichletCondition(obj.mesh, sDir2);
-%             s.dirichletFun = [dir2];
+            isDir   = @(coor)  abs(coor(:,1))==0;
+            isForce = @(coor)  (abs(coor(:,1))==xMax & abs(coor(:,2))>=0.3*yMax & abs(coor(:,2))<=0.7*yMax);
 
-            sDir3.domain    = @(coor) isTop(coor);
-            sDir3.direction = [1];
-            sDir3.value     = 0;
-            dir3 =  DirichletCondition(obj.mesh, sDir3);
+            sDir{1}.domain    = @(coor) isDir(coor);
+            sDir{1}.direction = [1,2];
+            sDir{1}.value     = 0;
 
-            sDir4.domain    = @(coor) isTop(coor);
-            sDir4.direction = [2];
-            sDir4.value     = 0.5;
-            dir4 =  DirichletCondition(obj.mesh, sDir4);
+            sPL{1}.domain    = @(coor) isForce(coor);
+            sPL{1}.direction = 2;
+            sPL{1}.value     = -1;
 
-            s.dirichletFun = [dir2,dir3,dir4];
-            s.pointloadFun = [];
-% 
-%             sPL.domain    = @(coor) isTop(coor);
-%             sPL.direction = 2;
-%             sPL.value     = 200;
-%             s.pointloadFun = DistributedLoad(obj.mesh, sPL);%DistributedLoad(obj.mesh, sPL);
+            dirichletFun = [];
+            for i = 1:numel(sDir)
+                dir = DirichletCondition(obj.mesh, sDir{i});
+                dirichletFun = [dirichletFun, dir];
+            end
+            s.dirichletFun = dirichletFun;
 
-%             [bM,l2g] = obj.mesh.getBoundarySubmesh(sPL.domain);
-% 
-%             sAF.fHandle = @(x) [0*x(1,:,:);sPL.value*ones(size(x(1,:,:)))];
-%             sAF.ndimf   = 2;
-%             sAF.mesh    = bM;
-%             xFun = AnalyticalFunction(sAF);
-%             xFunP1  =xFun.project('P1');
-% 
-%             s.mesh = bM;
-%             s.type = 'ShapeFunction';
-%             s.quadType = 2;
-%             rhsI       = RHSintegrator.create(s);
-%             test = LagrangianFunction.create(bM,xFun.ndimf,'P1');
-%             Fext2 = rhsI.compute(xFunP1,test);   
-%             Fext3 = reshape(Fext2,[bM.ndim,bM.nnodes])';
-% 
-%             Fext = zeros(obj.mesh.nnodes,2);
-%             Fext(l2g,:) = Fext3;
-% 
-% %             obj.FextInitial = Fext; 
-%             [pl_dofs, ~, pl_vals] = find(Fext);
-%             ndofs = obj.mesh.nnodes*obj.mesh.ndim;
-%             fVals = zeros(ndofs,1);
-%             fVals(pl_dofs) = pl_vals;
-%             fVals = obj.reshapeToMatrix(fVals);
+            pointloadFun = [];
+            for i = 1:numel(sPL)
+                pl = PointLoad(obj.mesh, sPL{i});
+                pointloadFun = [pointloadFun, pl];
+            end
+            s.pointloadFun = pointloadFun;
 
-%             s.pointloadFun.fun = LagrangianFunction.create(obj.mesh,xFun.ndimf,'P1');
-%             s.pointloadFun.fun.fValues = fVals;
-%             s.pointloadFun.fValues = fVals;
-%             s.pointloadFun.values = pl_vals;
             s.periodicFun  = [];
-            s.mesh         = obj.mesh;
-
+            s.mesh = obj.mesh;
             bc = BoundaryConditions(s);
-%             obj.boundaryConditions = bc;
-        end
-
-        function rshp = reshapeToMatrix(obj, A)
-            rshp = reshape(A,[obj.mesh.ndim,obj.mesh.nnodes])';
         end
 
     end
