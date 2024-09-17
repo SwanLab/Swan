3a4fa2fd67ecebd308cca93c7b7766d7bf31002f
jose-antonio-torres <jtorreslerma@icloud.com>
2024-09-17 10:39:42 +0200

    simpler BCs of test

--- tests/Source/MultimaterialTests/MultimaterialTesting.m
+++ tests/Source/MultimaterialTests/MultimaterialTesting.m
@@ -90,12 +90,36 @@ classdef MultimaterialTesting < handle
         end
 
         function createBoundaryConditions(obj)
+            xMax    = max(obj.mesh.coord(:,1));
+            yMax    = max(obj.mesh.coord(:,2));
+            isDir   = @(coor)  abs(coor(:,1))==0;
+            isForce = @(coor)  (abs(coor(:,1))==xMax & abs(coor(:,2))>=0.35*yMax & abs(coor(:,2))<=0.65*yMax);
+
+            sDir{1}.domain    = @(coor) isDir(coor);
+            sDir{1}.direction = [1,2];
+            sDir{1}.value     = 0;
+
+            sPL{1}.domain    = @(coor) isForce(coor);
+            sPL{1}.direction = 2;
+            sPL{1}.value     = -1;
+
+            dirichletFun = [];
+            for i = 1:numel(sDir)
+                dir = DirichletCondition(obj.mesh, sDir{i});
+                dirichletFun = [dirichletFun, dir];
+            end
+            s.dirichletFun = dirichletFun;
+
+            pointloadFun = [];
+            for i = 1:numel(sPL)
+                pl = PointLoad(obj.mesh, sPL{i});
+                pointloadFun = [pointloadFun, pl];
+            end
+            s.pointloadFun = pointloadFun;
+
+            s.periodicFun  = [];
             s.mesh = obj.mesh;
-            BoundCond  = BoundaryConditionsSwan(s);
-       
-            obj.bc = BoundCond.createBoundaryConditionsTutorialBeam();
-            %obj.bcSwan = BoundCond.createBoundaryConditionsTutorialBridge();
-            %obj.bcSwan = BoundCond.createBoundaryConditionsTutorialArch();
+            obj.bc = BoundaryConditions(s);
         end
 
         function createElasticProblem(obj)
