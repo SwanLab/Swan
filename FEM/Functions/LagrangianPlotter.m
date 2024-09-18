623c1ce95401e0072f39da2b839ee38f59015c77
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2024-09-18 14:38:55 +0200

    Create LagrangianPlotter.m

---
+++ FEM/Functions/LagrangianPlotter.m
@@ -0,0 +1,32 @@
+classdef LagrangianPlotter < handle
+    
+    properties (Access = public)
+        
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    methods (Access = public)
+        
+        function obj = LagrangianPlotter(cParams)
+            obj.init(cParams)
+            
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function init(obj,cParams)
+            
+        end
+        
+    end
+    
+end
