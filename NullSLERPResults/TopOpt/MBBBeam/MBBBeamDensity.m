001a6a68e5e88d29912bc6be2b5e3df6cb172628
jose-antonio-torres <jtorreslerma@icloud.com>
2024-09-24 09:57:05 +0200

    mbb beam requires less etaNorm

--- NullSLERPResults/TopOpt/MBBBeam/MBBBeamDensity.m
+++ NullSLERPResults/TopOpt/MBBBeam/MBBBeamDensity.m
@@ -170,7 +170,7 @@ classdef MBBBeamDensity < handle
             s.primal         = 'PROJECTED GRADIENT';
             s.ub             = 1;
             s.lb             = 0;
-            s.etaNorm        = 0.02;
+            s.etaNorm        = 0.005;
             s.gJFlowRatio    = 0.5;
             opt = OptimizerNullSpace(s);
             opt.solveProblem();
