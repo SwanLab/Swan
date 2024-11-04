7d1fd4d4fc6529b63cb0b3d87fbc8a47a835d3a7
Gerard Villalta <villaltagerard@gmail.com>
2024-11-04 22:57:37 +0100

    InitSwan

---
+++ startupSwan.m
@@ -0,0 +1,7 @@
+% Set up startup file
+% 1. Change userpath to your Swan path
+% 2. Copy this script in "~/Matlab/R202XX/toolbox/local"
+% 3. Change file name to "startup.m"
+userpath = 'C:\Users\XXXX\Documents\GitHub\Swan';
+addpath(genpath(userpath));
+cd(userpath);
