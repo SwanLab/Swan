43ee2b092b9a6b65f347a8719125af0aed73d429
Gerard Villalta <villaltagerard@gmail.com>
2024-11-05 16:15:06 +0100

    startupSwan finished

--- startupSwan.m
+++ startupSwan.m
@@ -2,6 +2,12 @@
 % 1. Change userpath to your Swan path
 % 2. Copy this script in "~/Matlab/R202XX/toolbox/local"
 % 3. Change file name to "startup.m"
+% 4. In Matlab, go to "preferences" and in "Matlab/General" 
+%    click on "Update Toolbox Path Cache"
+%
+% With this, everytime Matlab is opened, Swan folder and subfolders
+% will be included. If Github branch is changed, just run commmand 
+% "addpath(genpath(userpath))"
 userpath = 'C:\Users\XXXX\Documents\GitHub\Swan';
 addpath(genpath(userpath));
 cd(userpath);
