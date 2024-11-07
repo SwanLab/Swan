bfdcc875894735496b6a7237a103468d97fee7a6
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2024-11-07 10:21:45 +0100

    "CurlImproved"

--- Operators/Curl.m
+++ Operators/Curl.m
@@ -1,24 +1,3 @@
-function dom = Curl(u)
-    s.operation = @(xV) evaluate(u, xV);
-    s.ndimf = 1;
-    dom = DomainFunction(s);
+function curlU = Curl(u)
+  curlU = u.computeCurl();
 end
-
-function curl = evaluate(u, xV)
-    dNdx    = u.evaluateCartesianDerivatives(xV);
-    nPoints = size(xV, 2);
-    uV = u.getValuesByElem();
-    uP = createOrthogonal(uV);
-    uP = repmat(uP,[1 1 1 nPoints]);
-    uP = permute(uP,[1 2 4 3]);
-    grad  = pagemtimes(dNdx,uP);
-    divUp = squeeze(sum(grad,1));
-    curl  = divUp;
-end
-
-function uP = createOrthogonal(u)
-uP = zeros(size(u));
-uP(:,1,:) = u(:,2,:);
-uP(:,2,:) = -u(:,1,:);
-end
-
