1fe1076f215c9400ef34450454485530035f7e55
Gerard Villalta <villaltagerard@gmail.com>
2024-11-09 17:08:04 +0100

    startupSwan

--- startupSwan.m
+++ startupSwan.m
@@ -1,7 +1,8 @@
 % Set up startup file
 % 1. Change userpath to your Swan path
-% 2. Copy this script in "~/Matlab/R202XX/toolbox/local"
-% 3. In Matlab, go to "preferences" and in "Matlab/General" 
+% 2. Copy this script and change the name to "startup.m"
+% 3. Move the script to "~/Matlab/R202XX/toolbox/local"
+% 4. In Matlab, go to "preferences" and in "Matlab/General" 
 %    click on "Update Toolbox Path Cache"
 %
 % With this, everytime Matlab is opened, Swan folder and subfolders
