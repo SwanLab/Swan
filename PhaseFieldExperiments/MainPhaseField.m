10d272880a9dc4543bc954294dbe2b7e382bd8de
Gerard Villalta <villaltagerard@gmail.com>
2024-11-11 13:44:54 +0100

    Resultats Comparatius

--- PhaseFieldExperiments/MainPhaseField.m
+++ PhaseFieldExperiments/MainPhaseField.m
@@ -6,7 +6,7 @@ s.monitoring.print = true;
 s.benchmark.type.mesh = 'SEN';%'1Elem';
 s.benchmark.N = 10;
 s.benchmark.type.bc = 'displacementShear';%'displacementTraction';
-s.benchmark.bcValues = [0:1e-4:1e-2,1e-5:1e-3:3e-2];%[0:1e-4:5e-3,5e-3:1e-4:2e-1];%[5e-3:0.1*1e-3:1e-2];%;%[0:1e-4:1e-1];
+s.benchmark.bcValues = [0:1e-4:3e-2,3e-2:1e-5:3.5e-2];%[0:1e-4:5e-3,5e-3:1e-4:2e-1];%[5e-3:0.1*1e-3:1e-2];%;%[0:1e-4:1e-1];
 s.matInfo.E  = 210;
 s.matInfo.nu = 0.3;
 s.matInfo.Gc = 2.7e-3;
@@ -15,11 +15,12 @@ s.matInfo.fileName = 'CircleMicroDamagePerimeter'; %'IsoMicroDamage','Circle/Squ
 s.matInfo.degradation = 'PhaseFieldDegradation';
 s.dissipInfo.type = 'PhaseFieldDissipationAT';
 s.dissipInfo.pExp = 2;
-s.l0 = 0.1;
+s.l0 = 0.0015;
 
 tester = TestingPhaseField(s);
 outputData = tester.compute();
 
 PhaseFieldPlotter(outputData);
 
-save("SEN1e-4_0.0075_1e-8.mat","outputData") %ACTIVATE TO SAVE DATA!
+%save("SEN1e-4_0.0075_1e-8.mat","outputData") %ACTIVATE TO SAVE DATA!
+
