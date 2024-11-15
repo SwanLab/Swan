d8f839c8e337265ee90011f5e23ef224227a7398
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2024-11-15 20:46:48 +0100

    Update ENIAfirstLectureLearnMatlab.m

--- ENIA/Lecture1/ENIAfirstLectureLearnMatlab.m
+++ ENIA/Lecture1/ENIAfirstLectureLearnMatlab.m
@@ -3,6 +3,12 @@ nSteps = 10;
 t = createTimeStepping(nSteps);
 theta = t.*t;
 plot(t,theta,'-+');
+dtheta = t.*t.*t;
+hold on
+plot(t,dtheta,'-+');
+ddtheta = log(t).*t;
+figure
+plot(t,ddtheta,'-+');
 end
 
 function t = createTimeStepping(nSteps)
