1ef1e725ee93731c745f74ff2328a368b328cad3
jose-antonio-torres <jtorreslerma@icloud.com>
2024-11-20 19:14:15 +0100

    BC GidAndMatlab

--- ResultsTFGGerard/TopOptTestSingular.m
+++ ResultsTFGGerard/TopOptTestSingular.m
@@ -134,7 +134,7 @@ classdef TopOptTestSingular < handle
             s.scale = 'MACRO';
             s.material = obj.createMaterial();
             s.dim = '3D';
-            %s.boundaryConditions = obj.createBoundaryConditions(); 
+            %s.boundaryConditions = obj.createBoundaryConditionsGidAndMatlab(); 
             s.boundaryConditions = obj.createNewBoundaryConditionsWithGiD(); %obj.createBoundaryConditions();   %CANVI JOSE (new=GiD. altre=matlab)
             s.interpolationType = 'LINEAR';
             s.solverType = 'REDUCED';
@@ -232,53 +232,20 @@ classdef TopOptTestSingular < handle
 
         end
 
-        function bc = createBoundaryConditions(obj)
-            %---------------------------------------------------%
-            %---------------BOUNDARY CONDITIONS GiD-------------%
-            % femReader = FemInputReader_GiD();  %Llegim malla GiD
-            % s = femReader.read(obj.filename);  %Llegim malla GiD
-            % 
-            % [u,v]=unique(s.pointload(:,3));  %Creem un vector [u,v] que es igual a totes les files de la tercera columna de s.pointload
-            %                                  % u= valors diferents de les forces aplicades en diferents punts. v= nº de forces diferents
-            % for i = 1:length(v)
-            %     rows    = find(s.pointload(:,3)==u(i)); %Creem "rows" = numero de nodes que tenen força aplicada (1,2,...)
-            %     isForce = @(coor) s.pointload(rows,1);  %isForce guarda tots els valors dels nodes amb força aplicada
-            %     sPL{i}.domain    = @(coor) isForce(coor);
-            %     sPL{i}.direction = s.pointload(v(i),2); %La direcció de la força serà la segona columna de s.pointload
-            %     sPL{i}.value     = u(i);                %El valor de la força serà la u(v)
-            % end
-            % 
-            % isDir   = @(coor)  unique(s.dirichlet(:,1));  %llegeix les constraints de desplaçaments del GiD
-            % sDir{i}.domain    = @(coor) isDir(coor);
-            % sDir{i}.direction = [1,2,3]; % [1,2] 2D  /  [1,2,3] 3D  %restricció en aquest punts.
-            % sDir{i}.value     = 0;
-
-            %---------------BOUNDARY CONDITIONS GiD-------------%
-            %---------------------------------------------------%
-            %---------------------------------------------------%
-            %-------------BOUNDARY CONDITIONS MATLAB------------%
-
-            %---------------3D CANTIELEVER CASE-------------%
-          xMax    = max(obj.mesh.coord(:,1));
-          yMax    = max(obj.mesh.coord(:,2));
-          zMax    = max(obj.mesh.coord(:,3));
-
-          isDir  = @(coor)  abs(coor(:,1))==0;
-          isForce = @(coor) abs(coor(:,1))==xMax & abs(coor(:,2))>=0.4*yMax & abs(coor(:,2))<=0.6*yMax & abs(coor(:,3))>=0.4*zMax & abs(coor(:,3))<=0.6*zMax;
-
-          sDir{1}.domain    = @(coor) isDir(coor); %punt esquerre
-          sDir{1}.direction = [1,2,3]; %restricció vertical, horitzontal i 3r eix
-          sDir{1}.value     = 0;  %desplaçament =0
-
+        function bc = createBoundaryConditionsGidAndMatlab(obj)
+%           xMax    = max(obj.mesh.coord(:,1));
+%           yMax    = max(obj.mesh.coord(:,2));
+%           zMax    = max(obj.mesh.coord(:,3));
+            
+          femReader = FemInputReader_GiD();
+          s         = femReader.read(obj.filename);
+          sDir      = obj.computeCondition(s.dirichlet);
+          
+          isForce = @(coor) coor(:,3)>=-225 & coor(:,3)<=475 & coor(:,2)>=46.5 & coor(:,2)<=47.5;
           sPL{1}.domain    = @(coor) isForce(coor);
           sPL{1}.direction = 2;
           sPL{1}.value     = -1;
-%---------------3D CANTIELEVER CASE-------------%
 
-
-
-            %-------------BOUNDARY CONDITIONS MATLAB------------%
-            %---------------------------------------------------%
             dirichletFun = [];
             for i = 1:numel(sDir)
                 dir = DirichletCondition(obj.mesh, sDir{i});
