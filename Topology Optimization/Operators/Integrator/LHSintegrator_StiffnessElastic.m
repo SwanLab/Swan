2f791af1dfca2b25e2a7dc393f83286d1bee76d4
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2024-12-05 13:00:41 +0100

    No loop in LHS elastic problem

--- Topology Optimization/Operators/Integrator/LHSintegrator_StiffnessElastic.m
+++ Topology Optimization/Operators/Integrator/LHSintegrator_StiffnessElastic.m
@@ -21,33 +21,34 @@ classdef LHSintegrator_StiffnessElastic < LHSintegrator
     methods (Access = protected)
 
         function lhs = computeElementalLHS(obj)
-            xV = obj.quadrature.posgp;
+            xV   = obj.quadrature.posgp;
             dNdx = obj.test.evaluateCartesianDerivatives(xV);
-            dVolu = obj.mesh.computeDvolume(obj.quadrature);
-            nGaus = obj.quadrature.ngaus;
-            nElem = obj.mesh.nelem;
-            nNodE = size(dNdx, 2);
-            nDofE = nNodE * obj.test.ndimf;
-            lhs = zeros(nDofE, nDofE, nElem);
-            %    Bcomp = obj.createBComputer(dNdx);
+            B    = obj.computeB(dNdx);            
+            dV   = obj.mesh.computeDvolume(obj.quadrature);
+            dV   = permute(dV,[3, 4, 2, 1]);
             Cmat = obj.material.evaluate(xV);
-            deriv = dNdx;
-            nNode = size(deriv, 2);
-            nElem = size(deriv, 4);
+            Cmat = permute(Cmat,[1 2 4 3]);
+            Bt   = permute(B,[2 1 3 4]);
+            BtC  = pagemtimes(Bt, Cmat);
+            BtCB = pagemtimes(BtC, B);
+            lhs  = sum(BtCB .* dV, 4);
+        end
 
-            % nDim = obj.mesh.ndim;
-            % nVoigt = nDim * (nDim + 1) / 2;
-            % j = reshape((0:nNode-1) * obj.test.ndimf, 1, nNode, 1);
-            % B = zeros(nVoigt, obj.test.ndimf * nNode, nElem);
-            % 
+    end
 
-            nDim = obj.mesh.ndim;
+    methods (Access = private)
+
+        function B = computeB(obj, dNdx)
+            nGaus = obj.quadrature.ngaus;
+            nNode = size(dNdx, 2);
+            nElem = obj.mesh.nelem;
+            nDim  = obj.mesh.ndim;
+            nDimF = obj.test.ndimf;
+            nDofE = nNode*nDimF;            
             nVoigt = nDim * (nDim + 1) / 2;
-            j = reshape((0:nNode-1) * obj.test.ndimf, 1, nNode, 1);
-            B = zeros(nVoigt, obj.test.ndimf * nNode, nElem, nGaus);
-
-            d = permute(deriv(1:nDim, :, :, :), [1, 2, 4, 3]);
-
+            j = nDimF * reshape((1:nNode) - 1, 1, nNode, 1);
+            B = zeros(nVoigt, nDofE, nElem, nGaus);
+            d = permute(dNdx, [1, 2, 4, 3]);
             if nDim == 2
                 B(1, j + 1, :, :) = d(1, :, :, :);
                 B(2, j + 2, :, :) = d(2, :, :, :);
@@ -64,41 +65,6 @@ classdef LHSintegrator_StiffnessElastic < LHSintegrator
                 B(6, j + 2, :, :) = d(3, :, :, :);
                 B(6, j + 3, :, :) = d(2, :, :, :);
             end
-
-            for iGaus = 1:nGaus           
-                BG = B(:,:,:,iGaus);
-                C = squeeze(Cmat(:, :, iGaus, :));
-                dV = permute(dVolu(iGaus, :), [3, 1, 2]);
-                BtCB = pagemtimes(pagemtimes(permute(BG, [2, 1, 3]), C), BG);
-                t = BtCB .* dV;
-                lhs = lhs + t;
-            end
-
-
-
-            %lhs2 = lhs;
-
-
-
-% C = permute(Cmat, [1, 2, 4, 3]);
-% dV = reshape(dVolu, [1, 1, nElem, nGaus]);  % Ensure alignment with BtCB
-% 
-% BtCB = pagemtimes(pagemtimes(permute(B, [2, 1, 3, 4]), C), B);
-% 
-% lhs = sum(BtCB .* dV,4);
-
-
-
-        end
-
-    end
-
-    methods (Access = private)
-
-        function Bcomp = createBComputer(obj, dNdx)
-            s.fun  = obj.test;
-            s.dNdx = dNdx;
-            Bcomp = BMatrixComputer(s);
         end
 
     end
