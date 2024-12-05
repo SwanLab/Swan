7db0679fc2bdb3b9f482cc98b78bd098002c29e8
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2024-12-05 08:24:34 +0100

    "improving"

--- Topology Optimization/Operators/Integrator/LHSintegrator_StiffnessElastic.m
+++ Topology Optimization/Operators/Integrator/LHSintegrator_StiffnessElastic.m
@@ -35,64 +35,51 @@ classdef LHSintegrator_StiffnessElastic < LHSintegrator
             nNode = size(deriv, 2);
             nElem = size(deriv, 4);
 
+            % nDim = obj.mesh.ndim;
+            % nVoigt = nDim * (nDim + 1) / 2;
+            % j = reshape((0:nNode-1) * obj.test.ndimf, 1, nNode, 1);
+            % B = zeros(nVoigt, obj.test.ndimf * nNode, nElem);
+            % 
+
             nDim = obj.mesh.ndim;
             nVoigt = nDim * (nDim + 1) / 2;
             j = reshape((0:nNode-1) * obj.test.ndimf, 1, nNode, 1);
-            B = zeros(nVoigt, obj.test.ndimf * nNode, nElem);
+            B = zeros(nVoigt, obj.test.ndimf * nNode, nElem, nGaus);
 
-            for iGaus = 1:nGaus
-                d = permute(deriv(1:nDim, :, iGaus, :), [1, 2, 4, 3]);
-                if nDim == 2
-                    B(1, j + 1, :) = d(1, :, :);
-                    B(2, j + 2, :) = d(2, :, :);
-                    B(3, j + 1, :) = d(2, :, :);
-                    B(3, j + 2, :) = d(1, :, :);
-                elseif nDim == 3
-                    B(1, j + 1, :) = d(1, :, :);
-                    B(2, j + 2, :) = d(2, :, :);
-                    B(3, j + 3, :) = d(3, :, :);
-                    B(4, j + 1, :) = d(2, :, :);
-                    B(4, j + 2, :) = d(1, :, :);
-                    B(5, j + 1, :) = d(3, :, :);
-                    B(5, j + 3, :) = d(1, :, :);
-                    B(6, j + 2, :) = d(3, :, :);
-                    B(6, j + 3, :) = d(2, :, :);
-                end
+            d = permute(deriv(1:nDim, :, :, :), [1, 2, 4, 3]);
 
+            if nDim == 2
+                B(1, j + 1, :, :) = d(1, :, :, :);
+                B(2, j + 2, :, :) = d(2, :, :, :);
+                B(3, j + 1, :, :) = d(2, :, :, :);
+                B(3, j + 2, :, :) = d(1, :, :, :);
+            elseif nDim == 3
+                B(1, j + 1, :, :) = d(1, :, :, :);
+                B(2, j + 2, :, :) = d(2, :, :, :);
+                B(3, j + 3, :, :) = d(3, :, :, :);
+                B(4, j + 1, :, :) = d(2, :, :, :);
+                B(4, j + 2, :, :) = d(1, :, :, :);
+                B(5, j + 1, :, :) = d(3, :, :, :);
+                B(5, j + 3, :, :) = d(1, :, :, :);
+                B(6, j + 2, :, :) = d(3, :, :, :);
+                B(6, j + 3, :, :) = d(2, :, :, :);
+            end
+
+            for iGaus = 1:nGaus           
+                BG = B(:,:,:,iGaus);
                 C = squeeze(Cmat(:, :, iGaus, :));
                 dV = permute(dVolu(iGaus, :), [3, 1, 2]);
-                BtCB = pagemtimes(pagemtimes(permute(B, [2, 1, 3]), C), B);
+                BtCB = pagemtimes(pagemtimes(permute(BG, [2, 1, 3]), C), BG);
                 t = BtCB .* dV;
                 lhs = lhs + t;
             end
 
 
+
             %lhs2 = lhs;
 
-% nDim = obj.mesh.ndim;
-% nVoigt = nDim * (nDim + 1) / 2;
-% j = reshape((0:nNode-1) * obj.test.ndimf, 1, nNode, 1);
-% B = zeros(nVoigt, obj.test.ndimf * nNode, nElem, nGaus);
-% 
-% d = permute(deriv(1:nDim, :, :, :), [1, 2, 4, 3]);
-% 
-% if nDim == 2
-%     B(1, j + 1, :, :) = d(1, :, :, :);
-%     B(2, j + 2, :, :) = d(2, :, :, :);
-%     B(3, j + 1, :, :) = d(2, :, :, :);
-%     B(3, j + 2, :, :) = d(1, :, :, :);
-% elseif nDim == 3
-%     B(1, j + 1, :, :) = d(1, :, :, :);
-%     B(2, j + 2, :, :) = d(2, :, :, :);
-%     B(3, j + 3, :, :) = d(3, :, :, :);
-%     B(4, j + 1, :, :) = d(2, :, :, :);
-%     B(4, j + 2, :, :) = d(1, :, :, :);
-%     B(5, j + 1, :, :) = d(3, :, :, :);
-%     B(5, j + 3, :, :) = d(1, :, :, :);
-%     B(6, j + 2, :, :) = d(3, :, :, :);
-%     B(6, j + 3, :, :) = d(2, :, :, :);
-% end
-% 
+
+
 % C = permute(Cmat, [1, 2, 4, 3]);
 % dV = reshape(dVolu, [1, 1, nElem, nGaus]);  % Ensure alignment with BtCB
 % 
