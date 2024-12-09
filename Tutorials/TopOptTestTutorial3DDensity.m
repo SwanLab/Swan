88026891f2d1338459f1f6a4c161ba66443b58d2
gerard-gjv <gerard.jorba.vigo@gmail.com>
2024-12-09 18:20:58 +0100

    3d vtu

--- Tutorials/TopOptTestTutorial3DDensity.m
+++ Tutorials/TopOptTestTutorial3DDensity.m
@@ -170,17 +170,18 @@ classdef TopOptTestTutorial3DDensity < handle
             s.constraint     = obj.constraint;
             s.designVariable = obj.designVariable;
             s.dualVariable   = obj.dualVariable;
-            s.maxIter        = 1000;
+            s.maxIter        = 500;
             s.tolerance      = 1e-8; 
             s.constraintCase = {'EQUALITY'};
             s.primal         = 'PROJECTED GRADIENT'; 
             s.ub             = 1;
             s.lb             = 0;
             s.etaNorm        = 0.02;
-            s.gJFlowRatio    = 0.2; 
+            s.gJFlowRatio    = 2; 
             opt = OptimizerNullSpace(s);
             opt.solveProblem();
             obj.optimizer = opt;
+            obj.designVariable.fun.print('Density_3D_2'); %Guarda la simulació automàticament per poder veure-la després a paraview
         end
 
         function bc = createBoundaryConditions(obj)
