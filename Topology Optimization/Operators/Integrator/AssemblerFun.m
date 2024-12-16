20a625710c2a185244d7b9fd25741f2169f14853
jose-antonio-torres <jtorreslerma@icloud.com>
2024-12-16 12:49:10 +0100

    Update AssemblerFun.m

--- Topology Optimization/Operators/Integrator/AssemblerFun.m
+++ Topology Optimization/Operators/Integrator/AssemblerFun.m
@@ -11,35 +11,30 @@ classdef AssemblerFun < handle
         end
 
         function A = assemble(obj, Aelem, f1, f2)
-            
             dofsF1 = f1.getDofConnec();
             if isequal(f1, f2)
                 dofsF2 = dofsF1;
             else
                 dofsF2 = f2.getDofConnec();
             end
+            nDofs1     = numel(f1.fValues);
+            nDofs2     = numel(f2.fValues);
+            ndofsElem1 = size(Aelem, 1);
+            ndofsElem2 = size(Aelem, 2);
+            
+            [iElem, jElem] = meshgrid(1:ndofsElem1, 1:ndofsElem2);
+            iElem = iElem(:);
+            jElem = jElem(:);
 
-            nElem = size(Aelem,3);
-            nDofs1 = numel(f1.fValues);
-            nDofs2 = numel(f2.fValues);
-            ndofsElem1 = size(Aelem,1);
-            ndofsElem2 = size(Aelem,2);
+            dofsI = dofsF1(:, iElem);
+            dofsJ = dofsF2(:, jElem);
 
-            res = zeros(ndofsElem1*ndofsElem2 * nElem, 3);
-            strt = 1;
-            fnsh = nElem;
-            for i = 1:ndofsElem1
-                dofsI = dofsF1(:,i);
-                for j = 1:ndofsElem2
-                    dofsJ = dofsF2(:,j);
-                    a = squeeze(Aelem(i,j,:));
-                    matRes = [dofsI, dofsJ, a];
-                    res(strt:fnsh,:) = matRes;
-                    strt = strt + nElem;
-                    fnsh = fnsh + nElem;
-                end
-            end
-            A = sparse(res(:,1), res(:,2), res(:,3), nDofs1, nDofs2);
+            rowIdx = dofsI(:);
+            colIdx = dofsJ(:);
+            Aval   = permute(Aelem,[3 1 2]);
+            values = Aval(:);
+
+            A = sparse(rowIdx, colIdx, values, nDofs1, nDofs2);
         end
 
         function V = assembleV(obj, F, fun)
