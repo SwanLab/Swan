53901e8c34b0bc18e6b4d18d05d4fd7253552b49
jose-antonio-torres <jtorreslerma@icloud.com>
2024-12-17 12:37:49 +0100

    mlx working

---
+++ Tutorials/AllTutorials.m
@@ -0,0 +1,11 @@
+% MLX files
+run('Tutorial00_Mesh.mlx');
+run('Tutorial02_FEMElasticity.mlx');
+run('Tutorial03_LevelSet.mlx');
+run('Tutorial03p2_UnfittedMeshFunction.mlx');
+run('Tutorial04_Filters.mlx');
+run('Tutorial08_Projectors.mlx');
+run('Tutorial09_ProjectorsQuadrilaterals.mlx');
+run('Tutorial10_AcademicProblem.mlx');
+
+% M files
