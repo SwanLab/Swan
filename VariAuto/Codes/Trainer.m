cbb9031e425fea6da1518663dfa8d3b6f6364c80
PauCliment <pcliments@gmail.com>
2024-12-20 11:56:08 +0100

    New codes for NN architecture studies

--- VariAuto/Codes/Trainer.m
+++ VariAuto/Codes/Trainer.m
@@ -2,13 +2,17 @@ classdef Trainer < handle
 
     properties (Access = public)
         isDisplayed
+
+        designVariable
+        objectiveFunction % !! costFunction => objectiveFunction?
+
+
     end
     
     properties (Access = protected) 
         xIter
         nPlot
-        costFunction
-        designVariable
+        costFunction % Remove
         Xtrain
         Ytrain
         Xtest
@@ -69,23 +73,9 @@ classdef Trainer < handle
             end
         end
 
-%         function plotMinimization(obj,iter)
-%             nIter = obj.nPlot;
-%             v = 0:nIter:iter-1;
-%             if iter > 1
-%             obj.plotCostRegErr(v);
-%             obj.plotEpsOpt(v)
-%             end
-%             if obj.optimizationProblem.data.nFeatures <= 2 %CUIDADO, Arreglar
-%                 obj.optimizationProblem.plotBoundary('contour')
-%             end
-%         end  
-
         function plotCostRegErr(obj,v)
             figure(obj.figureCost)
-            %semilogy(v,obj.costHist(2:end,1),'+-r',v,obj.costHist(2:end,3),'+-b',v,obj.costHist(2:end,2),'+-k')
             plot(v(2:end),obj.costHist(2:end,1),'d--b','MarkerFaceColor','b')
-            %legend('Fval','Loss','Regularization')
             xlabel('Iterations')
             ylabel('Function Values')
             title('Cost minimization')
@@ -96,14 +86,6 @@ classdef Trainer < handle
 
         function plotEpsOpt(obj,v)
             figure(obj.figureOpt)
-%             subplot(2,1,1)
-%             plot(v,obj.optHist(2:end,1),'+-k')
-%             yline(1,'-','Gtol Criteria')
-%             xlabel('Iterations')
-%             ylabel('Optimalty criteria')
-%             title('Gradient norm vs iter')
-%             xlim([10,inf])
-%             subplot(2,1,2)
             plot(v(2:end),obj.optHist(2:end,2),'d--g','MarkerFaceColor','g')
             xlabel('Iterations')
             ylabel('Learning Rate')
