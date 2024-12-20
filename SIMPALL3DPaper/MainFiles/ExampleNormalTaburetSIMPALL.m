2d249f913c9aa28aa4d2dc679c13d6fd1ac7269a
jose-antonio-torres <jtorreslerma@icloud.com>
2024-12-20 13:18:00 +0100

    normal taburet

---
+++ SIMPALL3DPaper/MainFiles/ExampleNormalTaburetSIMPALL.m
@@ -0,0 +1,220 @@
+classdef ExampleNormalTaburetSIMPALL < handle
+
+    properties (Access = private)
+        mesh
+        filter
+        designVariable
+        materialInterpolator
+        material
+        physicalProblem
+        compliance
+        volume
+        cost
+        constraint
+        dualVariable
+        optimizer
+    end
+
+    methods (Access = public)
+
+        function obj = ExampleNormalTaburetSIMPALL()
+            obj.init()
+            obj.createMesh();
+            obj.createDesignVariable();
+            obj.createFilter();
+            obj.createMaterialInterpolator();
+            obj.createMaterial();
+            obj.createElasticProblem();
+            obj.createComplianceFromConstiutive();
+            obj.createCompliance();
+            obj.createVolumeConstraint();
+            obj.createCost();
+            obj.createConstraint();
+            obj.createDualVariable();
+            obj.createOptimizer();
+
+            saveas(gcf,'SIMPALL3DPaper/MatVoidCase/MonitoringNormalTaburetSIMPALL.fig');
+            obj.designVariable.fun.print('SIMPALL3DPaper/MatVoidCase/NormalTaburetSIMPALL');
+        end
+
+    end
+
+    methods (Access = private)
+
+        function init(obj)
+            close all;
+        end
+
+        function createMesh(obj)   
+            obj.mesh = HexaMesh(1,1,1,50,50,50);
+        end
+
+        function createDesignVariable(obj)
+            s.mesh             = obj.mesh;
+            s.order            = 'P1';
+            s.fValues          = ones(obj.mesh.nnodes,1);
+            s.fun              = LagrangianFunction(s);
+            s.type             = 'Density';
+            s.plotting         = false;
+            dens               = DesignVariable.create(s);
+            obj.designVariable = dens;
+        end
+
+        function createFilter(obj)
+            s.filterType = 'PDE';
+            s.mesh       = obj.mesh;
+            s.trial      = obj.designVariable.fun;
+            f            = Filter.create(s);
+            h            = obj.mesh.computeMeanCellSize();
+            f.updateEpsilon(2*h);
+            obj.filter = f;
+        end
+
+        function createMaterialInterpolator(obj)
+            E0   = 1e-3;
+            nu0  = 1/3;
+            ndim = obj.mesh.ndim;
+            matA.shear = IsotropicElasticMaterial.computeMuFromYoungAndPoisson(E0,nu0);
+            matA.bulk  = IsotropicElasticMaterial.computeKappaFromYoungAndPoisson(E0,nu0,ndim);
+
+
+            E1  = 1;
+            nu1 = 0.49;
+            matB.shear = IsotropicElasticMaterial.computeMuFromYoungAndPoisson(E1,nu1);
+            matB.bulk  = IsotropicElasticMaterial.computeKappaFromYoungAndPoisson(E1,nu1,ndim);
+
+            s.interpolation  = 'SIMPALL';
+            s.dim            = '3D';
+            s.matA = matA;
+            s.matB = matB;
+
+            m = MaterialInterpolator.create(s);
+            obj.materialInterpolator = m;
+        end
+
+        function createMaterial(obj)
+            f = obj.designVariable.fun;
+            s.type                 = 'DensityBased';
+            s.density              = f;
+            s.materialInterpolator = obj.materialInterpolator;
+            s.dim                  = '3D';
+            s.mesh                 = obj.mesh;
+            obj.material = Material.create(s);
+        end
+
+        function createElasticProblem(obj)
+            s.mesh = obj.mesh;
+            s.scale = 'MACRO';
+            s.material = obj.material;
+            s.dim = '3D';
+            s.boundaryConditions = obj.createBoundaryConditions();
+            s.interpolationType = 'LINEAR';
+            s.solverType = 'REDUCED';
+            s.solverMode = 'DISP';
+            s.solverCase = 'CG';
+            fem = ElasticProblem(s);
+            obj.physicalProblem = fem;
+        end
+
+        function c = createComplianceFromConstiutive(obj)
+            s.mesh         = obj.mesh;
+            s.stateProblem = obj.physicalProblem;
+            c = ComplianceFromConstiutiveTensor(s);
+        end
+
+        function createCompliance(obj)
+            s.mesh                        = obj.mesh;
+            s.filter                      = obj.filter;
+            s.complainceFromConstitutive  = obj.createComplianceFromConstiutive();
+            s.material                    = obj.material;
+            c = ComplianceFunctional(s);
+            obj.compliance = c;
+        end
+
+        function createVolumeConstraint(obj)
+            s.mesh = obj.mesh;
+            s.gradientTest = LagrangianFunction.create(obj.mesh,1,'P1');
+            s.volumeTarget = 0.6;
+            v = VolumeConstraint(s);
+            obj.volume = v;
+        end
+
+        function createCost(obj)
+            s.shapeFunctions{1} = obj.compliance;
+            s.weights           = 1;
+            s.Msmooth           = obj.createMassMatrix();
+            obj.cost            = Cost(s);
+        end
+
+        function M = createMassMatrix(obj)
+            n = obj.mesh.nnodes;
+            h = obj.mesh.computeMinCellSize();
+            M = h^2*sparse(1:n,1:n,ones(1,n),n,n);
+        end
+
+        function createConstraint(obj)
+            s.shapeFunctions{1} = obj.volume;
+            s.Msmooth           = obj.createMassMatrix();
+            obj.constraint      = Constraint(s);
+        end
+
+        function createDualVariable(obj)
+            s.nConstraints   = 1;
+            l                = DualVariable(s);
+            obj.dualVariable = l;
+        end
+
+        function createOptimizer(obj)
+            s.monitoring     = true;
+            s.cost           = obj.cost;
+            s.constraint     = obj.constraint;
+            s.designVariable = obj.designVariable;
+            s.dualVariable   = obj.dualVariable;
+            s.maxIter        = 2000;
+            s.tolerance      = 1e-8;
+            s.constraintCase = {'EQUALITY'};
+            s.ub             = 1;
+            s.lb             = 0;
+            s.primal         = 'PROJECTED GRADIENT';
+            s.physicalProblem = obj.physicalProblem;
+            opt = OptimizerMMA(s);
+            opt.solveProblem();
+            obj.optimizer = opt;
+        end
+
+        function bc = createBoundaryConditions(obj)
+            xMax = max(obj.mesh.coord(:,1));
+            yMax = max(obj.mesh.coord(:,2));
+            zMax = max(obj.mesh.coord(:,3));
+            isDir   = @(coor)  coor(:,1)==0;
+            isForce = @(coor)  coor(:,1)==xMax & coor(:,2)>=0.4*yMax & coor(:,2)<=0.6*yMax & coor(:,3)>=0.4*zMax & coor(:,3)<=0.6*zMax;
+
+            sDir{1}.domain    = @(coor) isDir(coor);
+            sDir{1}.direction = [1,2,3];
+            sDir{1}.value     = 0;
+
+            sPL{1}.domain    = @(coor) isForce(coor);
+            sPL{1}.direction = 1;
+            sPL{1}.value     = -1;
+
+            dirichletFun = [];
+            for i = 1:numel(sDir)
+                dir = DirichletCondition(obj.mesh, sDir{i});
+                dirichletFun = [dirichletFun, dir];
+            end
+            s.dirichletFun = dirichletFun;
+
+            pointloadFun = [];
+            for i = 1:numel(sPL)
+                pl = PointLoad(obj.mesh, sPL{i});
+                pointloadFun = [pointloadFun, pl];
+            end
+            s.pointloadFun = pointloadFun;
+
+            s.periodicFun  = [];
+            s.mesh         = obj.mesh;
+            bc = BoundaryConditions(s);
+        end
+
+    end
+end
