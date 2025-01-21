dbca98b6fc74e2b709ea73d42964ab82fa8d8985
jose-antonio-torres <jtorreslerma@icloud.com>
2025-01-21 20:15:06 +0100

    Swan Odyssey - Episode 2

---
+++ src/Integrators/RHS/RHSIntegratorFactory.m
@@ -0,0 +1,53 @@
+classdef RHSIntegratorFactory < handle
+
+    methods (Access = public, Static)
+
+        function obj = create(cParams)
+            switch cParams.type
+                case 'ShapeFunction'
+                    obj = RHSIntegratorShapeFunction(cParams);
+                case 'ShapeFunctionRT'
+                    % Computes the RHS using the NODAL FORCES and SHAPE
+                    % FUNCTIONS
+                    obj = RHSintegrator_ShapeFunctionRT(cParams);
+                case 'ShapeFunctionN'
+                    % Computes the RHS using the NODAL FORCES and SHAPE
+                    % FUNCTIONS
+                    obj = RHSintegrator_ShapeFunctionN(cParams);
+                case 'ShapeDerivative'
+                    % Computes the RHS using the NODAL FORCES and SHAPE
+                    % FUNCTIONS' DERIVATIVE
+                    obj = RHSintegrator_ShapeDerivative(cParams);
+                case 'ShapeSymmetricDerivative'
+                    % Computes the RHS using the NODAL FORCES and SHAPE
+                    % FUNCIONS' SYMMETRIC GRADIENT
+                    obj = RHSintegrator_ShapeSymmDerivative(cParams);
+                case 'CutMesh'
+                    % Computes the RHS using the NODAL FORCES and SHAPE
+                    % FUNCTIONS for CUT meshes
+                    obj = RHSintegrator_CutMesh(cParams);
+                case {'Composite','COMPOSITE'}
+                    % Computes the RHS as a COMPOSITE of other RHS
+                    % integrators
+                    obj = RHSintegrator_Composite(cParams);
+                    
+                case 'Elastic'
+                    % Computes the RHS for ELASTIC problems
+                    switch cParams.scale
+                        case 'MACRO'
+                            obj = RHSintegrator_ElasticMacro(cParams);
+                        case 'MICRO'
+                            obj = RHSintegrator_ElasticMicro(cParams);
+                    end
+                case 'ElasticMicro'
+                    obj = RHSintegrator_ElasticMicro(cParams);
+                case 'Stokes'
+                    obj = RHSintegrator_Stokes(cParams);
+                case 'Unfitted'
+                    obj = RHSIntegratorUnfitted(cParams);
+            end
+        end
+
+    end
+
+end
