6486b68730ad04c504f6489fd25e7d28ac0afb20
dcollv <desiree.coll@estudiantat.upc.edu>
2025-01-24 12:33:04 +0100

    New data

--- Desi_TFG/Prova1.m
+++ Desi_TFG/Prova1.m
@@ -12,7 +12,7 @@ learningRate    = 0.1;
 hiddenLayers    = [3,2,1,2,3];      
 
 %% INITIALIZATION
-s.fileName = '/OpenArms/Resultados1.csv';          
+s.fileName = '/OpenArms/Resultados2.csv';          
 s.polynomialOrder = pol_deg;        %% Orden del polinomio
 s.testRatio       = testratio;      %% Proporción de datos de prueba
 s.xFeatures = [1, 2];
@@ -30,13 +30,31 @@ s.costParams.lambda             = lambda;
 
 opt = OptimizationProblem(s);
 opt.solve();
-%opt.plotRegressionResults(); 
+%opt.plotRegressionResults();
 
 
 % if data.nFeatures == 2  % If you want to be asked for Features change it in "Data" Class
 %     opt.plotRegressionBoundary();
 % end
 
-% %% Evaluación: MSE = R² para regresión
-% MSE = mean((data.Ytest - opt.predict(data.Xtest)).^2);  %% Error cuadrático medio
-% fprintf('Mean Squared Error (MSE): %.4f\n', MSE);
+
+%% Función para hacer predicciones en nuevas situaciones
+function predicciones = predecirValor(inputData)
+    % inputData es un vector de situaciones que queremos predecir
+    % Asegúrate de que la estructura 'opt' y el modelo de regresión
+    % están disponibles, en caso de que desees usar el modelo entrenado.
+    
+    % Predecir usando la red entrenada
+    predicciones = opt.predict(inputData); 
+end
+
+%% Ejemplo de uso: predicción para nuevas situaciones
+nuevasSituaciones = [5, 10;   % Ejemplo de entrada: [x1, x2]
+                     2, 8];   % Ejemplo de entrada: [x1, x2]
+
+% Llamada a la función de predicción
+resultados = predecirValor(nuevasSituaciones);
+
+% Mostrar los resultados
+disp('Predicciones para las nuevas situaciones:');
+disp(resultados);
