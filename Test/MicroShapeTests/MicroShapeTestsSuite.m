203d5862e21e15ca53f1cfecf633938353e13e03
jose-antonio-torres <jtorreslerma@icloud.com>
2025-02-06 18:55:34 +0100

    starting micro shapes

--- Test/MicroShapeTests/MicroShapeTestsSuite.m
+++ Test/MicroShapeTests/MicroShapeTestsSuite.m
@@ -3,7 +3,7 @@ classdef MicroShapeTestsSuite < handle
     methods
 
         function obj = MicroShapeTestsSuite()
-            path = './tests/Source/MicroShapeTests/MicroShapeTests.m';
+            path = './Test/MicroShapeTests/MicroShapeTests.m';
             suite = matlab.unittest.TestSuite.fromFile(path, 'Tag','MicroShape');
             results = suite.run;
             table(results)
