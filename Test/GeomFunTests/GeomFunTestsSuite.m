7d483836935aa6585e6745291cae37d3f6370f59
jose-antonio-torres <jtorreslerma@icloud.com>
2025-02-09 18:56:22 +0100

    geom fun OK

--- Test/GeomFunTests/GeomFunTestsSuite.m
+++ Test/GeomFunTests/GeomFunTestsSuite.m
@@ -3,7 +3,7 @@ classdef GeomFunTestsSuite < handle
     methods
 
         function obj = GeomFunTestsSuite()
-            path = './tests/Source/GeomFunTests/GeomFunTests.m';
+            path = './Test/GeomFunTests/GeomFunTests.m';
             suite = matlab.unittest.TestSuite.fromFile(path, 'Tag','GeomFun');
             results = suite.run;
             table(results)
