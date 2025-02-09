8bde7a42b33e74cb048ab26ead87fea7b112e4f6
jose-antonio-torres <jtorreslerma@icloud.com>
2025-02-09 18:48:19 +0100

    bound cond OK

--- Test/BoundaryConditionsTests/BoundaryCondTestsSuite.m
+++ Test/BoundaryConditionsTests/BoundaryCondTestsSuite.m
@@ -2,7 +2,7 @@ classdef BoundaryCondTestsSuite < handle & matlab.unittest.TestCase
     
     methods (Access = public)
         function obj = BoundaryCondTestsSuite()
-            path = './tests/Source/BoundaryConditionsTests/BoundaryCondTests.m';
+            path = './Test/BoundaryConditionsTests/BoundaryCondTests.m';
             suite = matlab.unittest.TestSuite.fromFile(path, 'Tag', 'Monolithic' );
             results = suite.run;
             table(results)
