a6bae2ae4db8f82adc9deb17d977be1c146e6788
Gerard Villalta <villaltagerard@gmail.com>
2025-02-14 13:01:33 +0100

    Small changes

--- PhaseFieldExperiments/MainFiles/HomogenizationProcess/Homogenisation.m
+++ PhaseFieldExperiments/MainFiles/HomogenizationProcess/Homogenisation.m
@@ -24,41 +24,77 @@
 % [mat,phi,holeParam] = PFH.computeHomogMaterial();
 % save("RectangleMicroDamageArea","mat","phi","holeParam")
 
-clc,clear,close all
-s.E          = 1;
-s.nu         = 0.3;
-s.meshType   = 'Square';
-s.meshN      = 150;
-s.holeType   = 'Ellipse';
-s.nSteps     = [100,100];
-s.pnorm      = 'Inf';
-s.damageType = "Perimeter";
-PFH = PhaseFieldHomogenizer(s);
-[mat,phi,holeParam] = PFH.computeHomogMaterial();
-save("EllipseMicroDamagePerimeter","mat","phi","holeParam")
+% clc,clear,close all
+% s.E          = 1;
+% s.nu         = 0.3;
+% s.meshType   = 'Square';
+% s.meshN      = 150;
+% s.holeType   = 'Ellipse';
+% s.nSteps     = [100,100];
+% s.pnorm      = 'Inf';
+% s.damageType = "Perimeter";
+% PFH = PhaseFieldHomogenizer(s);
+% [mat,phi,holeParam] = PFH.computeHomogMaterial();
+% save("EllipseMicroDamagePerimeter","mat","phi","holeParam")
+% 
+% clc,clear,close all
+% s.E          = 1;
+% s.nu         = 0.3;
+% s.meshType   = 'Square';
+% s.meshN      = 150;
+% s.holeType   = 'Ellipse';
+% s.nSteps     = [100,100];
+% s.pnorm      = 'Inf';
+% s.damageType = "Perimeter";
+% PFH = PhaseFieldHomogenizer(s);
+% [mat,phi,holeParam] = PFH.computeHomogMaterial();
+% save("RectangleMicroDamagePerimeter","mat","phi","holeParam")
 
 clc,clear,close all
 s.E          = 1;
 s.nu         = 0.3;
-s.meshType   = 'Square';
+s.meshType   = 'Hexagon';
 s.meshN      = 150;
-s.holeType   = 'Ellipse';
-s.nSteps     = [100,100];
+s.holeType   = 'SmoothHexagon';
+s.nSteps     = [10];
 s.pnorm      = 'Inf';
 s.damageType = "Area";
 PFH = PhaseFieldHomogenizer(s);
 [mat,phi,holeParam] = PFH.computeHomogMaterial();
-save("RectangleMicroDamagePerimeter","mat","phi","holeParam")
+save("PhaseFieldExperiments/PFVademecum/Degradation/HexagonArea","mat","phi","holeParam")
 
+% clc,clear,close all
+% s.E          = 1;
+% s.nu         = 0.3;
+% s.meshType   = 'Hexagon';
+% s.meshN      = 150;
+% s.holeType   = 'Hexagon';
+% s.nSteps     = [100];
+% s.pnorm      = 'Inf';
+% s.damageType = "Perimeter";
+% PFH = PhaseFieldHomogenizer(s);
+% [mat,phi,holeParam] = PFH.computeHomogMaterial();
+% save("PhaseField/PFVademecum/Degradation/HexagonPerimeter","mat","phi","holeParam")
+
+%% PLOTS 
 
 % for i=1:9
 %     subplot(3,3,i)
 %     i1 = ceil(i/3);
 %     i2 = rem(i-1,3)+1;
 %     if ismember(i,[1,2,4,5,9])
-%     surf(holeParam{1},holeParam{2},squeeze(mat(i1,i2,:,:)))
+%     s = surf(holeParam{1},holeParam{2},squeeze(mat(i1,i2,:,:)));
 %     xlabel('m2')
 %     ylabel('m1')
+%     rotate(s,[0 0 1],180)
 %     end
+%     str = sprintf('C%d%d',i1,i2);
+%     title(str)
 % end
-% title('Ellipse homogenization')
+% sgtitle('Ellipse homogenization')
+% 
+% figure()
+% s = surf(holeParam{1},holeParam{2},squeeze(phi));
+% xlabel('m2')
+% ylabel('m1')
+% sgtitle('Rectangle Perimeter Damage')
