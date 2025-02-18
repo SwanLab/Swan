0e7b09dbe0d7def3c67bc3717fc9d073b0f7ff7e
Gerard Villalta <villaltagerard@gmail.com>
2025-02-18 17:16:22 +0100

    plot to tiledlayout

--- PhaseFieldExperiments/MainPhaseFieldHomogenizer.m
+++ PhaseFieldExperiments/MainPhaseFieldHomogenizer.m
@@ -1,22 +1,23 @@
 %% RUN
-clc,clear,close all
-s.E          = 1;
-s.nu         = 0.3;
-s.meshType   = 'Square';
-s.meshN      = 150;
-s.holeType   = 'Ellipse';
-s.nSteps     = [2,2];
-s.pnorm      = 'Inf';
-s.damageType = "Area";
-PFH = TestingPhaseFieldHomogenizer(s);
-[mat,phi,holeParam] = PFH.compute();
+% clc,clear,close all
+% s.E          = 1;
+% s.nu         = 0.3;
+% s.meshType   = 'Square';
+% s.meshN      = 150;
+% s.holeType   = 'Square';
+% s.nSteps     = [2];
+% s.pnorm      = 'Inf';
+% s.damageType = "Area";
+% PFH = TestingPhaseFieldHomogenizer(s);
+% [mat,phi,holeParam] = PFH.compute();
 
 
 %% SAVE + PLOTS 
 %save("EllipseMicroDamageArea","mat","phi","holeParam")
-
+figure()
+tiledlayout(3,3)
 for i=1:9
-    subplot(3,3,i)
+    nexttile
     i1 = ceil(i/3);
     i2 = rem(i-1,3)+1;
     if ismember(i,[1,2,4,5,9])
@@ -24,12 +25,16 @@ for i=1:9
     xlabel('m2')
     ylabel('m1')
     view(2)
-    shading interp
+    colormap turbo
+    %shading interp
+    caxis([0,1])
     end
     str = sprintf('C%d%d',i1,i2);
     title(str)
 end
 sgtitle('Ellipse homogenization')
+cb = colorbar;
+cb.Layout.Tile = 'East';
 
 % figure()
 % s = surf(holeParam{1},holeParam{2},squeeze(phi));
