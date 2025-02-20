ae67cc37ef0463b9a28b2e99212a67a648f46082
tarrut <91079521+tarrut@users.noreply.github.com>
2025-02-20 10:21:04 +0100

    Update navier_stokes_pautarres.m

--- navier_stokes_pautarres.m
+++ navier_stokes_pautarres.m
@@ -1,33 +1,50 @@
 mesh = UnitQuadMesh(10,10);
 
+e.type  = 'STOKES';
+e.nelem = mesh.nelem;
+material = Material.create(e);
+dtime = Inf;
+
 u = LagrangianFunction.create(mesh,2,'P2');
 p = LagrangianFunction.create(mesh,2,'P1');
+n_dofs = u.nDofs + p.nDofs;
+
 
 isLeft   = @(coor) (abs(coor(:,1) - min(coor(:,1)))   < 1e-12);
 isRight  = @(coor) (abs(coor(:,1) - max(coor(:,1)))   < 1e-12);
 isBottom = @(coor) (abs(coor(:,2) - min(coor(:,2)))   < 1e-12);
 isTop    = @(coor) (abs(coor(:,2) - max(coor(:,2)))   < 1e-12);
 
-sDir{1}.domain    = @(coor) isLeft(coor) | isRight(coor) | isTop(coor) | isBottom(coor);
-sDir{1}.direction = [1,2];
-sDir{1}.value     = 0;
+dir_vel{2}.domain    = @(coor) isTop(coor) | isBottom(coor);
+dir_vel{2}.direction = [1,2];
+dir_vel{2}.value     = [0,0]; 
 
-sDir{2}.domain    = @(coor) isRight(coor) & isTop(coor);
-sDir{2}.direction = 1;
-sDir{2}.value     = 0;
+dir_vel{1}.domain    = @(coor) isLeft(coor) & not(isTop(coor) | isBottom(coor));
+dir_vel{1}.direction = [1,2];
+dir_vel{1}.value     = [1,0];
 
-velocityBC.domain = 'Border';
-velocityBC.value  = 0;
+dirichlet = [];
+dir_dofs = [];
+for i = 1:length(dir_vel)
+    dirDofs = u.getDofsFromCondition(dir_vel{i}.domain);
+    nodes = 1 + (dirDofs(2:2:end)-2)/u.ndimf;
+    nodes2 = repmat(nodes, [1 2]);
+    iNod = sort(nodes2(:));
+    mat12 = repmat([1;2], [length(iNod)/2 1]);
+    valmat = repmat(dir_vel{i}.value', [length(iNod)/2 1]);
+    dirichlet(size(dirichlet,1)+1:size(dirichlet,1)+length(iNod),:) = [iNod mat12 valmat];
+    dir_dofs(size(dir_dofs,1)+1:size(dir_dofs,1)+length(iNod),1) = dirDofs;
+end
 
-inBC.pressure  = cParams.bc.pressure;
-inBC.velocity  = cParams.bc.velocity;
-inBC.pointload = [];
-inBC.velocityBC    = cParams.bc.velocityBC;
-inBC.forcesFormula = cParams.bc.forcesFormula;
-obj.inputBC    = inBC;
+% DEFINE APPLIED FORCES
+sAF.fHandle = @(coor) [0.*coor,0.*coor];
+sAF.ndimf   = 2;
+sAF.mesh    = mesh;
+forcesFormula = AnalyticalFunction(sAF);
 
-
-
+% CREATE SOLVER
+b.type =  'DIRECT';
+solver = Solver.create(b);
 
 
 
@@ -52,10 +69,61 @@ LHS = [K D; D', BB];
 
 
 
-a.type = 'ShapeFunction';
-a.mesh = mesh;
-a.quadType = 3;
-rhsI = RHSintegrator.create(a);
-test = u;
-Fext = rhsI.compute(obj.forcesFormula,test);
+d.type          = 'Stokes';
+d.mesh          = mesh;
+d.velocityFun   = u;
+d.pressureFun   = p;
+d.forcesFormula = forcesFormula;
+RHSint = RHSintegrator.create(d);
+F = RHSint.integrate();
+uD = dirichlet(:,3);
+R  = -LHS(:,dir_dofs)*uD;
+RHS = F + R;
+
+
+
+
+
+
+n_dofs
+
+% SOLVE PROBLEM
+% free_dofs_plus = setdiff(1:(n_dofs+1),dir_dofs);
+free_dofs_plus = setdiff(1:n_dofs,dir_dofs);
+LHSr = LHS(free_dofs_plus,free_dofs_plus); %Li treiem els nodes restringits per deixar la LHS només amb lliures i la RHS de la mateixa mida
+RHSr = RHS(free_dofs_plus);
+x = solver.solve(LHSr, RHSr);
+% x(end)=[];
+
+% ADD DDIRICHLET BOUNDARY CONDITIONS
+uD  = dirichlet(:,3);
+nsteps = length(x(1,:));
+uD = repmat(uD,1,nsteps);
+fullx = zeros(n_dofs,nsteps);
+free_dofs = setdiff(1:(n_dofs),dir_dofs);
+fullx(free_dofs,:) = x;
+if ~isempty(dir_dofs)
+    fullx(dir_dofs,:) = uD;
+end
+
+% SEPARATE VARIABLES FVALUES
+ndofsV = u.nDofs;
+vars.u = fullx(1:ndofsV,:);
+vars.p = fullx(ndofsV+1:end,:);
+
+% DEFINE VARIABLES
+nu = u.ndimf;
+nnode = round(length(vars.u)/nu);
+nodes = 1:nnode;
+velfval = zeros(nnode,nu);
+for idim = 1:nu
+    dofs = nu*(nodes-1)+idim;
+    velfval(:,idim) = vars.u(dofs, end);
+end
+u.fValues = velfval;
+p.fValues = vars.p(:,end);
+
+% PLOT RESULTS
+velocityFun.plot()
+pressureFun.plot()
 
