dc5c65adba5d32a0ed1d7e0fa42cbefc1c68c49e
jose-antonio-torres <jtorreslerma@icloud.com>
2025-02-21 15:33:03 +0100

    cherry pick monitoring

--- src/Monitoring/DisplayBar.m
+++ src/Monitoring/DisplayBar.m
@@ -12,14 +12,22 @@ classdef DisplayBar < DisplayPlot
 
         function refresh(obj)
             if ~isempty(obj.ArrayDataX) && ~isempty(obj.ArrayDataY)
-                axes = obj.obtainDisplayAxes();
-                set(axes,'XTick',obj.ArrayDataX);
+                axes  = obj.obtainDisplayAxes();
+                nTick = 5;
+                nP    = length(obj.ArrayDataX);
+                if nP<=nTick
+                    set(axes,'XTick',obj.ArrayDataX);
+                else
+                    step = ceil(nP/nTick);
+                    set(axes,'XTick',obj.ArrayDataX(1:step:end));
+                end
                 set(obj.handle,'XData',obj.ArrayDataX,'YData',obj.ArrayDataY);
                 if obj.ArrayDataY(end)>0
                     set(obj.style,'XLim',[min(0,min(obj.ArrayDataX)), max(0,max(obj.ArrayDataX)+1)])
                     set(obj.style,'YLim',[min(-0.01,min(obj.ArrayDataY)), max(0,max(obj.ArrayDataY)+1)])
                 end
             end
+            grid off;
         end
 
     end
