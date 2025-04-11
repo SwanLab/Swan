6f3a23977d79b59a686e29fd7ac61af340dd799d
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2025-04-11 12:42:08 +0200

    Update ElasticProblemMicro.m

--- src/Problems/Elasticity/ElasticProblemMicro.m
+++ src/Problems/Elasticity/ElasticProblemMicro.m
@@ -30,43 +30,17 @@ classdef ElasticProblemMicro < handle
             nBasis = obj.computeNbasis();
             obj.Chomog = zeros(nBasis, nBasis);
             for iB = 1:nBasis
-                strainBase = obj.createDeformationBasis(iB);
-                RHS      = obj.computeRHS(strainBase,LHS);
-                uF{iB}    = obj.computeDisplacement(LHS,RHS,iB,nBasis);
-                strnFluc{iB} = SymGrad(uF{iB});
-                str{iB} = obj.computeStrain(strainBase,strnFluc{iB});
-                sigma{iB} = DDP(obj.material, str{iB});
-                Ch(:,iB) = obj.computeChomog(sigma{iB});
+                strainB     = obj.createDeformationBasis(iB);
+                RHS         = obj.computeRHS(strainB,LHS);
+                uF{iB}      = obj.computeDisplacement(LHS,RHS,iB,nBasis);
+                strainF{iB} = strainB+SymGrad(uF{iB});
+                stressF{iB} = DDP(obj.material, strainF{iB});
+                Ch(:,iB)    = obj.computeChomog(stressF{iB},iB);
             end
-            obj.uFluc = uF;
-            obj.strain = str;
-            obj.stress = sigma;
+            obj.uFluc  = uF;
+            obj.strain = strainF;
+            obj.stress = stressF;
             obj.Chomog = Ch;
-        end
-
-        function s = createDeformationBasis(obj,iBasis)
-            nBasis = obj.computeNbasis();
-            sV = zeros(nBasis,1);
-            sV(iBasis) = 1;
-            s = ConstantFunction.create(sV,obj.mesh);
-        end
-
-        function nBasis = computeNbasis(obj)
-            homogOrder = 1;
-            nDim = obj.mesh.ndim;
-            nBasis = homogOrder*nDim*(nDim+1)/2;
-        end
-
-        function LHS = computeLHS(obj)
-            ndimf = obj.trialFun.ndimf;
-            s.type     = 'ElasticStiffnessMatrix';
-            s.mesh     = obj.mesh;
-            s.test     = LagrangianFunction.create(obj.mesh,ndimf, 'P1');
-            s.trial    = obj.trialFun;
-            s.material = obj.material;
-            s.quadratureOrder = 2;
-            lhs = LHSIntegrator.create(s);
-            LHS = lhs.compute();
         end
 
         function v = computeGeometricalVolume(obj)
@@ -104,6 +78,19 @@ classdef ElasticProblemMicro < handle
             obj.trialFun = LagrangianFunction.create(obj.mesh, obj.mesh.ndim, 'P1');
         end
 
+       function s = createDeformationBasis(obj,iBasis)
+            nBasis = obj.computeNbasis();
+            sV = zeros(nBasis,1);
+            sV(iBasis) = 1;
+            s = ConstantFunction.create(sV,obj.mesh);
+        end
+
+        function nBasis = computeNbasis(obj)
+            homogOrder = 1;
+            nDim = obj.mesh.ndim;
+            nBasis = homogOrder*nDim*(nDim+1)/2;
+        end
+
         function dim = getFunDims(obj)
             d.ndimf  = obj.trialFun.ndimf;
             d.nnodes = size(obj.trialFun.fValues, 1);
@@ -131,6 +118,18 @@ classdef ElasticProblemMicro < handle
             obj.problemSolver = ProblemSolver(s);
         end
 
+        function LHS = computeLHS(obj)
+            ndimf = obj.trialFun.ndimf;
+            s.type     = 'ElasticStiffnessMatrix';
+            s.mesh     = obj.mesh;
+            s.test     = LagrangianFunction.create(obj.mesh,ndimf, 'P1');
+            s.trial    = obj.trialFun;
+            s.material = obj.material;
+            s.quadratureOrder = 2;
+            lhs = LHSIntegrator.create(s);
+            LHS = lhs.compute();
+        end
+
         function rhs = computeRHS(obj,strainBase,LHS)
             s.fun  = obj.trialFun;
             s.type = 'ElasticMicro';
@@ -151,65 +150,40 @@ classdef ElasticProblemMicro < handle
             s.nBasis    = nBasis;
             [u, L]      = obj.problemSolver.solve(s);
             obj.lagrangeMultipliers = L;
-
             uSplit = reshape(u,[obj.mesh.ndim,obj.mesh.nnodes])';
             uFun = copy(obj.trialFun);
             uFun.setFValues(uSplit);            
-
-            % z.mesh    = obj.mesh;
-            % z.fValues = reshape(u,[obj.mesh.ndim,obj.mesh.nnodes])';
-            % z.order   = 'P1';
-            % uFeFun = LagrangianFunction(z);
-            % obj.uFun{iVoigt} = uFeFun;
-
-          %  uSplit = reshape(u,[obj.mesh.ndim,obj.mesh.nnodes])';
-          %  obj.displacementFun.setFValues(uSplit);
         end
 
-        function strainFun = computeStrain(obj, strainBase,strainFluc)
-            s.operation    = @(xV) strainBase.evaluate(xV)+strainFluc.evaluate(xV);
-            s.mesh         = obj.mesh;
-            s.ndimf        = size(strainBase,1);
-            strainFun      = DomainFunction(s);
-        end
-
-        %% 
-
-        function Chomog = computeChomog(obj,stress)
-            Chomog = Integrator.compute(stress,obj.mesh,2);
+        function Chomog = computeChomog(obj,stress,iBase)
+            if strcmp(obj.solverMode, 'DISP')
+                Chomog = computeChomogFromLagrangeMultipliers(obj,iBase);
+            else
+                Chomog = Integrator.compute(stress,obj.mesh,2);
+            end
         end
 
         function Chomog = computeChomogFromLagrangeMultipliers(obj,iBase)
-           if strcmp(obj.solverMode, 'DISP')
-                L = obj.lagrangeMultipliers;
-                nPeriodic = length(obj.boundaryConditions.periodic_leader);
-                nBorderNod = nPeriodic/4; % cause 2D
-                Lx  = sum( L(1:nBorderNod) );
-                Lxy = sum( L(nBorderNod+1:2*nBorderNod));
-                Ly  = sum( L(2*nBorderNod+1 : 3*nBorderNod));
-                Ld = L(3*nBorderNod+1 : end); % dirich (2 per + 6 dir)
-                switch iBase
-                    case 1
-                        Lx = Lx + Ld(1) + Ld(2) + Ld(3) + Ld(5);
-                        Ly = Ly + Ld(4) + Ld(7);
-                    case 2
-                        Ly = Ly + Ld(1) + Ld(2) + Ld(4) + Ld(6);
-                        Lx = Lx + Ld(3) + Ld(7);
-                    case 3
-                        Lxy = Lxy + Ld(1) + Ld(2);
-                end
-                Chomog = [-Lx; -Ly; -Lxy];
-           else
-               error('Not implemented');
-           end
+            L = obj.lagrangeMultipliers;
+            nPeriodic = length(obj.boundaryConditions.periodic_leader);
+            nBorderNod = nPeriodic/4; % cause 2D
+            Lx  = sum( L(1:nBorderNod) );
+            Lxy = sum( L(nBorderNod+1:2*nBorderNod));
+            Ly  = sum( L(2*nBorderNod+1 : 3*nBorderNod));
+            Ld = L(3*nBorderNod+1 : end); % dirich (2 per + 6 dir)
+            switch iBase
+                case 1
+                    Lx = Lx + Ld(1) + Ld(2) + Ld(3) + Ld(5);
+                    Ly = Ly + Ld(4) + Ld(7);
+                case 2
+                    Ly = Ly + Ld(1) + Ld(2) + Ld(4) + Ld(6);
+                    Lx = Lx + Ld(3) + Ld(7);
+                case 3
+                    Lxy = Lxy + Ld(1) + Ld(2);
+            end
+            Chomog = [-Lx; -Ly; -Lxy];
         end
 
-        function n = createFunctionNames(obj, name)
-            nStre = numel(obj.uFluc);
-            nums = 1:nStre;
-            n = cellstr([repmat(name, [nStre,1]), num2str(nums')])';
-        end
-        
     end
 
     
