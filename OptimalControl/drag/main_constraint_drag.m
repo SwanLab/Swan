f95d85cef0a1bb06c7cc092558b55420fa29a515
XaviPozo <168111392+XaviPozo@users.noreply.github.com>
2025-05-12 23:53:54 +0200

    Update main_constraint_drag.m

--- OptimalControl/drag/main_constraint_drag.m
+++ OptimalControl/drag/main_constraint_drag.m
@@ -11,7 +11,7 @@ function main_constraint_drag
     Clalpha = 2*pi;
     Cl0 = 0;
 
-    N = 500; % Discretization
+    N = 50; % Discretization
 
     x1_0 = 0; x2_0 = 0; v0 = 15;
     gamma0 = deg2rad(40);
@@ -25,22 +25,25 @@ function main_constraint_drag
     lb = [lb(1) ones(1,N)*lb(2)];
     ub = [ub(1) ones(1,N)*ub(2)];
 
+    % Previous calculations (Derivatives)
+
     Cl = @(alpha) Cl0 + Clalpha*alpha;
     Cd = @(alpha) Cd0 + k*Cl(alpha).^2;
-    D = @(V,alpha) 0.5*rho*Sw*V.^2.*Cd(alpha)/m;
+    D = @(V,alpha) 0.5*rho*Sw*V.^2.*Cd(alpha);
    
-    dCl = Clalpha;
-    dCd = @(alpha) 2*k*Cl(alpha)*dCl;
-    dDda = @(V,alpha) 0.5*rho*Sw*V.^2.*dCd(alpha)/m;
+    dCl = Clalpha; % Derivative respecto to alpha
+    dCd = @(alpha) 2*k*Cl(alpha)*dCl; % Derivative respect to alpha
+    dDda = @(V,alpha) 0.5*rho*Sw*V.^2.*dCd(alpha); % Derivative respect to alpha
 
-    dDragdv = @(V,alpha) rho*Sw*V.*Cd(alpha)/m;
+    dDdv = @(V,alpha) rho*Sw*V.*Cd(alpha); % Derivative respect to velocity
 
+    cost = @(u) f_cost(u, g, t0, v0, gamma0, x1_0, x2_0, D, dDda, dDdv, N, m);
+    constraint = @(u) groundConstraint(u, g, t0, v0, x1_0, x2_0, gamma0, D, dDda, dDdv, N, m);
 
-    cost = @(u) f_cost(u, g, t0, v0, gamma0, x1_0, x2_0, D,dDda,dDragdv, N);
-    constraint = @(u) groundConstraint(u, g, t0, v0, x1_0, x2_0, gamma0, D,dDda,dDragdv, N);
-
-    [valid,err] = checkGradients(cost, u0, "Display","on")
-    [valid2,err2] = checkGradients(constraint, u0, "Display","on")
+    [valid,err] = checkGradients(cost, u0, 'Tolerance', 1e-3, 'Display', 'on');
+    assignin('base', 'err', err);
+    [valid2,err2] = checkGradients(constraint, u0, 'Tolerance', 1e-3, 'Display', 'on')
+    assignin('base', 'err2', err2);
 
     options = optimoptions("fmincon", ...
         "OutputFcn", @store_fmincon, ...
@@ -51,7 +54,7 @@ function main_constraint_drag
 
     y0 = [x1_0 x2_0 v0 gamma0];
     t_span = linspace(t0, u_opt(1), N);
-    [~, y] = ode45(@(t, y) dynamics(t, y, tf, u_opt(2:N+1), g, D, N), t_span, y0);
+    [~, y] = ode45(@(t, y) dynamics(t, y, tf, u_opt(2:N+1), g, D, N, m), t_span, y0);
 
     disp(["Maximum distance [m] = ", num2str(y(end, 1))])
     disp(["Initial angle [°]: ", num2str(rad2deg(u_opt(2)))])
@@ -74,7 +77,6 @@ function main_constraint_drag
     for i = 1:length(vars)
         nodrag_results.(vars(i).name) = eval(vars(i).name); 
     end
-    save('nodrag_data.mat', 'nodrag_results');
 
     function stop = store_fmincon(u, optimValues, state)
         if optimValues.iteration == 0
@@ -97,82 +99,87 @@ function [c, ceq, Dc, Dceq] = nonlcon(u,constraint)
     [ceq, Dceq] = constraint(u);
 end
 
-function [J, gradJ] = f_cost(u, g, t0, v0, gamma0, x1_0, x2_0,Drag,dDragdv,dDragda, N)
+function [J, gradJ] = f_cost(u, g, t0, v0, gamma0, x1_0, x2_0, D, dDdv, dDda, N, m)
     y0 = [x1_0 x2_0 v0 gamma0];
     tf = u(1);
     alpha = u(2:N+1);
     t_span = linspace(t0, u(1), N);    
-    [t, y] = ode45(@(t, y) dynamics(t, y, tf, alpha, g, Drag, N), t_span, y0);
+    [~, y] = ode45(@(t, y) dynamics(t, y, tf, alpha, g, D, N, m), t_span, y0);
 
     J = -y(end,1);
-    dydt_final = dynamics(t_span(end), y(end,:)', tf, alpha, g, Drag, N);
+    dydt_final = dynamics(t_span(end), y(end,:)', tf, alpha, g, D, N, m);
 
     pT = [1 0 0 0];
-    [~, p] = ode45(@(t, p) adjoint(t, p, y, g, t_span, alpha, dDragdv), flip(t_span), pT);
+    [~, p] = ode45(@(t, p) adjoint(t, p, y, g, t_span, alpha, dDdv, m), flip(t_span), pT);
     
 
-    ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
-    v = y(ind,3);    
+    % ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
+    % v = y(ind,3);    
     %v = interp1(t_span, y(:,3), t);
-    DFdu = -dDragda(v,alpha');
+    v = y(:,3);
+    DFdu = -dDda(v,alpha')./m;
 
-    gradJ = [-dydt_final(1); -DFdu.*p(:,3)];
+    % gradJ = [-dydt_final(1); DFdu.*p(:,3)];
+    gradJ = zeros(N+1,1);
+    gradJ(1) = -dydt_final(1);
+    for i = 1:N
+    gradJ(i+1) = DFdu(i) * p(i,3);
+    end
 end
 
-function [ceq, Dceq] = groundConstraint(u, g, t0, v0, x1_0, x2_0, gamma0, Drag,dDragdv,dDragda, N)
+function [ceq, Dceq] = groundConstraint(u, g, t0, v0, x1_0, x2_0, gamma0, D, dDdv, dDda, N, m)
     y0 = [x1_0 x2_0 v0 gamma0]';
     tf = u(1);
     t_span = linspace(t0, tf, N);
     alpha = u(2:N+1);
-    [t, y] = ode45(@(t, y) dynamics(t, y, tf, alpha, g, Drag, N), t_span, y0);
+    [~, y] = ode45(@(t, y) dynamics(t, y, tf, alpha, g, D, N, m), t_span, y0);
 
     ceq = y(end,2);
-    dydt_final = dynamics(t_span(end), y(end,:)', tf, alpha, g, Drag, N);
+    dydt_final = dynamics(t_span(end), y(end,:)', tf, alpha, g, D, N, m);
 
     pT = [0 -1 0 0];
-    [~, p] = ode45(@(t, p) adjoint(t, p, y, g, t_span, alpha, dDragdv), flip(t_span), pT);
+    [~, p] = ode45(@(t, p) adjoint(t, p, y, g, t_span, alpha, dDdv, m), flip(t_span), pT);
    
+    % v = interp1(t_span, y(:,3), t);
+    % ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
+    % v = y(ind,3);    
+    v = y(:,3);
+    DFdu = -dDda(v,alpha');
 
-    %v = interp1(t_span, y(:,3), t);
-    ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
-    v = y(ind,3);    
-    DFdu = dDragda(v,alpha');
-
-    Dceq = [dydt_final(2); -DFdu.*p(:,3)];
+    Dceq = [dydt_final(2); DFdu.*p(:,3)];
 end
 
-function dpdt = adjoint(t, p, y, g, t_span, alpha, dDragdv)
-    %v = interp1(t_span, y(:,3), t);
-    %gamma = interp1(t_span, y(:,4), t);
-    %alpha = interp1(t_span, alpha, t);
+function dpdt = adjoint(t, p, y, g, t_span, alpha, dDdv, m)
+    v = interp1(t_span, y(:,3), t);
+    gamma = interp1(t_span, y(:,4), t);
+    alpha = interp1(t_span, alpha, t);
 
-    ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
-    v = y(ind,3);
-    gamma = y(ind,4);    
-    alpha = alpha(ind);
+    % ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
+    % v = y(ind,3);
+    % gamma = y(ind,4);    
+    % alpha = alpha(ind);
 
     J = [0 0 0 0;
          0 0 0 0;
-         cos(gamma) sin(gamma) -dDragdv(v,alpha') (g/v^2)*cos(gamma);
+         cos(gamma) sin(gamma) -dDdv(v,alpha')./m (g/v^2)*cos(gamma);
          -v*sin(gamma) v*cos(gamma) -g*cos(gamma) (g/v)*sin(gamma)];
     dpdt = -J*p;
 end
 
-function dydt = dynamics(t, y, tf, alpha, g, Drag, N)
+function dydt = dynamics(t, y, tf, alpha, g, D, N, m)
     v = y(3);
     gamma = y(4);
 
     t_span = linspace(0, tf, N);    
-    ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
-    alpha = alpha(ind);
-
+    % ind = round((t - t_span(1)) / (t_span(2) - t_span(1))) + 1;
+    % alpha = alpha(ind);
     
-    %alpha = interp1(t_span, alpha, t);
+    alpha = interp1(t_span, alpha, t);
 
     dydt = [
         v*cos(gamma);
         v*sin(gamma);
-        -g*sin(gamma)-Drag(v,alpha);
+        -g*sin(gamma)-D(v,alpha)/m;
         -(g/v)*cos(gamma)
     ];
 end
