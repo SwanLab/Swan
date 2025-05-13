954806aba8629c49ca1e69ed11b9f063a799a1a8
Gerard Villalta <villaltagerard@gmail.com>
2025-05-13 17:41:44 +0200

    repairing tests (not fully working)

--- src/Problems/PhaseField/Cases/MainPhaseFieldCase.m
+++ src/Problems/PhaseField/Cases/MainPhaseFieldCase.m
@@ -9,9 +9,9 @@ clc,clear,close all
 %load('case1ElemAT2.mat')
 
 %% GENERAL SETTINGS
-s.monitoring.set = true;
+s.monitoring.set = false;
 s.monitoring.type = 'full'; %'reduced'
-s.monitoring.print = true;
+s.monitoring.print = false;
 
 s.tolerance.u = 1e-13;
 s.tolerance.phi = 1e-6;
@@ -21,9 +21,9 @@ s.maxIter.phi = 300;
 s.maxIter.stag = 300;
 
 s.benchmark.N = 10;
-s.benchmark.type.mesh = '1Elem';
+s.benchmark.type.mesh = 'SENtest';
 s.benchmark.type.bc = 'displacementTraction';
-s.benchmark.bcValues = [0:0.001:0.1];
+s.benchmark.bcValues = [0:1e-3:0.1];
 
 s.matInfo.matType = 'Analytic';
 s.matInfo.degradationType = 'AT';
@@ -35,8 +35,11 @@ s.l0 = 0.1;
 
 s.dissipInfo.type = 'PhaseFieldDissipationAT';
 s.dissipInfo.pExp = 2;
-s.solver.type = 'Gradient';
+s.solver.type = 'Newton';
 s.solver.tau  = 150;
+
+input = s;
+save('testPhaseFieldSEN','input','xRef')
 
 %% RUN
 tester = TestingPhaseField(s);
