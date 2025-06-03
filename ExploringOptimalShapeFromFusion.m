49a1c7707443be0b2f32e566f856519b694ad7f2
rosa-galan <rosa.galan@estudiantat.upc.edu>
2025-06-03 09:50:14 +0200

    Applying BC

--- ExploringOptimalShapeFromFusion.m
+++ ExploringOptimalShapeFromFusion.m
@@ -6,9 +6,13 @@ classdef ExploringOptimalShapeFromFusion < handle
         young
         poisson   
         material
-        physicalProblem
+        stateProblem
         compliance
         volume
+        fractionVolume
+        designVariable
+        filter
+        
     end
 
     methods (Access = public)
@@ -17,6 +21,13 @@ classdef ExploringOptimalShapeFromFusion < handle
             obj.init()
             obj.createMesh();
             obj.createVolume();
+            obj.createDesignVariable();
+            obj.createFilter();
+            obj.computeElasticProperties();
+            obj.createMaterial();
+            obj.solveElasticProblem();
+            obj.createComplianceFromConstiutive();
+            obj.createCompliance();
         end
 
     end
@@ -37,15 +48,37 @@ classdef ExploringOptimalShapeFromFusion < handle
 
 
         function createVolume(obj)
-            volume = obj.mesh.computeVolume();
-            InitialVolume = 10;
-            fractionVolume = volume/InitialVolume;
-        end        
+            obj.volume = obj.mesh.computeVolume();
+            InitialVolume = 7.002e5;
+            obj.fractionVolume = obj.volume/InitialVolume;
+        end
+
+        function createDesignVariable(obj)
+            s.fHandle = @(x) ones(size(x(1,:,:)));
+            s.ndimf   = 1;
+            s.mesh    = obj.mesh;
+            aFun      = AnalyticalFunction(s);
+            
+            sD.fun      = aFun.project('P1');
+            sD.mesh     = obj.mesh;
+            sD.type     = 'Density';
+            sD.plotting = true;
+            dens        = DesignVariable.create(sD);
+            obj.designVariable = dens;
+        end
+
+        function createFilter(obj)
+            s.filterType = 'LUMP';
+            s.mesh  = obj.mesh;
+            s.trial = LagrangianFunction.create(obj.mesh,1,'P1');
+            f = Filter.create(s);
+            obj.filter = f;
+        end
 
 
         function computeElasticProperties(obj)
-            E  = 1;
-            nu = 1/3;
+            E  = 71e3; %1; % canviar?
+            nu = 1/3; % canviar?
             obj.young   = ConstantFunction.create(E,obj.mesh);
             obj.poisson = ConstantFunction.create(nu,obj.mesh);
         end
@@ -60,6 +93,69 @@ classdef ExploringOptimalShapeFromFusion < handle
             obj.material = tensor;
         end
 
+        function solveElasticProblem(obj)
+            s.mesh = obj.mesh;
+            s.scale = 'MACRO';
+            s.material = obj.material;
+            s.dim = '3D';
+            s.boundaryConditions = obj.createBoundaryConditions();
+            s.interpolationType = 'LINEAR';
+            s.solverType = 'REDUCED';
+            s.solverMode = 'DISP';
+            s.solverCase = 'DIRECT';
+            fem = ElasticProblem(s);
+            fem.solve();
+            obj.stateProblem = fem;
+        end
+
+        function c = createComplianceFromConstiutive(obj)
+            s.mesh         = obj.mesh;
+            s.stateProblem = obj.stateProblem;
+            c = ComplianceFromConstitutiveTensor(s);
+        end
+
+        function createCompliance(obj)
+            s.mesh                        = obj.mesh;
+            s.filter                      = obj.filter;
+            s.complainceFromConstitutive  = obj.createComplianceFromConstiutive();
+            s.material                    = obj.material; %obj.createMaterial();
+            c = ComplianceFunctional(s);
+            obj.compliance = c;
+        end
+        
+        function bc = createBoundaryConditions(obj)
+            xMax    = max(obj.mesh.coord(:,1));
+            yMax    = max(obj.mesh.coord(:,2));
+            zMax    = max(obj.mesh.coord(:,3));
+            isDir   = @(coor)  abs(coor(:,1))==20;
+            isForce = @(coor)  (abs(coor(:,1))==xMax & abs(coor(:,2))>=0.3*yMax & abs(coor(:,2))<=0.7*yMax & abs(coor(:,3))>=0.3*zMax & abs(coor(:,3))<=0.7*zMax);
+
+            sDir{1}.domain    = @(coor) isDir(coor);
+            sDir{1}.direction = [1,2,3];
+            sDir{1}.value     = 0;
+
+            sPL{1}.domain    = @(coor) isForce(coor);
+            sPL{1}.direction = 3;
+            sPL{1}.value     = -10000;
+
+            dirichletFun = [];
+            for i = 1:numel(sDir)
+                dir = DirichletCondition(obj.mesh, sDir{i});
+                dirichletFun = [dirichletFun, dir];
+            end
+            s.dirichletFun = dirichletFun;
+
+            pointloadFun = [];
+            for i = 1:numel(sPL)
+                pl = PointLoad(obj.mesh, sPL{i});
+                pointloadFun = [pointloadFun, pl];
+            end
+            s.pointloadFun = pointloadFun;
+
+            s.periodicFun  = [];
+            s.mesh         = obj.mesh;
+            bc = BoundaryConditions(s);
+        end
 
       
 
