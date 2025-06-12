fefbf7730b70d5e3b27816701b39ad7b87eaae9b
XaviPozo <168111392+XaviPozo@users.noreply.github.com>
2025-06-13 01:00:39 +0200

    buena pinta

--- OptimalControl/drag y lift/main_constraint_draglift.m
+++ OptimalControl/drag y lift/main_constraint_draglift.m
@@ -11,19 +11,19 @@ function main_constraint_draglift
     Cd0 = 0.03; % Drag coefficient
     m = 5; % Object mass [kg]
     k = 0.05;
-    %Clalpha = 5.7296; % en radianes
-    Clalpha = 0.1; % en grados
+    Clalpha = 0.1*360/(2*pi); % en radianes
+    %Clalpha = 0.1; % en grados
     Cl0 = 0;
 
-    N = 50; % Discretization
+    N = 100; % Discretization
 
     x1_0 = 0; x2_0 = 0; v0 = 15;
     gamma0 = deg2rad(30);
-    t0 = 0; tf = 10;
-    alpha0 = deg2rad(30);
+    t0 = 0; tf_guess = 10;
+    alpha0 = deg2rad(3);
     u0 = [resultsfalcon.tfinal resultsfalcon.u.alpha];
-    lb = [0 deg2rad(-10)]; % Lower bounds for the control
-    ub = [15 deg2rad(11)]; % Upper bounds for the control
+    lb = [0.1 deg2rad(-10)]; % Lower bounds for the control
+    ub = [15 deg2rad(10)]; % Upper bounds for the control
 
     %u0 = [u0(1) ones(1,N)*u0(2)];
     lb = [lb(1) ones(1,N)*lb(2)];
@@ -31,13 +31,13 @@ function main_constraint_draglift
 
     % Previous calculations (Derivatives)
 
-    Cl = @(alpha) Cl0 + Clalpha*rad2deg(alpha);
+    Cl = @(alpha) Cl0 + Clalpha*alpha;
     Cd = @(alpha) Cd0 + k*Cl(alpha).^2;
     D = @(V,alpha) 0.5*rho*Sw*V.^2.*Cd(alpha);
     L = @(V,alpha) 0.5*rho*Sw*V.^2.*Cl(alpha);   
 
     cost = @(u) f_cost(u, g, t0, v0, gamma0, x1_0, x2_0, D, N, m, L);
-    constraint = @(u) groundConstraint(u, g, t0, v0, x1_0, x2_0, gamma0, D, N, m, L);
+    nonlcon_fun = @(u) nonlcon_full(u, g, t0, v0, x1_0, x2_0, gamma0, D, N, m, L);
 
     options = optimoptions("fmincon", ...
         "OutputFcn", @store_fmincon, ...
@@ -45,14 +45,18 @@ function main_constraint_draglift
         "DerivativeCheck", "off","Display","iter","FiniteDifferenceType","central", ...
         "FiniteDifferenceStepSize", 1e-4);
 
-    [u_opt, ~] = fmincon(cost, u0, [], [], [], [], lb, ub, @(u) nonlcon(u,constraint), options);
+    [u_opt, ~] = fmincon(cost, u0, [], [], [], [], lb, ub, nonlcon_fun, options);
 
     y0 = [x1_0 x2_0 v0 gamma0];
-    t_span = linspace(t0, u_opt(1), N);
+    tf = u_opt(1);
+    t_span = linspace(t0, tf, N);
     [~, y] = ode45(@(t, y) dynamics(t, y, tf, u_opt(2:N+1), g, D, N, m, L), t_span, y0);
 
     disp(["Maximum distance [m] = ", num2str(y(end, 1))])
     disp(["Final Time [s]: ", num2str(u_opt(1))])
+
+    figure
+    plot(y(:,1),y(:,2))
 
     function stop = store_fmincon(u, optimValues, state)
         if optimValues.iteration == 0
@@ -70,11 +74,6 @@ function main_constraint_draglift
 
 end
 
-function [c, ceq] = nonlcon(u,constraint)
-    c = [];
-    [ceq] = constraint(u);
-end
-
 function [J] = f_cost(u, g, t0, v0, gamma0, x1_0, x2_0, D, N, m, L)
     y0 = [x1_0 x2_0 v0 gamma0];
     tf = u(1);
@@ -84,33 +83,34 @@ function [J] = f_cost(u, g, t0, v0, gamma0, x1_0, x2_0, D, N, m, L)
 
     J = -y(end,1);
 
-    figure(100); plot(y(:,1), y(:,2), 'b-+', 'LineWidth', 2);
-    xlabel("Horizontal distance [m]"); ylabel("Vertical distance [m]"); grid on;
-    xlim([0 200])
-    ylim([-50 50])
-
-    figure(101); plot(t_span, rad2deg(u(2:end)), 'b-+', 'LineWidth', 2);
-    xlabel("Time [t]"); ylabel("angle of attack [deg]"); grid on;
-    xlim([0 tf])
-    ylim([0 10])
-
-    figure(102); plot(t_span, (y(:,3)), 'b-+', 'LineWidth', 2);
-    xlabel("Time [t]"); ylabel("Velocity [m/s]"); grid on;
-    xlim([0 tf])
-    ylim([-50 50])
-
-    figure(103); plot(J);
-    hold on
+    % figure(100); plot(y(:,1), y(:,2), 'b-+', 'LineWidth', 2);
+    % xlabel("Horizontal distance [m]"); ylabel("Vertical distance [m]"); grid on;
+    % xlim([0 200])
+    % ylim([-50 50])
+    % 
+    % figure(101); plot(t_span, rad2deg(u(2:end)), 'b-+', 'LineWidth', 2);
+    % xlabel("Time [t]"); ylabel("angle of attack [deg]"); grid on;
+    % xlim([0 tf])
+    % ylim([0 10])
+    % 
+    % figure(102); plot(t_span, (y(:,3)), 'b-+', 'LineWidth', 2);
+    % xlabel("Time [t]"); ylabel("Velocity [m/s]"); grid on;
+    % xlim([0 tf])
+    % ylim([-50 50])
+    % 
+    % figure(103); plot(J);
+    % hold on
 
 end
 
-function [ceq] = groundConstraint(u, g, t0, v0, x1_0, x2_0, gamma0, D, N, m, L)
-    y0 = [x1_0 x2_0 v0 gamma0]';
-    tf = u(1);
+function [c, ceq] = nonlcon_full(u, g, t0, v0, gamma0, x1_0, x2_0, D, N, m, L)
+    tf    = u(1);
+    alpha = u(2:end);
     t_span = linspace(t0, tf, N);
-    alpha = u(2:N+1);
-    [~, y] = ode45(@(t, y) dynamics(t, y, tf, alpha, g, D, N, m, L), t_span, y0);
+    y0 = [x1_0; x2_0; v0; gamma0];
+    [~, y] = ode45(@(t,y)dynamics(t,y,tf,alpha,g,D,N,m,L), t_span, y0);
 
+    c = -y(:,2);
     ceq = y(end,2);
 
 end
