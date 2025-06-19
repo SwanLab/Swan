12533ec4219a4165e931f6fe5e25dfdd16458508
Biel Pujadas Suriol <66383311+Biel-PS@users.noreply.github.com>
2025-06-19 11:59:38 +0200

    Plot program

--- case1D_coolerPlots.m
+++ case1D_coolerPlots.m
@@ -6,7 +6,7 @@ close all;clear;
 % Material parameters
 E0 = 210;
 r0 = 10;
-r1 = 200000000000000;
+r1 = 2000000;
 A = 1;
 
 
@@ -16,7 +16,7 @@ uVals = 0:1e-3:10;
 % Define hardening and softening parameters
 q_infVals = [20, -0.1]; % H = +0.5 (hardening), H = -0.5 (softening)
 H_vals = [0.5, -0.5];
-labels = {'Analytic Hardening', 'Analytic Softening', 'Numerical Hardening','Numerical Softening'};
+labels = {'Hardening: q_{\infty} = 20', 'Softening: q_{\infty} = -0.1'};
 
 % Initialize storage for both cases
 dmg_all = {};
@@ -79,9 +79,9 @@ for h = 1:length(q_infVals)
     tau_all{end+1} = tauVec;
 end
 
-load('1ELEM_TRACTION_EXP_HARDENING.mat','data');
+load('1ELEM_TRACTION_LINEAR_HARDENING.mat','data');
 dataHard = data;
-load('1ELEM_TRACTION_EXP_SOFT.mat','data');
+load('1ELEM_TRACTION_LINEAR_SOFT.mat','data');
 dataSoft = data;
 
 % Plot Damage vs Displacement
@@ -91,8 +91,8 @@ hold on;
 plot(uVals, dmg_all{1}, 'LineWidth', 2);
 plot(uVals, dmg_all{2}, 'LineWidth', 2);
 
-plot(dataHard.displacement.value,dataHard.damage.maxValue,'o','MarkerSize',10,'MarkerIndices',1:30:length(dataHard.damage.maxValue),Color='#0072BD')
-plot(dataSoft.displacement.value,dataSoft.damage.maxValue,'square','MarkerSize',10,'MarkerIndices',1:20:length(dataSoft.damage.maxValue),Color='#D95319')
+% plot(dataHard.displacement.value,dataHard.damage.maxValue,'o','MarkerSize',10,'MarkerIndices',1:30:length(dataHard.damage.maxValue),Color='#0072BD')
+% plot(dataSoft.displacement.value,dataSoft.damage.maxValue,'square','MarkerSize',10,'MarkerIndices',1:20:length(dataSoft.damage.maxValue),Color='#D95319')
 
 xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
 ylabel('Damage (d)', 'Interpreter', 'latex');
@@ -101,9 +101,6 @@ legend(labels, 'Location', 'best');
 grid on;
 fontsize(gcf,15,'points')
 
-
-
-
 % Plot q vs Tau
 figure();
 hold on;
@@ -111,8 +108,8 @@ hold on;
 plot(uVals, q_all{1}, 'LineWidth', 2);
 plot(uVals, q_all{2}, 'LineWidth', 2);
 
-plot(dataHard.displacement.value,dataHard.q.maxValue,'o','MarkerSize',10,'MarkerIndices',1:30:length(dataHard.q.maxValue),Color='#0072BD')
-plot(dataSoft.displacement.value,dataSoft.q.maxValue,'square','MarkerSize',10,'MarkerIndices',1:20:length(dataSoft.q.maxValue),Color='#D95319')
+% plot(dataHard.displacement.value,dataHard.q.maxValue,'o','MarkerSize',10,'MarkerIndices',1:30:length(dataHard.q.maxValue),Color='#0072BD')
+% plot(dataSoft.displacement.value,dataSoft.q.maxValue,'square','MarkerSize',10,'MarkerIndices',1:20:length(dataSoft.q.maxValue),Color='#D95319')
 
 xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
 ylabel('Hardening law (q)', 'Interpreter', 'latex');
@@ -130,8 +127,8 @@ hold on;
 plot(uVals, F_all{1}, 'LineWidth', 2);
 plot(uVals, F_all{2}, 'LineWidth', 2);
 
-plot(dataHard.displacement.value,dataHard.reaction,'o','MarkerSize',10,'MarkerIndices',1:30:length(dataHard.q.maxValue),Color='#0072BD')
-plot(dataSoft.displacement.value,dataSoft.reaction,'square','MarkerSize',10,'MarkerIndices',1:20:length(dataSoft.q.maxValue),Color='#D95319')
+% plot(dataHard.displacement.value,dataHard.reaction,'o','MarkerSize',10,'MarkerIndices',1:30:length(dataHard.q.maxValue),Color='#0072BD')
+% plot(dataSoft.displacement.value,dataSoft.reaction,'square','MarkerSize',10,'MarkerIndices',1:20:length(dataSoft.q.maxValue),Color='#D95319')
 
 xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
 ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
@@ -152,3 +149,267 @@ fontsize(gcf,15,'points')
 % % title('Damage evolution parameter as a function of displacement');
 % legend(labels, 'Location', 'best');
 % grid on;
+
+%%
+clear; close all;
+
+load('Swan/Resultats_definitius/1Elem-PF/1Elem_AT1.mat');
+dataAT1_1elem = outputData;
+load('Swan/Resultats_definitius/SENtraction-PF/AT1traction.mat');
+dataAT1_Traction = outputData;
+load('Swan/Resultats_definitius/SENshear-PF/AT1shear.mat');
+dataAT1_Shear = outputData;
+%------------------------------------------
+load('Swan/Resultats_definitius/1Elem-PF/1Elem_AT2.mat');
+dataAT2_1elem = outputData;
+load('Swan/Resultats_definitius/SENtraction-PF/AT2traction.mat');
+dataAT2_Traction = outputData;
+load('Swan/Resultats_definitius/SENshear-PF/AT2shear.mat');
+dataAT2_Shear = outputData;
+%-------------------------------------------
+load('Swan/Resultats_definitius/1Elem-CD/1ELM_LINEAR_SOFTENING_H_05_R0_10_R1_25.mat');
+dataLinSoft_1elem = data;
+load('Swan/Resultats_definitius/SEN Traction CD/LinearTraction.mat');
+dataLinSoft_Traction = data;
+load('Swan/Resultats_definitius/SEN Shear CD/LinearShear.mat');
+dataLinSoft_Shear = data;
+%-------------------------------------------
+load('Swan/Resultats_definitius/1Elem-CD/1ELEM_TRACTION_EXP_SOFT.mat');
+dataExpSoft_1elem = data;
+load('Swan/Resultats_definitius/SEN Traction CD/SEN_TRACTION_EXP_SOFT_A_01_Qinf_2_R0_10.mat');
+dataExpSoft_Traction = data;
+load('Swan/Resultats_definitius/SEN Shear CD/ExpShear.mat');
+dataExpSoft_Shear = data;
+%-------------------------------------------
+load('Swan/Resultats_definitius/1Elem-CD/1ELM_LINEAR_HARDENING_H_05_R0_10_R1_25.mat');
+dataLinHard_1elem = data;
+load('Swan/Resultats_definitius/SEN Traction CD/SEN_TRACTION_LINEAR_HARD_H_05_R0_10_R1_20.mat');
+dataLinHard_Traction = data;
+load('Swan/Resultats_definitius/SEN Shear CD/SEN_SHEAR_LINEAR_HARD_H_05_R0_10_R1_20.mat');
+dataLinHard_Shear = data;
+%-------------------------------------------
+load('Swan/Resultats_definitius/1Elem-PF/1Elem-CD-AT1.mat');
+dataCdAT1_1elem = data;
+% dataCdAT1_Traction = data;
+% dataCdAT1_Shear = data;
+%-------------------------------------------
+load('Swan/Resultats_definitius/1Elem-PF/1Elem-CD-AT2.mat');
+dataCdAT2_1elem = data;
+load('Swan/Resultats_definitius/SEN Shear CD/SEN_TRACTION_Q_AT2_W1_3750.mat');
+dataCdAT2_Traction = data;
+load('Swan/Resultats_definitius/SENshear-PF/SEN_SHEAR_Q_AT2_W1_3750.mat')
+dataCdAT2_Shear = data;
+%-------------------------------------------
+% dataPfLinear_1elem = outputData;
+% dataPfLinear_Traction = outputData;
+% dataPfLinear_Shear = outputData;
+
+%% 1 ELEM CD
+
+figure ();
+hold on
+
+labels = {'Exponential Softening','Linear Softening', 'Linear Hardening'};
+data_cd = [dataExpSoft_1elem,dataLinSoft_1elem,dataLinHard_1elem];
+
+for i = 1:length(data_cd)
+    plot(data_cd(i).displacement.value,data_cd(i).reaction,'LineWidth', 2);
+end
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
+title('1Elem CD')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+
+%----------------------------------------------------------------------------------------------
+figure ();
+hold on
+
+labels = {'Exponential Softening','Linear Softening', 'Linear Hardening'};
+data_cd = [dataExpSoft_1elem,dataLinSoft_1elem,dataLinHard_1elem];
+
+for i = 1:length(data_cd)
+    plot(data_cd(i).displacement.value,data_cd(i).damage.maxValue,'LineWidth', 2);
+end
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Damage ($d$)', 'Interpreter', 'latex');
+title('1Elem CD')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+%----------------------------------------------------------------------------------------------
+%% 1 ELM PF
+
+figure ();
+hold on
+
+labels = {'AT1','AT2','CD-AT1','CD-AT2'};
+colorLabel = {'#0072BD' ,'#D95319'};
+markerLabel = {'o','square'};
+
+data_1elem_Pf_cd = [dataCdAT1_1elem,dataCdAT2_1elem];
+data_1elem_Pf = [dataAT1_1elem,dataAT2_1elem];
+for i = 1:length(data_1elem_Pf)
+    plot(data_1elem_Pf(i).displacement.value,data_1elem_Pf(i).force,'LineWidth', 2);
+end
+for i = 1:length(data_1elem_Pf_cd)
+        plot(data_1elem_Pf_cd(i).displacement.value, data_1elem_Pf_cd(i).reaction, ...
+         'LineStyle', 'none', ...
+         'Color', colorLabel{i}, ...
+         'Marker', markerLabel{i}, ...
+         'MarkerSize', 5, ...
+         'MarkerIndices', 1:10:length(data_1elem_Pf_cd(i).reaction));
+end
+
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
+title('1 Element PF')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+%-------------------------------------------------
+figure ();
+hold on
+
+labels = {'AT1','AT2','CD-AT1','CD-AT2'};
+colorLabel = {'#0072BD' ,'#D95319'};
+markerLabel = {'o','square'};
+
+data_1elem_Pf = [dataAT1_1elem,dataAT2_1elem];
+data_1elem_Pf_cd = [dataCdAT1_1elem,dataCdAT2_1elem];
+
+for i = 1:length(data_1elem_Pf)
+    plot(data_1elem_Pf(i).displacement.value,data_1elem_Pf(i).damage.maxValue,'LineWidth', 2);
+end
+for i = 1:length(data_1elem_Pf_cd)
+        plot(data_1elem_Pf_cd(i).displacement.value, data_1elem_Pf_cd(i).damage.maxValue, ...
+         'LineStyle', 'none', ...
+         'Color', colorLabel{i}, ...
+         'Marker', markerLabel{i}, ...
+         'MarkerSize', 5, ...
+         'MarkerIndices', 1:10:length(data_1elem_Pf_cd(i).reaction));
+end
+
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Damage ($d$)', 'Interpreter', 'latex');
+title('1 Element PF')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+
+%% Traction CD
+
+figure ();
+hold on
+
+labels = {'Exponential Softening','Linear Softening', 'Linear Hardening'};
+data_cd = [dataExpSoft_Traction,dataLinSoft_Traction,dataLinHard_Traction];
+
+for i = 1:length(data_cd)
+    plot(data_cd(i).displacement.value,data_cd(i).reaction,'LineWidth', 2);
+end
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
+title('SEN Traction CD')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+hold off
+%----------------------------------------------------------------------------------------------
+% for i = 1:length(data_cd)
+%     figure();
+%     hold on
+%     data_cd(i).damage.field.plot;
+%     title (labels{i})
+%     hold off;
+% end
+
+%% Traction PF
+
+figure ();
+hold on
+
+labels = {'AT1','AT2','CD-AT2'};
+colorLabel = {'#D95319'};
+markerLabel = {'square'};
+
+data_1elem_Pf = [dataAT1_Traction,dataAT2_Traction];
+data_1elem_Pf_cd = [dataCdAT2_Traction];
+
+for i = 1:length(data_1elem_Pf)
+    plot(data_1elem_Pf(i).displacement.value,data_1elem_Pf(i).force,'LineWidth', 2);
+end
+for i = 1:length(data_1elem_Pf_cd)
+        plot(data_1elem_Pf_cd(i).displacement.value, data_1elem_Pf_cd(i).reaction, ...
+         'LineStyle', 'none', ...
+         'Color', colorLabel{i}, ...
+         'Marker', markerLabel{i}, ...
+         'MarkerSize', 5, ...
+         'MarkerIndices', 1:10:length(data_1elem_Pf_cd(i).reaction));
+end
+
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
+title('SEN Traction PF')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+
+%% SHEAR CDM
+
+figure ();
+hold on
+
+labels = {'Exponential Softening','Linear Softening', 'Linear Hardening'};
+data_cd = [dataExpSoft_Shear,dataLinSoft_Shear,dataLinHard_Shear];
+
+for i = 1:length(data_cd)
+    plot(data_cd(i).displacement.value,data_cd(i).reaction,'LineWidth', 2);
+end
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
+title('SEN Shear CD')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
+hold off
+%----------------------------------------------------------------------------------------------
+% for i = 1:length(data_cd)
+%     figure();
+%     hold on
+%     data_cd(i).damage.field.plot;
+%     title (labels{i})
+%     hold off;
+% end
+
+%% Shear PF
+
+figure ();
+hold on
+
+labels = {'AT1','AT2','CD-AT2'};
+colorLabel = {'#D95319'};
+markerLabel = {'square'};
+
+data_1elem_Pf = [dataAT1_Shear,dataAT2_Shear];
+data_1elem_Pf_cd = [dataCdAT2_Shear];
+
+for i = 1:length(data_1elem_Pf)
+    plot(data_1elem_Pf(i).displacement.value,data_1elem_Pf(i).force,'LineWidth', 2);
+end
+for i = 1:length(data_1elem_Pf_cd)
+        plot(data_1elem_Pf_cd(i).displacement.value, data_1elem_Pf_cd(i).reaction, ...
+         'LineStyle', 'none', ...
+         'Color', colorLabel{i}, ...
+         'Marker', markerLabel{i}, ...
+         'MarkerSize', 5, ...
+         'MarkerIndices', 1:10:length(data_1elem_Pf_cd(i).reaction));
+end
+
+xlabel('Displacement ($\varepsilon$)', 'Interpreter', 'latex');
+ylabel('Reaction ($\sigma$)', 'Interpreter', 'latex');
+title('SEN Shear PF')
+legend(labels, 'Location', 'best');
+grid on;
+fontsize(gcf,15,'points')
