fb4a4197f1d351dba134fa3809af57a730a1c9ef
Gerard Villalta <villaltagerard@gmail.com>
2025-07-01 11:16:33 +0200

    Expand and RHSSymDeriv

--- src/Operators/Expand.m
+++ src/Operators/Expand.m
@@ -1,6 +1,11 @@
-function A = Expand(a)
+function A = Expand(varargin)
+    a = varargin{1};
+    ndim = 2;
+    if nargin==2
+        ndim=varargin{2};
+    end
     if isa(a,'BaseFunction')
-        s.operation = @(xV) evaluate(a,xV);
+        s.operation = @(xV) evaluate(a,ndim,xV);
         s.mesh = a.mesh;
         s.ndimf = a.ndimf;
         A = DomainFunction(s);
@@ -11,12 +16,13 @@ function A = Expand(a)
 
 end
 
-function aEval = evaluate(a,xV)
+function aEval = evaluate(a,ndim,xV)
     aEval      = a.evaluate(xV);
     isTensorA  = checkTensor(a,aEval);
     if ~isTensorA
         dims = size(aEval);
-        aEval = reshape(aEval,[dims(1), 1, dims(2:end)]);
+        extraDims = ones(1,ndim-1);
+        aEval = reshape(aEval,[dims(1), extraDims, dims(2:end)]);
     end
 end
 
