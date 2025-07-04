24595fb81141ffb28f531c38d5326a027301439b
Gerard Villalta <villaltagerard@gmail.com>
2025-07-04 17:43:06 +0200

    Update MainPhaseFieldHomogenizer.m

--- src/Problems/PhaseField/Homogenization/MainPhaseFieldHomogenizer.m
+++ src/Problems/PhaseField/Homogenization/MainPhaseFieldHomogenizer.m
@@ -3,9 +3,9 @@ s.monitoring = false;
 s.E          = 1;
 s.nu         = 0.3;
 s.meshType   = 'Square';
-s.meshN      = 200;
-s.holeType   = 'Circle';
-s.nSteps     = [100];
+s.meshN      = 50;
+s.holeType   = 'Ellipse';
+s.nSteps     = [2 2];
 s.pnorm      = 'Inf';
 s.damageType = 'Area';
 PFH = TestingPhaseFieldHomogenizer(s);
@@ -16,23 +16,4 @@ degradation.fun = f;
 degradation.dfun = df;
 degradation.ddfun = ddf;
 fplot(f,[0 1])
-save('CircleArea4T','mat','phi','degradation')
-
-s.monitoring = false;
-s.E          = 1;
-s.nu         = 0.3;
-s.meshType   = 'Square';
-s.meshN      = 100;
-s.holeType   = 'Circle';
-s.nSteps     = [100];
-s.pnorm      = 'Inf';
-s.damageType = 'Perimeter';
-PFH = TestingPhaseFieldHomogenizer(s);
-[mat,phi,holeParam] = PFH.compute();
-
-[f,df,ddf] = DamageHomogenizationFitter.computePolynomial(9,phi,mat);
-degradation.fun = f;
-degradation.dfun = df;
-degradation.ddfun = ddf;
-fplot(f,[0 1])
-save('CirclePerimeter4T','mat','phi','degradation')
+%save('CirclePerimeter','mat','phi','degradation')
