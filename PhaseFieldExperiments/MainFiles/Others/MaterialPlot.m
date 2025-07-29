ff7575fa6abc7e4f6599ebf31b678e8b1a7dc582
Gerard Villalta <villaltagerard@gmail.com>
2025-07-29 16:03:54 +0200

    MaterialPlot

---
+++ PhaseFieldExperiments/MainFiles/Others/MaterialPlot.m
@@ -0,0 +1,37 @@
+clc,clear,close all
+load('NewSet2.mat')
+
+figure()
+tiledlayout(1,3)
+nexttile
+fplot(degradation.fun{1,1,1,1},[0 1])
+hold on
+plot(phi,squeeze(mat(1,1,1,1,:)),'.')
+title('C11')
+
+nexttile
+fplot(degradation.fun{1,1,2,2},[0 1])
+hold on
+plot(phi,squeeze(mat(1,1,2,2,:)),'.')
+title('C12')
+
+nexttile
+fplot(degradation.fun{1,2,1,2},[0 1])
+hold on
+plot(phi,squeeze(mat(1,2,1,2,:)),'.')
+title('C33')
+
+figure()
+tiledlayout(1,3)
+nexttile
+fplot(degradation.dfun{1,1,1,1},[0 1])
+
+title('dC11')
+
+nexttile
+fplot(degradation.dfun{1,1,2,2},[0 1])
+title('dC12')
+
+nexttile
+fplot(degradation.dfun{1,2,1,2},[0 1])
+title('dC33')
