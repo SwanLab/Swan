667cb5d6ae7462e655b8608cd67b1daf181e2b6b
joelsendraUPC <joel.sendra@estudiantat.upc.edu>
2025-08-28 17:56:21 +0200

    Update of functional code

--- Tutorials/OpenArmsTutorial/Reinforcement/Offsets/generateDiagonalOffsets.m
+++
@@ -1,5 +0,0 @@
-function offsets = generateDiagonalOffsets(n_dims, n_tilings)
-    base = linspace(0, 1, n_tilings + 1);
-    base(end) = [];  % remove the last to avoid overlap
-    offsets = repmat(base, n_dims, 1);
-end
