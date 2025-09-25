a90971649b8e5711cc54419ff89632460b81551e
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2025-09-25 11:56:00 +0200

    Create ToDo.m

---
+++ ToDo.m
@@ -0,0 +1,6 @@
+% ToDo / Brainstorming
+
+% Add ModesFunction 2D
+% varargin as 3rd input solver.solve (solverSettings)
+% move checkStep outside PCG
+% @(r) obj.Preconditioner.solve(r,LHS) as an input
