a3e2362aee66e509cde0e190e874449f6b4442f6
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2025-10-02 13:03:55 +0200

    bug in vonmises found

--- src/Operators/VonMisesStress.m
+++ src/Operators/VonMisesStress.m
@@ -6,19 +6,27 @@ end
 
 function fEval = evaluate(stress, xV)
     m = stress.mesh;
-    for i=1:3
-        for j=1:3 
+    for i=1:m.ndim
+        for j=1:m.ndim
         base   = createBaseFunction(m,i,j);
         stressMatrix = DDP(stress,base);
         sigma{i,j} = stressMatrix.evaluate(xV);
         end    
     end
-    fEval = sqrt(0.5*((sigma{1,1}-sigma{2,2}).^2 + (sigma{2,2}-sigma{3,3}).^2 ...
+
+    switch m.ndim
+        case 2
+            fEval = sqrt( sigma{1,1}.^2 + sigma{2,2}.^2 ...
+                - sigma{1,1}.*sigma{2,2} ...
+                + 3*sigma{1,2}.^2 );
+        case 3
+          fEval = sqrt(0.5*((sigma{1,1}-sigma{2,2}).^2 + (sigma{2,2}-sigma{3,3}).^2 ...
                 + (sigma{3,3}-sigma{1,1}).^2 )+3*(sigma{1,2}.^2 + sigma{2,3}.^2 + sigma{1,3}.^2));
+    end
 end
 
 function base = createBaseFunction(m,i,j)
-    constant = zeros(3,3);
+    constant = zeros(2,2);
     constant(i,j) = 1;
     base = ConstantFunction.create(constant,m);
 end
