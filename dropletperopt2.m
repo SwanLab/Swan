bb95794de297d7164659d04562de988d072744db
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2025-10-02 11:06:14 +0200

    "working"

--- dropletperopt2.m
+++ dropletperopt2.m
@@ -1,4 +1,5 @@
-function dropletperopt
+function dropletperopt2
+
 nx=200; ny=150; nxy=nx*ny;
 eps=10;
 k=[1,1]; k=k'/norm(k); kx=k(1); ky=k(2);
@@ -28,31 +29,31 @@ disp('main loop');
 sigmacp=0.25; taucp=0.25; thetacp=1;
 taucpe=taucp/eps^2;
 for iopt=1:20
+V = PerimeterMinimization(U,nx,ny,nxy,alpha,sigmacp,k,Dx,Dy,ep,taucp,taucpe,thetacp);
+%pause;
+U = ProjectToVolumeConstraint(V,vol);
+sum(U)
+end
+
+end
+
+function V = PerimeterMinimization(U,nx,ny,nxy,alpha,sigmacp,k,Dx,Dy,ep,taucp,taucpe,thetacp)
 % Chambolle Pock
-%V=U; Vbar=U;
-V=zeros(nxy,1); Vbar=zeros(nxy,1);
+V=U; Vbar=U;
+%V=zeros(nxy,1); Vbar=zeros(nxy,1);
 Wx=zeros(nxy,1); Wy=zeros(nxy,1);
 for kcp=1:1000
     xix=Wx+sigmacp*Dx*Vbar; xiy=Wy+sigmacp*Dy*Vbar;
-    W = [Wx,Wy];
-    xi = W + sigmacp*[Dx*Vbar Dy*Vbar];
-    
     xi=[xix,xiy];
-
-
     W1=xi/(1+sigmacp);
     Wx1=W1(:,1); Wy1=W1(:,2);
     xik=xi(:,1)*k(1)+xi(:,2)*k(2);
-    xikperp=xi(:,1)*kperp(1)+xi(:,2)*kperp(2);
-    W2k=((alpha^4*(1+sigmacp)+sigmacp^2*(alpha^2-1))/(alpha^2*(alpha^2+sigmacp)*(1+sigmacp)))*xik-((sigmacp*sqrt(alpha^2-1))/(alpha^2*(1+sigmacp)))*abs(xikperp);
-    sig=real(xikperp>0)-real(xikperp<0);
-    W2kperp=((alpha^2+sigmacp)*xikperp-sigmacp*sqrt(alpha^2-1)*sig.*xik)/(alpha^2*(1+sigmacp));
-    W2=W2k*k'+W2kperp*kperp';
-    Wx2=W2(:,1); Wy2=W2(:,2);
-    noxi=sqrt(xi(:,1).^2+xi(:,2).^2);
-    case1=real(alpha*xik-noxi<0);
+    nxi2=xix.^2+xiy.^2;
+    deltaWx2=(sigmacp/(alpha^2*(1+sigmacp)))*(xix+(alpha^2-2)*k(1)*xik-sqrt((alpha^2-1)./(nxi2-xik.^2+ep)).*((nxi2-2*xik.^2)*k(1)+xik.*xix));
+    deltaWy2=(sigmacp/(alpha^2*(1+sigmacp)))*(xiy+(alpha^2-2)*k(2)*xik-sqrt((alpha^2-1)./(nxi2-xik.^2+ep)).*((nxi2-2*xik.^2)*k(2)+xik.*xiy));
+    noxi=sqrt(nxi2);
     case2=real(alpha*xik-noxi>0);
-    Wx=case1.*Wx1+case2.*Wx2; Wy=case1.*Wy1+case2.*Wy2;
+    Wx=Wx1+case2.*deltaWx2; Wy=Wy1+case2.*deltaWy2;
     Vold=V;
     zeta=Vold-taucp*Dx'*Wx-taucp*Dy'*Wy;
     V=(zeta+taucpe*U)/(1+taucpe);
@@ -66,7 +67,9 @@ figure(3); clf; image(C); set(gca,'xtick',[]); set(gca,'ytick',[]); axis image;
 
 Vt=reshape(V,nx,ny)';
 figure(2); clf; surf(Vt);
-%pause;
+end
+
+function U = ProjectToVolumeConstraint(V,vol)
 % Bissection
 lambdamin=0; lambdamax=1;
 for idic=1:100
@@ -80,12 +83,4 @@ for idic=1:100
     end
 end
 U=Utest;
-sum(U)
-end
-
-end
-
-
-function proxF()
-
 end
