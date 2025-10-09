73b48833fc93741f04fa382d658d064b606321ff
Gerard Villalta <villaltagerard@gmail.com>
2025-10-09 17:27:55 +0200

    Create ElasticProblemDavid.m

---
+++ ElasticProblemDavid.m
@@ -0,0 +1,40 @@
+classdef ElasticProblemDavid < handle
+    
+    properties (Access = public)
+        uFun
+        sigma
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    methods (Access = public)
+        
+        function obj = ElasticProblemDavid()
+            obj.init()
+            obj.createMesh()
+            obj.createMaterial()
+            obj.createBoundaryConditions()
+            obj.displacementFunction()
+            obj.createStiffness()
+            obj.createForce()
+            obj.computeNewDisplacement()
+            obj.computeStress()
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function init(obj)
+            
+        end
+        
+    end
+    
+end
