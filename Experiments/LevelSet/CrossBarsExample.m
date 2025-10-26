f6222922f53724b8aeb0fa3d35aa13acf9f91a6d
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2025-10-26 23:31:00 +0100

    Create CrossBarsExample.m

---
+++ Experiments/LevelSet/CrossBarsExample.m
@@ -0,0 +1,91 @@
+classdef CrossBarsExample < handle
+
+    properties (Access = private)
+        mesh
+        filter
+        designVariable
+        materialInterpolator
+        physicalProblem
+        compliance
+        volume
+        filterSegment
+        perimeter
+        cost
+        constraint
+        primalUpdater
+        optimizer
+    end
+
+    methods (Access = public)
+
+        function obj = CrossBarsExample()
+            obj.init()
+            obj.createMesh();
+            obj.createDesignVariable();
+            obj.createFilter();
+            obj.createSegmentFilter();
+            obj.createPerimeter();
+            obj.designVariable.fun.print('Experiments/LevelSet/CantileverLevelSetGlobalSegmentfValues');
+        end
+
+    end
+
+    methods (Access = private)
+
+        function init(obj)
+            close all;
+        end
+
+        function createMesh(obj)
+            obj.mesh = TriangleMesh(2,1,100,50);
+        end
+
+        function createDesignVariable(obj)
+            s.type = 'Full';
+            g      = GeometricalFunction(s);
+            lsFun  = g.computeLevelSetFunction(obj.mesh);
+            s.fun  = lsFun;
+            s.mesh = obj.mesh;
+            s.type = 'LevelSet';
+            s.plotting = false;
+            ls     = DesignVariable.create(s);
+            obj.designVariable = ls;
+        end
+
+        function createFilter(obj)
+            s.filterType = 'LUMP';
+            s.mesh  = obj.mesh;
+            s.trial = LagrangianFunction.create(obj.mesh,1,'P1');
+            f = Filter.create(s);
+            obj.filter = f;
+        end
+
+       function uMesh = createBaseDomain(obj)
+            levelSet         = -ones(obj.mesh.nnodes,1);
+            s.backgroundMesh = obj.mesh;
+            s.boundaryMesh   = obj.mesh.createBoundaryMesh();
+            uMesh = UnfittedMesh(s);
+            uMesh.compute(levelSet);
+        end
+
+        function createSegmentFilter(obj)
+            s.mesh  = obj.mesh;
+            s.alpha = 4;
+            s.beta  = 0;
+            s.theta = 90;
+            s.tol0  = 1e-6;
+            obj.filterSegment  = NonLinearFilterSegment(s);
+        end
+
+        function createPerimeter(obj)
+            h         = obj.mesh.computeMinCellSize();
+            s.mesh    = obj.mesh;
+            s.uMesh   = obj.createBaseDomain();
+            s.filter  = obj.filterSegment;
+            s.epsilon = 2*h;
+            s.value0  = 1;
+            obj.perimeter  = PerimeterFunctional(s);
+        end
+
+    end
+end
