cd565270fce57a143db8a92e2ef201deade52e9b
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2025-10-31 07:41:22 +0100

    "up"

---
+++ PerimeterExperiments/PerimeterVolumeChambollePock.m
@@ -0,0 +1,32 @@
+classdef PerimeterVolumeChambollePock < handle
+
+    properties (Access = public)
+
+    end
+
+    properties (Access = private)
+
+    end
+
+    properties (Access = private)
+
+    end
+
+    methods (Access = public)
+
+        function obj = PerimeterVolumeChambollePock(cParams)
+            obj.init(cParams)
+
+        end
+
+    end
+
+    methods (Access = private)
+
+        function init(obj,cParams)
+
+        end
+
+    end
+
+end
