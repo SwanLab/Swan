9b4ae0b608b69932e0729be7e8de2f62c39ebf8c
Nil Heinz <nheinz@virtual-fly.com>
2025-11-05 18:02:42 +0100

    plot eigencomparison

--- TutorialEIFEM.m
+++ TutorialEIFEM.m
@@ -57,10 +57,10 @@ classdef TutorialEIFEM < handle
             [lambdaCoarse, PhiCoarse, omega] = obj.computeModalAnalysis(Kcoarse, Mcoarse);
             
             %% Eigenvalues to print
-            obj.printContinuousSolution(EIFEM,PhiCoarse,mD);
+            %obj.printContinuousSolution(EIFEM,PhiCoarse,mD);
             %obj.printDiscontinuousSolution(EIFEM,PhiCoarse,discMesh);
             %obj.printCoarseSolution(EIFEM,PhiCoarse,discMesh);
-            %obj.printFineSolution(eigenvectors,mD);
+            obj.printFineSolution(eigenvectors,mD);
             
             % for i = 1:length(radius_to_analyse)
             % 
@@ -73,7 +73,24 @@ classdef TutorialEIFEM < handle
             % end
 
             %save('KcoarseTraining.mat', 'Kc');
-            %plot([lambdaCoarse,eigenvalues],"LineWidth",2, "Marker","+")
+            
+            
+            errPct = abs(eigenvalues - lambdaCoarse) ./ abs(eigenvalues) * 100;
+            
+            figure;
+            
+            yyaxis left
+            plot(lambdaCoarse, 'LineWidth', 2, 'Marker', 'x'); hold on
+            plot(eigenvalues, 'LineWidth', 2, 'Marker', 'x');
+            ylabel('Eigenvalues')
+            xlabel('Mode Index')
+            legend('Coarse','Fine','Error','Location','best')
+            
+            yyaxis right
+            plot(errPct, 'LineWidth', 1, 'Marker', 'x');
+            ylabel('Error (%)')
+            title('Fine vs Coarse Eigenvalues and Percentage Error')
+            grid on
             
             Milu         = obj.createILUpreconditioner(LHSr);
             Mmult        = @(r) Preconditioner.multiplePrec(r,LHSfun,Milu,Meifem,Milu);
