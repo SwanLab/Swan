742bb283919f9b4736de76558d20302ddab4fd83
Abril Bertran Garrigos <abriljinbg@gmail.com>
2025-11-25 01:32:00 +0100

    Update SVD_Abril.m

--- AbrilTFGfiles/SVD_Abril.m
+++ AbrilTFGfiles/SVD_Abril.m
@@ -3,11 +3,11 @@
 clc;
 clear
 
-r=0:0.05:0.999;
+r=0:0.02:0.999;
 
 for i=1:size(r,2)
-    string = strrep("UL_r"+num2str(r(i), '%.4f'), ".", "_")+"-20x20"+".mat"; 
-    FileName=fullfile('AbrilTFGfiles','DataVariables','20x20',string);
+    string = strrep("UL_r"+num2str(r(i), '%.4f'), ".", "_")+"-50x50"+".mat"; 
+    FileName=fullfile('AbrilTFGfiles','DataVariables','50x50',string);
     
     load(FileName,"T","mesh");
 
@@ -19,5 +19,58 @@ for i=1:size(r,2)
 
 end
 
-[U,S,V]=svd(T_SVD);
+[U,S,V]=svd(T_SVD,10);
+
+%% Graphics
+
+
+% PLot the V columns grouped in 10
+step=10;
+Nwindow=ceil(size(V,2)/step);
+idx=1;
+
+for j=1:Nwindow
+    figure('Position',[75 100 1400 600]);
+    tiledlayout(2,5,'TileSpacing','compact','Padding','compact');
+    for i=1:step
+        ax=nexttile;
+        plot(r,V(:,idx), 'LineWidth', 1.5);
+        xlabel('r');
+        ylabel("V(:,"+idx+")");
+        title("V column "+ idx);
+        grid on
+        idx=idx+1;
+    end
+end
+
+% Plot the S
+figure
+plot(log(diag(S)));
+title("S singular values");
+ylabel('value');
+xlabel('column');
+
+% Plot U
+%s.mesh=mesh;
+%s.order='P1';
+%n=10;
+%
+%Ufun=cell(1,n);
+%for i=1:n
+%    s.fValues=U(:,i);
+%    Ufun{1,i}=LagrangianFunction(s);
+%    Ufun{1,i}.plot();
+%end
+
+figure('Position',[75 100 1400 600]);
+tiledlayout(2,5,'TileSpacing','compact','Padding','compact');
+for i=1:10
+    ax=nexttile;
+    plot(U(:,i));
+    ylabel("U values");
+    title("U column "+ i);
+end
+
+
+
 
