7bd255794f09d3d1a5c0cf0418eb410a4c41e13a
rosa-galan <rosa.galan@estudiantat.upc.edu>
2025-11-25 23:11:11 +0100

    more simulations now correct

--- Anisotropic Simulation/LevelSet/Anisotropic_0_LevelSet.m
+++ Anisotropic Simulation/LevelSet/Anisotropic_0_LevelSet.m
@@ -169,7 +169,7 @@ classdef Anisotropic_0_LevelSet < handle
             s.cost           = obj.cost;
             s.constraint     = obj.constraint;
             s.designVariable = obj.designVariable;
-            s.maxIter        = 500;
+            s.maxIter        = 300;
             s.tolerance      = 1e-8;
             s.constraintCase = {'EQUALITY'};
             s.primalUpdater  = obj.primalUpdater;
