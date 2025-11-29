69c731eb37ef293a208ba571898758dc7d1e0740
DavidRJ2004 <david.rodrigo@estudiantat.upc.edu>
2025-11-29 20:36:57 +0100

    CohesiveMesh 2

--- CohesiveMesh.m
+++ CohesiveMesh.m
@@ -7,8 +7,13 @@ classdef CohesiveMesh < handle
         newCoord
         newConnec
 
+        listNodeCohesive
+        listElemCohesive
+
         nnodeCohesive
         pairsMatrix
+
+        separation
     end
     
     properties (Access = private)
@@ -33,27 +38,28 @@ classdef CohesiveMesh < handle
     methods (Access = private)
         
         function init(obj)
-            
+            obj.separation = 0.1;
         end
         
         function baseMesh(obj)
-            obj.mesh = UnitQuadMesh(3,3);
+            obj.mesh = UnitQuadMesh(2,2);
         end
 
         function duplicator(obj)
 
-
             ymin = min(obj.mesh.coord(:,2));
             obj.isCohesive = abs(obj.mesh.coord(:,2)) == ymin; 
             obj.nnodeCohesive = sum(obj.isCohesive == 1) * 2;
-            
+            obj.listNodeCohesive = find(obj.isCohesive == 1);
+
             obj.newCoord = obj.mesh.coord;
 
-
-            duplicated = obj.newCoord(obj.isCohesive == 1, :) + [0 , 0.1];
+            dispV      = [0, 1];
+            duplicated = obj.newCoord(obj.isCohesive == 1, :) + obj.separation*dispV;
             obj.newCoord = [obj.newCoord; duplicated];
 
-            obj.pairsMatrix = [find(obj.isCohesive == 1) , (size(obj.isCohesive,1)+1:1:obj.nnodeCohesive/2+size(obj.isCohesive,1))'];
+            obj.pairsMatrix = [obj.listNodeCohesive , (size(obj.isCohesive,1)+1:1:obj.nnodeCohesive/2+size(obj.isCohesive,1))'];
+            obj.pairsMatrix = [obj.pairsMatrix , dispV(1)*ones(obj.nnodeCohesive/2,1), dispV(2)*ones(obj.nnodeCohesive/2,1)];
 
             obj.isCohesive = [obj.isCohesive; ones(obj.nnodeCohesive/2,1)];
 
@@ -64,17 +70,16 @@ classdef CohesiveMesh < handle
         function updateConnec(obj)
             obj.newConnec = obj.mesh.connec;
             
+            isCohesiveElem = any(obj.isCohesive(obj.newConnec),2);
+            obj.listElemCohesive = find(isCohesiveElem == 1);
+
+            % No se com identificar quins s'han d'intercanviar d'una forma
+            % eficient
+
             cohesiveConnec = [obj.pairsMatrix(1:end-1,1), obj.pairsMatrix(2:end,1), obj.pairsMatrix(2:end,2), obj.pairsMatrix(1:end-1,2)];
-
-            for i = obj.pairsMatrix(:,1)
-                
-        
-            end
-
-
+            obj.newConnec = [obj.newConnec; cohesiveConnec];
 
         end
-
         
     end
     
