01049df40f4cae357ee6ab8d663d9fd353fd8e29
DavidRJ2004 <david.rodrigo@estudiantat.upc.edu>
2025-12-07 12:42:43 +0100

    Funcionals 1

---
+++ CohesiveFunctional.m
@@ -0,0 +1,46 @@
+classdef CohesiveFunctional < handle
+    
+    properties (Access = public)
+        mesh
+        material
+ 
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    methods (Access = public)
+        
+        function obj = CohesiveFunctional(cParams)
+            obj.init(cParams)
+            
+        end
+
+
+
+        function E = computeCost(obj,u,quadOrder, ????)
+            
+
+        end
+
+        function F = computeGradient(obj,u,quadOrder, ????)
+                
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function init(obj,cParams)
+            obj.mesh = cParams.mesh;
+            obj.material = cParams.material;
+        end
+        
+    end
+    
+end
---
+++ CohesiveProblemFunctional.m
@@ -0,0 +1,64 @@
+
+classdef CohesiveProblemFunctional < handle
+    
+
+    properties (Access = private)
+        functionals
+        quadOrder
+    end
+    
+    properties (Access = private)
+        
+    end
+    
+    methods (Access = public)
+        
+        function obj = CohesiveProblemFunctional(cParams)
+            obj.init(cParams)
+        end
+
+        function Etot = computeCost(obj,u,bc)
+            fExt = bc.tractionFun;
+            if ~isempty(bc.tractionFun)
+                vals = bc.tractionFun.computeRHS([]);
+                fExt = LagrangianFunction.create(u.mesh, u.mesh.ndim,'P1');
+                fExt.setFValues(reshape(vals,u.mesh.nnodes,u.mesh.ndim));
+            end
+            Eint = obj.functionals.energy.computeCost(u,obj.quadOrder);
+            Ecoh = obj.functinonals.cohesive.computeCost(u,obj.quadOrder, ????);
+            Wext = obj.functionals.extWork.computeCost(u,fExt,obj.quadOrder);
+            Etot = Eint+Ecoh+Wext;
+        end
+
+        function LHS = computeLHS(obj,u)
+            LHS = obj.functionals.energy.computeHessianDisplacement(u,obj.quadOrder);
+        end
+
+        function RHS = computeRHS(obj,u,bc)
+            fExt = bc.tractionFun;
+            if ~isempty(bc.tractionFun)
+                vals = bc.tractionFun.computeRHS([]);
+                fExt = LagrangianFunction.create(u.mesh, u.mesh.ndim,'P1');
+                fExt.setFValues(reshape(vals,u.mesh.nnodes,u.mesh.ndim));
+            end
+            Fint = obj.functionals.energy.computeGradientDisplacement(u,obj.quadOrder);
+            Fext = obj.functionals.extWork.computeGradient(u,fExt,obj.quadOrder);
+            Fcoh = obj.functionals.cohesive.computeGradient(u,obj.quadOrder, ????);
+            RHS = Fint-Fext+Fcoh;
+        end
+
+       
+    end
+    
+    methods (Access = private)
+        
+        function init(obj,cParams)
+            obj.quadOrder = cParams.quadOrder;
+            obj.functionals.extWork = ExternalWorkFunctional(cParams);
+            obj.functionals.energy = LinearElasticityFunctional(cParams);
+            obj.functionals.cohesive = CohesiveFunctional(cParams);
+        end
+        
+    end
+    
+end
