18bd7b85d0deeefa50fcf8d7053ea15bfb79d97d
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2025-12-17 10:58:38 +0100

    mesh and initial guess from freefem in python

--- Florian/NonLinearNull/01_NullSpace_HJ_PerVol/01_NullSpace_HJ_PerVol.py
+++ Florian/NonLinearNull/01_NullSpace_HJ_PerVol/01_NullSpace_HJ_PerVol.py
@@ -1,176 +1,37 @@
+## PACKAGES
 import sys
 sys.path.append("/home/joseantonio/Documentos/GitHub/Swan/Florian"), \
-    # C:/Users/JOSE A. TORRES/Documents/GitHub/Swan/Florian
 
 from nullspace_optimizer import EuclideanOptimizable,\
 bound_constraints_optimizable, memoize, filtered_optimizable
 import numpy as np
 from nullspace_optimizer.optimizable import Optimizable
-
 from nullspace_optimizer.inout import tic, toc   
 from nullspace_optimizer import inout as io   
 from nullspace_optimizer.optimizers.nullspace.utils import compute_norm, get_xiJ_xiC, get_tilde, pack_constraints
 from nullspace_optimizer.optimizers.utils import OptimizationResults, check_params
 from nullspace_optimizer.optimizers.nullspace import utils
-
 import cvxopt
 import cvxopt.cholmod
 import scipy.sparse as sp
 import matplotlib.pyplot as plt
 from matplotlib import colors
- 
-# element stiffness matrix
-def lk():
-    E = 1
-    nu = 0.3
-    k = np.array([1/2-nu/6, 1/8+nu/8,-1/4-nu/12,-1/8+3*nu/8,
-        -1/4+nu/12,-1/8-nu/8, nu/6, 1/8-3*nu/8])
-    KE = E/(1-nu**2)*np.array([
-        [k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7]],
-        [k[1], k[0], k[7], k[6], k[5], k[4], k[3], k[2]],
-        [k[2], k[7], k[0], k[5], k[6], k[3], k[4], k[1]],
-        [k[3], k[6], k[5], k[0], k[7], k[2], k[1], k[4]],
-        [k[4], k[5], k[6], k[7], k[0], k[1], k[2], k[3]],
-        [k[5], k[4], k[3], k[2], k[1], k[0], k[7], k[6]],
-        [k[6], k[3], k[4], k[1], k[2], k[7], k[0], k[5]],
-        [k[7], k[2], k[1], k[4], k[3], k[6], k[5], k[0]]])
-    return (KE)
- 
-def deleterowcol(A, delrow, delcol):
-    # Delete row and columns of a sparse csc matrix A
-    m = A.shape[0]
-    keep = np.delete(np.arange(0, m), delrow)
-    A = A[keep, :]
-    keep = np.delete(np.arange(0, m), delcol)
-    A = A[:, keep]
-    return A
- 
-# Max and min stiffness
-Emin = 1e-9
-Emax = 1.0
- 
-# Assemble FEM matrices and filter
-def init(**kwargs):
-# Initialized global variables
-    global nelx, nely, volfrac, rmin, penal
-    global HFilt, Hs, ndof, KE, iK, jK, fixed, free
-    global edofMat, f, dofs
- 
- 
-# Default input parameters
-    nelx = kwargs.get("nelx", 180)
-    nely = kwargs.get("nely", 60)
-    volfrac = kwargs.get("volfrac", 0.4)
-    rmin = kwargs.get("rmin", 5.4)
-    penal = kwargs.get("penal", 3.0)
-
-# Degrees of Freedom matrix: edofMat[i,j,:] contains the
-# DOFs associated to the square element (i,j)
-    ndof = 2*(nelx+1)*(nely+1)
-    dofs = np.arange(ndof).reshape((nelx+1, nely+1, 2))
-    edofMat = np.zeros((nelx, nely, 8), dtype=int)
-    edofMat[:, :, 0] = dofs[:-1, 1:, 0]
-    edofMat[:, :, 1] = dofs[:-1, 1:, 1]
-    edofMat[:, :, 2] = dofs[1:, 1:, 0]
-    edofMat[:, :, 3] = dofs[1:, 1:, 1]
-    edofMat[:, :, 4] = dofs[1:, :-1, 0]
-    edofMat[:, :, 5] = dofs[1:, :-1, 1]
-    edofMat[:, :, 6] = dofs[:-1, :-1, 0]
-    edofMat[:, :, 7] = dofs[:-1, :-1, 1]
-    edofMat = edofMat.reshape((nelx*nely, 8))
-
-# FE: Build the index vectors for the for coo matrix format.
-    KE = lk()
-# Construct the index pointers for the coo format
-    iK = np.kron(edofMat, np.ones((8, 1))).flatten()
-    jK = np.kron(edofMat, np.ones((1, 8))).flatten()
-
-# BC’s and support
-    fixed = np.union1d(dofs[0, :, 0], dofs[-1,-1, 1])
-    free = np.setdiff1d(dofs.reshape(ndof), fixed)
-
-# Filter: Build (and assemble) the index+data vectors
-# for the coo matrix format
-    nfilter = int(nelx*nely*((2*(np.ceil(rmin)-1)+1)**2))
-    iH = np.zeros(nfilter)
-    jH = np.zeros(nfilter)
-    sH = np.zeros(nfilter)
-    cc = 0
-    for i in range(nelx):
-        for j in range(nely):
-            row = i*nely+j
-            kk1 = int(np.maximum(i-(np.ceil(rmin)-1), 0))
-            kk2 = int(np.minimum(i+np.ceil(rmin), nelx))
-            ll1 = int(np.maximum(j-(np.ceil(rmin)-1), 0))
-            ll2 = int(np.minimum(j+np.ceil(rmin), nely))
-            for k in range(kk1, kk2):
-                for l in range(ll1, ll2):
-                    col = k*nely+l
-                    fac = rmin-np.sqrt(((i-k)*(i-k)+(j-l)*(j-l)))
-
-                    iH[cc] = row
-                    jH[cc] = col
-                    sH[cc] = np.maximum(0.0, fac)
-                    cc = cc+1
-# Finalize assembly and convert to csc format
-    HFilt = sp.coo_matrix((sH, (iH, jH)),
-        shape=(nelx*nely, nelx*nely)).tocsc()
-    Hs = sp.diags(1/np.asarray(HFilt.sum(1)).flatten())
-
-# f is the unit load vector at the bottom right corner
-    f = np.zeros((ndof, 1))
-    f[1, 0] =-1
-
-# Compute compliance and its sensitivity
-@memoize()
-def solve_state(x):
-    dc = np.ones(nely*nelx)
-    ce = np.ones(nely*nelx)
-
-    # Setup and solve FE problem
-    sK = ((KE.flatten()[np.newaxis]).T*(Emin+x**penal*(Emax-Emin)))\
-    .flatten(order="F")
-    K = sp.coo_matrix((sK, (iK, jK)), shape=(ndof, ndof)).tocsc()
-    # Remove constrained dofs from matrix and convert to coo
-    K = deleterowcol(K, fixed, fixed).tocoo()
-    # Solve system
-    K = cvxopt.spmatrix(K.data, K.row.astype(int), K.col.astype(int))
-    B = cvxopt.matrix(f[free, :])
-    cvxopt.cholmod.linsolve(K, B)
-    u = np.zeros((ndof, f.shape[1]))
-    u[free, :] = np.array(B)[:, :]
-
-    # compliance
-    ce = np.einsum("ija,jk,ika->ia", u[edofMat, :], KE, u[edofMat, :])
-    obj = (Emin + (x**penal*(Emax-Emin))).dot(ce)
-
-    # sensitivity of compliance with respect to x
-    dc = np.multiply((-penal*x**(penal-1)*(Emax-Emin))[:, np.newaxis],
-    ce).T
-
-    # If not multi-objective
-    if len(obj)==1:
-        return (obj[0], dc[0,:])
-    # else
-    return (obj, dc)
-
-# Density filter
-@memoize()
-def filter(x):
-    return Hs @ (HFilt @ x)
+from pyfreefem import FreeFemRunner
 
 
-# Sensitivity of the filter
-@memoize()
-def diff_filter(x, v):
-    return (HFilt @ (Hs @ v.T)).T
 
-# Definition of the optimization problem
-@bound_constraints_optimizable(l=0, u=1)
-@filtered_optimizable(filter, diff_filter)
+
+## FREEFEM PROBLEM DEFINITION
+path = "NonLinearNull/01_NullSpace_HJ_PerVol/"
+
+Th = FreeFemRunner(path+"01_Mesh.edp").execute()['Th']
+
+@bound_constraints_optimizable(l=float('-inf'), u=float('inf'))
 class TO_problem(EuclideanOptimizable):
     def x0(self):
-        return volfrac * np.ones(nely*nelx, dtype=float)
+        runner = FreeFemRunner(path+"01_InitialGuess.edp")
+        runner.import_variables(Th=Th)
+        return runner.execute()['phio[]']
 
     def J(self, x):
         (obj, dc) = solve_state(x)
@@ -204,7 +65,9 @@ class TO_problem(EuclideanOptimizable):
             self.fig.canvas.draw()
         plt.pause(0.01)
 
-# Optimization parameters
+
+
+## OPTIMIZATION PARAMETERS
 params = {"dt": 0.05,
           "itnormalisation": 50,
           "save_only_N_iterations": 1,
@@ -212,8 +75,6 @@ params = {"dt": 0.05,
           "alphaJ": 1.25,
           "alphaC": 1,
           "maxit": 10}
-# Initialize and solve the TO problem
-init()
 problem:Optimizable = TO_problem()
 
 
@@ -222,6 +83,7 @@ problem:Optimizable = TO_problem()
 
 
 
+## SETTINGS MANAGEMENT
 default_parameters = dict(alphaJ=1,  
                             alphaC=1,     
                             maxit=4000,    
@@ -277,7 +139,6 @@ utils.tol_cg = params['tol_cg']
 if not callable(params['alphas']):  
     alphas = params['alphas']
     params['alphas'] = lambda x : alphas
-    
                             
 io.display("="*20+"Null Space Optimizer"+"="*20,
         color="magenta", attr="bold", level=1)
@@ -325,6 +186,12 @@ else:
     muls = starting_values['muls']
     io.display("Restart from iteration "+str(it), color="dark_olive_green_3b", attr="bold", level=0)
 
+
+
+
+
+
+## NULLSPACE ALGORITHM
 J = problem.J(x)
 G = problem.G(x)
 H = problem.H(x)
@@ -451,6 +318,8 @@ io.display_iteration(it, J, G, H, x,  level = -1, debug= params['debug'], color=
 
 
 
+
+## POSTPROCESS
 results = abstract_results.implementation()
 iter = results['it']
 Per  = results['J']
