cd52681f8bc7f2fa704b72f0c9e74241392b6756
Gerard Villalta <villaltagerard@gmail.com>
2025-12-18 13:27:43 +0100

    moving files

--- src/Operators/inv4D.m
+++ src/Operators/inv4D.m
@@ -1,5 +1,5 @@
 function Ainv = inv4D(A)
-    Avoigt = convert2Voigt(A,'Material');
+    Avoigt = convert2Voigt(A,'Constitutive');
     AvoigtInv = inv(Avoigt);
-    Ainv = convert2Tensor(AvoigtInv,'Material');
+    Ainv = convert2Tensor(AvoigtInv,'Compliance');
 end
