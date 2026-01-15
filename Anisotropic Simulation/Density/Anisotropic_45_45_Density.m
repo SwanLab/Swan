ca5521dc32b7054b95872f9020341d7d1c002fea
Abril Bertran Garrigos <abriljinbg@gmail.com>
2026-01-15 12:40:51 +0100

    Monitoring figures MBB density

--- Anisotropic Simulation/Density/Anisotropic_45_45_Density.m
+++ Anisotropic Simulation/Density/Anisotropic_45_45_Density.m
@@ -46,7 +46,11 @@ classdef Anisotropic_45_45_Density < handle
 
         function createMesh(obj)
             %UnitMesh better
-            x1      = linspace(0,2,150);
+            % Cantilever beam
+            %x1      = linspace(0,2,150);
+            %x2      = linspace(0,1,75);
+            % MBB Beam
+            x1      = linspace(0,6,500);
             x2      = linspace(0,1,75);
             [xv,yv] = meshgrid(x1,x2);
             [F,V]   = mesh2tri(xv,yv,zeros(size(xv)),'x');
@@ -207,14 +211,27 @@ classdef Anisotropic_45_45_Density < handle
         end
 
         function bc = createBoundaryConditions(obj)
+            % Cantilever beam
+            %xMax    = max(obj.mesh.coord(:,1));
+            %yMax    = max(obj.mesh.coord(:,2));
+            %isDir   = @(coor)  abs(coor(:,1))==0;
+            %isForce = @(coor)  (abs(coor(:,1))==xMax & abs(coor(:,2))>=0.4*yMax & abs(coor(:,2))<=0.6*yMax);
+            
+            % MBB beam
             xMax    = max(obj.mesh.coord(:,1));
             yMax    = max(obj.mesh.coord(:,2));
-            isDir   = @(coor)  abs(coor(:,1))==0;
-            isForce = @(coor)  (abs(coor(:,1))==xMax & abs(coor(:,2))>=0.4*yMax & abs(coor(:,2))<=0.6*yMax);
-
+            isDir   = @(coor)  (abs(coor(:,1))==0 & abs(coor(:,2)) == 0);
+            isDir2  = @(coor)  (abs(coor(:,1))==xMax & abs(coor(:,2)) == 0);
+            isForce = @(coor)  (abs(coor(:,2))==yMax & abs(coor(:,1))>=0.4*xMax & abs(coor(:,1))<=0.6*xMax);
+            
             sDir{1}.domain    = @(coor) isDir(coor);
-            sDir{1}.direction = [1,2];
+            sDir{1}.direction = 2;    % Cantilever--> [1,2]   MBB--> 2
             sDir{1}.value     = 0;
+
+            % Comentar sDir 2 quan es faci cantilever beam
+            sDir{2}.domain    = @(coor) isDir2(coor);
+            sDir{2}.direction = [1,2];
+            sDir{2}.value     = 0;
 
             sPL{1}.domain    = @(coor) isForce(coor);
             sPL{1}.direction = 2;
