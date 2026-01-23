a00ee6dd061c1461c9bc6cf44162d112b2f2bdce
FerrerFerreAlex <32134913+FerrerFerreAlex@users.noreply.github.com>
2026-01-23 15:53:11 +0100

    Update TutorialShells.m

--- TutorialShells.m
+++ TutorialShells.m
@@ -10,6 +10,7 @@ classdef TutorialShells < handle
         uFun
         thetaFun
         wFun
+        bcU,bcT,bcW
     end
 
     methods (Access = public)
@@ -18,9 +19,10 @@ classdef TutorialShells < handle
             obj.createMesh()
             obj.createMaterialProperties()
             obj.createSolutionField()
-
-            obj.createLHS();
             
+            obj.createBoundaryConditions()
+            obj.createLHS();
+            obj.createRHS();
         end
 
     end
@@ -50,28 +52,116 @@ classdef TutorialShells < handle
             A = obj.area;
             f = @(u,v) E.*A.*DDP(SymGrad(v),SymGrad(u));
             Ku = IntegrateLHS(f,obj.uFun,obj.uFun,obj.mesh,'Domain',2);
+            Ku = obj.reduceMatrix(Ku,obj.bcU,obj.bcU);
 
             E = obj.young;
             I = obj.inertia;
             f = @(u,v) E.*I.*DDP(SymGrad(v),SymGrad(u));
             Ktheta = IntegrateLHS(f,obj.thetaFun,obj.thetaFun,obj.mesh,'Domain',2);
+            Ktheta = obj.reduceMatrix(Ktheta,obj.bcT,obj.bcT);
 
 
             A = obj.area;
             G = obj.shear;
             f = @(u,v) G.*A.*DP(v,u);
             Mtheta = IntegrateLHS(f,obj.thetaFun,obj.thetaFun,obj.mesh,'Domain',2);
+            Mtheta = obj.reduceMatrix(Mtheta,obj.bcT,obj.bcT);
 
 
             A = obj.area;
             G = obj.shear;
-            f = @(u,v) G.*A.*DP(u,SymGrad(v));
-            Nthetaw = IntegrateLHS(f,obj.wFun,obj.thetaFun,obj.mesh,'Domain',2);            
+            f = @(u,v) G.*A.*DP(v,Grad(u));
+            Nthetaw = IntegrateLHS(f,obj.thetaFun,obj.wFun,obj.mesh,'Domain',2);            
+            Nthetaw = obj.reduceMatrix(Nthetaw,obj.bcT,obj.bcW);
 
         
+            A = obj.area;
+            G = obj.shear;
+            f = @(u,v) A.*G.*DP(Grad(v),Grad(u));
+            Kw = IntegrateLHS(f,obj.wFun,obj.wFun,obj.mesh,'Domain',2);  
+            Kw = obj.reduceMatrix(Kw,obj.bcW,obj.bcW);
+
+            nTheta = obj.thetaFun.nDofs;
+            nU     = obj.uFun.nDofs;
+            nW     = obj.wFun.nDofs;
+
+            Zut = zeros(nU,nTheta);
+            Zuw = zeros(nU,nW);
+            LHS = [Ku Zut Zuw; Zut' (Ktheta+Mtheta) Nthetaw; Zuw' Nthetaw' Kw];
+        end
+
+        function createRHS(obj)
+            p = ConstantFunction.create([0 0],obj.mesh);
+            m = ConstantFunction.create([0 0],obj.mesh);
+            q = ConstantFunction.create(1,obj.mesh);
+
+            fu = @(v) DP(p,v);
+            RHSu = IntegrateRHS(fu,obj.uFun,obj.mesh,'Domain',2);
+            RHSu = obj.reduceVector(RHSu,obj.bcU);
+
+            ftheta   = @(v) DP(m,v);
+            RHStheta = IntegrateRHS(ftheta,obj.thetaFun,obj.mesh,'Domain',2);
+            RHStheta = obj.reduceVector(RHStheta,obj.bcT);
+
+            fw = @(v) q.*v;
+            RHSw = IntegrateRHS(fw,obj.wFun,obj.mesh,'Domain',2);
+            RHSw = obj.reduceVector(RHSw,obj.bcW);
+            
+
+            RHS = [RHSu;RHStheta;RHSw];
+
+
 
         end
 
+        function createBoundaryConditions(obj)
+            obj.bcU = obj.createGeneralBoundaryConditions([1 2]);
+            obj.bcT = obj.createGeneralBoundaryConditions([1 2]);
+            obj.bcW = obj.createGeneralBoundaryConditions([1]);
+        end
+
+        function bc = createGeneralBoundaryConditions(obj,direct)
+            TOL = 1e-12;
+            xMax    = max(obj.mesh.coord(:,1));
+            yMax    = max(obj.mesh.coord(:,2));
+            xMin    = min(obj.mesh.coord(:,1));
+            yMin    = min(obj.mesh.coord(:,2));
+            isLeft  = @(coor)  abs(coor(:,1)-xMin)< TOL;
+            isRight = @(coor)  abs(coor(:,1)-xMax)< TOL;
+            isTop   = @(coor)  abs(coor(:,2)-yMax)< TOL;
+            isBotom = @(coor)  abs(coor(:,2)-yMin)< TOL;
+
+            sDir{1}.domain    = @(coor) isLeft(coor) | isRight(coor) | isTop(coor) | isBotom(coor);
+            sDir{1}.direction = direct;
+            sDir{1}.value     = 0;
+
+
+            dirichletFun = [];
+            for i = 1:numel(sDir)
+                dir = DirichletCondition(obj.mesh, sDir{i});
+                dirichletFun = [dirichletFun, dir];
+            end
+            s.dirichletFun = dirichletFun;
+
+            s.periodicFun  = [];
+            s.pointloadFun    = [];
+            s.mesh = obj.mesh;
+            bc = BoundaryConditions(s);            
+        end
+
+        function LHSred = reduceMatrix(obj,LHS,bcV,bcU)
+            dofsV = 1:bcV.dirichletFun.nDofs;
+            dofsU = 1:bcU.dirichletFun.nDofs;
+            fdofV = setdiff(dofsV,bcV.dirichlet_dofs);
+            fdofU = setdiff(dofsU,bcU.dirichlet_dofs);
+            LHSred = LHS(fdofV,fdofU);
+        end
+
+        function RHSred = reduceVector(obj,RHS,bc)
+            dofsV = 1:bc.dirichletFun.nDofs;
+            fdofV = setdiff(dofsV,bc.dirichlet_dofs);
+            RHSred = RHS(fdofV,1);
+        end
 
 
 
