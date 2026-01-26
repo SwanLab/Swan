b31c4433d1f95341e13e439666f5ec6030af50fd
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2026-01-26 17:30:07 +0100

    new results for 0.8

--- Florian/NonLinearNull/03_NullSpace_HJ_Compl_VolPer_Iterative/03_Main.py
+++ Florian/NonLinearNull/03_NullSpace_HJ_Compl_VolPer_Iterative/03_Main.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 from fun03_NullSpace_HJ_CVP_Iterative import FunctionCase03
 
 maxItj  = [1,2,4,8]
-stepHJ  = 1
-No      = 12
-maxIter = 40
+stepHJ  = 0.8
+No      = 10
+maxIter = 100
 
 for i in range(len(maxItj)):
     FunctionCase03(maxItj[i],stepHJ,No,maxIter)
