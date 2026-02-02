3852ae28ca03e12f8b7e7bb4cfc6f55c14434816
Abril Bertran Garrigos <abriljinbg@gmail.com>
2026-02-02 15:28:17 +0100

    Run3DSimulations and num printing fValues modified

--- Anisotropic Simulation/Run3DSimulations.m
+++ Anisotropic Simulation/Run3DSimulations.m
@@ -2,24 +2,29 @@
 
 
 %% LEVEL SET
-Anisotropic_0_LevelSet_3D();
-Anisotropic_45_45_LevelSet_3D();
-Anisotropic_0_Density_3D();
-Anisotropic_45_45_Density_3D();
-
+% Anisotropic_0_LevelSet_3D();
+% Anisotropic_45_45_LevelSet_3D();
 Anisotropic_45_LevelSet_3D();
-Anisotropic_45_Density_3D();
-
 Anisotropic_90_LevelSet_3D();
+% Anisotropic_0_45_LevelSet_3D();
+% Anisotropic_0_90_LevelSet_3D();
+
+
+
+%% DENSITY
+% Anisotropic_0_Density_3D();
+% Anisotropic_45_45_Density_3D();
+Anisotropic_45_Density_3D();
 Anisotropic_90_Density_3D();
-
-Anisotropic_0_45_LevelSet_3D();
-Anisotropic_0_45_Density_3D();
-Anisotropic_0_90_LevelSet_3D();
-Anisotropic_0_90_Density_3D();
+% Anisotropic_0_45_Density_3D();
+% Anisotropic_0_90_Density_3D();
 
 
-%% MONITORING
+
+
+
+
+
 
 
 
