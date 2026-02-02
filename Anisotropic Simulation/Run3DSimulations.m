f7acfa6c2e5a84636aa3603bdfeb0fa8ac30e441
Abril Bertran Garrigos <abriljinbg@gmail.com>
2026-02-02 08:59:50 +0100

    3D simulations done

--- Anisotropic Simulation/Run3DSimulations.m
+++ Anisotropic Simulation/Run3DSimulations.m
@@ -4,9 +4,9 @@
 %% LEVELSET
 
 % Anisotropic_0_LevelSet_3D();
+Anisotropic_45_45_LevelSet_3D();
 Anisotropic_90_LevelSet_3D();
 Anisotropic_45_LevelSet_3D();
-Anisotropic_45_45_LevelSet_3D();
 Anisotropic_0_45_LevelSet_3D();
 Anisotropic_0_90_LevelSet_3D();
 
