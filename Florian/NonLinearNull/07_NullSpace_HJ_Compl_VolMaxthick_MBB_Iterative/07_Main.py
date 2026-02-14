3800c0f1f598e28348e1d5ee664990606820d505
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2026-02-14 13:24:20 +0100

    subiterating max thick

---
+++ Florian/NonLinearNull/07_NullSpace_HJ_Compl_VolMaxthick_MBB_Iterative/07_Main.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("/home/joseantonio/Documentos/GitHub/Swan/Florian/NonLinearNull/07_NullSpace_HJ_Compl_VolMaxthick_MBB_Iterative/")
+import matplotlib.pyplot as plt
+
+from fun07_NullSpace_HJ_CVT_Iterative import FunctionCase07
+
+maxItj  = [1,5]
+stepHJ  = 0.2
+No      = 50
+maxIter = 200
+
+for i in range(len(maxItj)):
+    FunctionCase07(maxItj[i],stepHJ,No,maxIter)
+    plt.close('all')
---
+++ Florian/NonLinearNull/07_NullSpace_HJ_Compl_VolMaxthick_MBB_Iterative/07_Plotting.py
@@ -0,0 +1,78 @@
+## PACKAGES
+import sys
+sys.path.append("/home/joseantonio/Documentos/GitHub/Swan/Florian"), \
+
+from nullspace_optimizer import EuclideanOptimizable,\
+bound_constraints_optimizable, memoize, filtered_optimizable
+import numpy as np
+from nullspace_optimizer.optimizable import Optimizable
+from nullspace_optimizer.inout import tic, toc   
+from nullspace_optimizer import inout as io   
+from nullspace_optimizer.optimizers.nullspace.utils import compute_norm, get_xiJ_xiC, get_tilde, pack_constraints
+from nullspace_optimizer.optimizers.utils import OptimizationResults, check_params
+from nullspace_optimizer.optimizers.nullspace import utils
+import cvxopt
+import cvxopt.cholmod
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from pyfreefem import FreeFemRunner
+from pymedit import P1Function
+
+
+
+
+
+path = "NonLinearNull/07_NullSpace_HJ_Compl_VolMaxthick_MBB_Iterative/"
+
+exports = FreeFemRunner(path+"07_Mesh.edp").execute()
+Th = exports['Th']
+
+## POSTPROCESS
+data  = np.load(path+"07_ResultIts1Step0.2.npz")
+iter1 = data['it']
+Comp1 = data['c']
+VT1  = data['vt']
+Vol1,T1 = zip(*VT1)
+
+data  = np.load(path+"07_ResultIts5Step0.2.npz")
+iter5 = data['it']
+Comp5 = data['c']
+VT5  = data['vt']
+Vol5,T5 = zip(*VT5)
+
+data = np.load(path+"07_ResultIts5Step0.2.npz")
+fig0, ax0 = plt.subplots()
+x = data["xF"]
+u = P1Function(Th,x<=0)
+u.plot(fig=fig0,ax=ax0)
+plt.pause(0.05)
+
+fig, axes = plt.subplots(1, 3, figsize=(10, 4))
+
+axes[0].plot(iter1, Comp1, color='b', label='1 geo it')
+axes[0].plot(iter5, Comp5, color='r', label='5 geo it')
+axes[0].legend()
+axes[0].set_xlabel('Iter')
+axes[0].set_ylabel('Compliance')
+axes[0].grid(True, linestyle='--', alpha=0.6)
+
+axes[1].plot(iter1, Vol1, color='b', label='1 geo it')
+axes[1].plot(iter5, Vol5, color='r', label='5 geo it')
+axes[1].legend()
+axes[1].set_xlabel('Iter')
+axes[1].set_ylabel('Volume constraint')
+axes[1].grid(True, linestyle='--', alpha=0.6)
+
+axes[2].plot(iter1, T1, color='b', label='1 geo it')
+axes[2].plot(iter5, T5, color='r', label='5 geo it')
+axes[2].legend()
+axes[2].set_xlabel('Iter')
+axes[2].set_ylabel('Max. Thick. constraint')
+axes[2].grid(True, linestyle='--', alpha=0.6)
+
+plt.tight_layout()
+plt.show()
+plt.pause(0.1)
+
+a = 1
