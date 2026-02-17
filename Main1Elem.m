1c837d07923310adecc4217d54cdc38f18a32528
Gerard Villalta <villaltagerard@gmail.com>
2026-02-17 23:06:48 +0100

    1Elem results

--- Main1Elem.m
+++ Main1Elem.m
@@ -13,29 +13,25 @@ s.maxIter.u = 100;
 s.maxIter.phi = 300;
 s.maxIter.stag = 300;
 
-s.benchmark.mesh.type   = 'Uniaxial';%'SENshear';
-s.benchmark.mesh.length = 200;
-s.benchmark.mesh.width  = 10;
-s.benchmark.mesh.lN     = 400;
-s.benchmark.mesh.wN     = 20;
-s.benchmark.bc.u.type   = 'DisplacementTractionX';%'DisplacementShear';
+s.benchmark.mesh.type   = '1Elem';
+s.benchmark.bc.u.type   = 'DisplacementTractionX';
 s.benchmark.bc.u.values =  [0:1e-4:0.1];
-s.benchmark.bc.phi.type = 'DamageFixedLimitsX';%'DamageFixedLimitsX'; %DamageFree
+s.benchmark.bc.phi.type = 'DamageFree';
 
 s.matInfo.matType = 'Analytic'; %'Analytic','Homogenized'
 s.matInfo.degradationType = 'PhaseField'; %'PhaseField','SIMPALL'
-s.matInfo.degradationSubType = 'AT2linear'; %'AT','ATSplit','Rational','General'
+s.matInfo.degradationSubType = 'General'; %'AT','ATSplit','Rational','General'
 s.matInfo.fileName = 'HoneycombBenchmark02'; 
-s.matInfo.young   = 30000;
-s.matInfo.poisson = 0.2;
-s.matInfo.Gc      = 8e-3;
-s.matInfo.sigmaMax = 3;
-s.l0 = 10; %10/5
+s.matInfo.young   = 210;
+s.matInfo.poisson = 0.3;
+s.matInfo.Gc      = 5e-3;
+s.matInfo.sigmaMax = 1.984;
+s.l0 = 0.1; %10/5
 s.matInfo.params.coeffs = [(4/pi)*(s.matInfo.Gc*s.matInfo.young)/(s.matInfo.sigmaMax^2 * s.l0), -0.5]; %(4/pi)
 s.matInfo.params.exp = 2;
 
 s.dissipInfo.type = 'AT';
-s.dissipInfo.constant = 2; % 2 AT2 / 8/3 AT1 / pi Wu 
+s.dissipInfo.constant = pi; % 2 AT2 / 8/3 AT1 / pi Rational 
 s.dissipInfo.pExp = 1;
 s.dissipInfo.xi = 2; % 0 AT2 / 1 AT1 / 2 Mix in type FullQuadratic
 s.solver.type = 'Gradient';
@@ -48,5 +44,5 @@ outputData = tester.compute();
 outputData.inputParameters = s;
 
 % SAVE + PLOT
-save("UniaxialAT2linear_unstructured.mat",'outputData') %ACTIVATE TO SAVE DATA!
+save("1ElemRational_noUB.mat",'outputData') %ACTIVATE TO SAVE DATA!
 PhaseFieldPlotter(outputData); 
