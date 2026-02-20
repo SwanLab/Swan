04510af5d5ab044a74938666aa2e0e546441964f
Gerard Villalta <villaltagerard@gmail.com>
2026-02-20 16:53:21 +0100

    results and analysis

---
+++ derivatives_analysis.m
@@ -0,0 +1,45 @@
+a1 = (4/pi)*(5e-3*210)/(1.984^2 *0.1);
+a2 = (4/pi)*(5e-3*210)/(1^2 *0.1);
+C11 = 210/((1+0.3)*(1-0.3));
+syms x
+ratFun1 = C11*(1-x)/((1-x)+a1*x/2);
+ratFun2 = C11*(1-x)/((1-x)+a2*x/2);
+ratWu = C11*(1-x)^2 / ((1-x)^2 + a1*x*(1-0.5*x));
+
+load('HexagonBenchmark03.mat');
+C11hexa = degradation.fun{2,2,2,2};
+dC11hexa = degradation.dfun{2,2,2,2};
+d2C11hexa = degradation.ddfun{2,2,2,2};
+
+load('HoneycombBenchmark03.mat');
+C11honey = degradation.fun{1,1,1,1};
+dC11honey = degradation.dfun{1,1,1,1};
+d2C11honey = degradation.ddfun{1,1,1,1};
+
+tiledlayout(1,3)
+nexttile
+hold on
+fplot(ratFun1,[0 1])
+fplot(ratFun2,[0 1])
+fplot(ratWu,[0 1])
+fplot(@(x) 210*C11hexa(x),[0 1])
+fplot(@(x) 210*C11honey(x),[0 1])
+
+nexttile
+hold on
+fplot(diff(ratFun1),[0 1])
+fplot(diff(ratFun2),[0 1])
+fplot(diff(ratWu),[0 1])
+fplot(@(x) 210*dC11hexa(x),[0 1])
+fplot(@(x) 210*dC11honey(x),[0 1])
+
+nexttile
+hold on
+fplot(diff(diff(ratFun1)),[0 1])
+fplot(diff(diff(ratFun2)),[0 1])
+fplot(diff(diff(ratWu)),[0 1])
+fplot(@(x) 210*d2C11hexa(x),[0 1])
+fplot(@(x) 210*d2C11honey(x),[0 1])
+
+
+legend('Rational 1.98','Rational 1','Rational Wu 1.98','Hexagon','Honeycomb')
--- src/Problems/Damage/Models/PhaseField/Cases/MainPhaseFieldCase.m
+++ src/Problems/Damage/Models/PhaseField/Cases/MainPhaseFieldCase.m
@@ -7,42 +7,38 @@ clc,clear,close all
 
 %% GENERAL SETTINGS
 
-s.monitoring.set = false;
+s.monitoring.set =true;
 s.monitoring.type = 'full'; %'reduced'
-s.monitoring.print = false;
+s.monitoring.print = true;
 
 s.tolerance.u = 1e-6;
-s.tolerance.phi = 1e-6;
-s.tolerance.stag = 1e-6;
+s.tolerance.phi = 1e-8;
+s.tolerance.stag = 1e-8;
 s.maxIter.u = 100;
 s.maxIter.phi = 300;
 s.maxIter.stag = 300;
 
-s.benchmark.mesh.type   = 'SENtraction';%'SENshear';
-s.benchmark.mesh.length = 200;
-s.benchmark.mesh.width  = 10;
-s.benchmark.mesh.lN     = 200;
-s.benchmark.mesh.wN     = 10;
-s.benchmark.bc.u.type   = 'DisplacementTractionYClamped';%'DisplacementShear';
-s.benchmark.bc.u.values =  [0:1e-5:0.005,0.005:1e-6:0.0065];
-s.benchmark.bc.phi.type = 'DamageFree';%'DamageFixedLimitsX'; %DamageFree
+s.benchmark.mesh.type   = '1Elem';%'SENshear';
+s.benchmark.bc.u.type   = 'DisplacementTractionX';%'DisplacementShear';
+s.benchmark.bc.u.values =  [0:1e-3:0.04];
+s.benchmark.bc.phi.type = 'DamageFree';
 
-s.matInfo.matType = 'Analytic'; %'Analytic','Homogenized'
-s.matInfo.degradationType = 'PhaseField'; %'PhaseField'
-s.matInfo.degradationSubType = 'AT2linear'; %'AT','AT2linear','General'
-s.matInfo.fileName = 'HoneycombBenchmark02'; 
+s.matInfo.matType = 'Homogenized'; %'Analytic','Homogenized'
+s.matInfo.degradationType = 'PhaseField'; %'PhaseField','SIMPALL'
+s.matInfo.degradationSubType = 'General'; %'AT','AT2linear','General'
+s.matInfo.fileName = 'HexagonBenchmark03'; 
 s.matInfo.young   = 210;
 s.matInfo.poisson = 0.3;
-s.matInfo.Gc      = 2.7e-3;
-s.matInfo.sigmaMax =2.44542;
-s.l0 = 0.01;
+s.matInfo.Gc      = 5e-3;
+s.matInfo.sigmaMax = 1;
+s.l0 = 0.1;
 s.matInfo.params.coeffs = [(4/pi)*(s.matInfo.Gc*s.matInfo.young)/(s.matInfo.sigmaMax^2 * s.l0), -0.5]; %(4/pi)
 s.matInfo.params.exp = 2;
 
 s.dissipInfo.type = 'AT';
-s.dissipInfo.constant = 2; % 2 AT2 / 8/3 AT1 / pi Wu 
+s.dissipInfo.constant = 8/3; % 2 AT2 / 8/3 AT1 / pi Wu 
 s.dissipInfo.pExp = 1;
-s.dissipInfo.xi = 1; % 0 AT2 / 1 AT1 / 2 Mix in type FullQuadratic
+s.dissipInfo.xi = 2; % 0 AT2 / 1 AT1 / 2 Mix in type FullQuadratic
 s.solver.type = 'Gradient';
 s.solver.tau  = 150;
 
@@ -53,5 +49,6 @@ outputData = tester.compute();
 outputData.inputParameters = s;
 
 %% SAVE + PLOT
-save("SENtraction001AT2_v2.mat",'outputData') %ACTIVATE TO SAVE DATA!
+%save("1ElemRational1_v2.mat",'outputData') %ACTIVATE TO SAVE DATA!
 PhaseFieldPlotter(outputData);
+
