34a4bdc4b33a72991aee88a49aa43ba174b5d990
Gerard Villalta <villaltagerard@gmail.com>
2026-02-25 20:01:40 +0100

    2D analysis good!

--- eps_analysis.m
+++ eps_analysis.m
@@ -41,3 +41,51 @@ legend('AT1','AT2','Wu','WuAT1','Rational','Hexagon','Honeycomb')
 figure(101)
 sigHexa =  sqrt(-gHexa*gHexa*diff(wAT1)/diff(gHexa));
 fplot(sigHexa,[0 1]);
+
+%% Computation 2D
+syms x 
+[dataHexa]  = load('HexagonBenchmark03.mat');
+C11hexa = dataHexa.degradation.fun{1,1,1,1}(x);
+C12hexa = dataHexa.degradation.fun{2,2,1,1}(x);
+C33hexa = dataHexa.degradation.fun{1,2,1,2}(x);
+
+bulkHexa   = (C11hexa-C33hexa);
+shearHexa  = C33hexa;
+
+[dataHoney] = load('HoneycombBenchmark03.mat');
+C11honey = dataHoney.degradation.fun{1,1,1,1}(x);
+C12honey = dataHoney.degradation.fun{2,2,1,1}(x);
+C33honey = dataHoney.degradation.fun{1,2,1,2}(x);
+bulkHoney  = (C11honey-C33honey);
+shearHoney = C33honey;
+
+k = bulkHexa;
+m = shearHexa;
+CPrime1Dhexa = ( (diff(k)+diff(m))*(1+((k-m)/(k+m))^2) - 2*(diff(k)-diff(m))*((k-m)/(k+m)));
+
+k = bulkHoney;
+m = shearHoney;
+CPrime1Dhoney = ( (diff(k)+diff(m))*(1+((k-m)/(k+m))^2) - 2*(diff(k)-diff(m))*((k-m)/(k+m)));
+
+
+epsHexa = sqrt(-diff(wAT1)/CPrime1Dhexa);
+epsHoney = sqrt(-diff(wAT1)/CPrime1Dhoney);
+figure(102)
+a = fplot(epsHexa,[0 1]);
+aXData = a.XData; aYData = a.YData;
+b = fplot(epsHoney,[0 1]);
+bXData = b.XData; bYData = b.YData;
+plot(aYData,aXData)
+hold on
+plot(bYData,bXData)
+legend('Hexa','Honey')
+title('Damage-displacement')
+
+figure(103)
+hold on
+fplot(CPrime1Dhexa,[0 1])
+fplot(CPrime1Dhoney,[0 1])
+legend('Hexa','Honey')
+title('Degradation derivative equivalent to 1D')
+
+
