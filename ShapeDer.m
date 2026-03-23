46b7b88fd088af303c755d9ac0fdc79541418000
aleixmtrias <aleixmartintrias@gmail.com>
2026-03-23 20:24:32 +0100

    Create ShapeDer.m

---
+++ ShapeDer.m
@@ -0,0 +1,20 @@
+function dom = ShapeDer(u)
+    s.operation = @(xV) evaluate(u, xV);
+    s.mesh      = u.mesh;
+    dom         = DomainFunction(s);
+end
+
+function gradN = evaluate(u, xV)
+    dNdx   = u.evaluateCartesianDerivatives(xV);
+    nnodeE = u.mesh.nnodeElem;
+    ndim   = u.mesh.ndim;
+    ndimf  = u.ndimf;
+    ndofE  = nnodeE*ndimf;
+    nGauss = size(xV,2);
+    nElem  = u.mesh.nelem;
+    gradN  = zeros(ndimf,ndim,ndofE,nGauss,nElem);
+    for i=1:ndimf
+        gradN(i,:,i:ndimf:end,:,:) = dNdx;
+    end
+    gradN = permute(gradN,[1 2 4 5 3]);
+end
