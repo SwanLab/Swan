43ddbf72a9052748c41b9d7ee68d9192ccb2fc9c
giovannacandrade <giovanna.castelloa@gmail.com>
2026-04-15 23:50:56 -0300

    creating connectivity files

---
+++ src/Functionals/MinimumEigenValueFunctional.m
@@ -0,0 +1,70 @@
+classdef MinimumEigenValueFunctional < handle
+    
+    properties (Access = public)
+        value
+    end
+       
+    properties (Access = private)
+       eigModes 
+       designVariable
+       mesh
+       filter
+       filterAdjoint
+       isCompl
+    end
+    
+    methods (Access = public)
+        
+        function obj = MinimumEigenValueFunctional(cParams)
+            obj.init(cParams)
+        end   
+
+        function [f, dfdx] = computeFunctionAndGradient(obj,x) 
+            xD  = x.obtainDomainFunction();
+            xR = obj.filterDesignVariable(xD{1});
+            if obj.isCompl == true
+                xR.setFValues(max(min(1 - xR.fValues,1),0)); % 1 - FP
+            end
+            [f,dfdx]= obj.eigModes.computeFunctionAndGradient(xR);    
+            if ~isempty(obj.filterAdjoint)
+                dfdx     = obj.filterAdjoint.compute(dfdx,2);
+            else
+                dfdx     = obj.filter.compute(dfdx,2);
+            end
+            if obj.isCompl == true
+                dfdx.setFValues(-dfdx.fValues);              % Chain rule for (1 - FP)
+            end
+        end
+                
+    end
+    
+    methods (Access = private)
+        
+        function init(obj,cParams)
+            obj.eigModes       = cParams.eigenModes;
+            obj.designVariable = cParams.designVariable;
+            obj.mesh           = cParams.mesh;
+            obj.filter = cParams.filter;
+            if isfield(cParams,'filterAdjoint')
+                obj.filterAdjoint  = cParams.filterAdjoint;
+            end
+            if isfield(cParams,'isCompl')
+                obj.isCompl  = cParams.isCompl;
+            end
+        end
+      
+        function xR = filterDesignVariable(obj,x)
+            xR = obj.filter.compute(x,2);
+            if ~isempty(obj.filterAdjoint)
+                obj.filterAdjoint.updateFilteredField(obj.filter);
+            end
+        end
+
+    end
+    
+    methods (Static, Access = public)
+        function title = getTitleToPlot()
+            title = 'MinimumEigenvalue';
+        end
+    end  
+end
--- src/Problems/Optimization/Topology/MaterialInterpolators/MaterialInterpolatorFactory.m
+++ src/Problems/Optimization/Topology/MaterialInterpolators/MaterialInterpolatorFactory.m
@@ -29,6 +29,8 @@ classdef MaterialInterpolatorFactory < handle
                     obj = SimpInterpolationP3(cParams);
                 case 'SIMPThermal'
                     obj = SIMPThermalInterpolation(cParams);
+                case 'SIMPAllThermal'
+                    obj = SimpAllThermalInterpolation(cParams);
                 case 'HomogenizedMicrostructure'
                     obj = HomogenizedMicrostructureInterpolator(cParams);
                 case 'PhaseFieldDegradation'
