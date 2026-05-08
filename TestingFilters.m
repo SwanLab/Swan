8a07112108f4c10ade373d77284c4731f5d4a9e5
SergiuA101 <aldeaserge@gmail.com>
2026-05-08 16:35:12 +0300

    Update TestingFilters.m

--- TestingFilters.m
+++ TestingFilters.m
@@ -26,24 +26,69 @@ chi = CharacteristicFunction.create(uMesh);
 
 
 % Filter PDE Isotropic
-epsilon = 4*h;
-sF.trial = LagrangianFunction.create(mesh,1,'P1');
-sF.mesh = mesh;
-sF.filterType = 'PDE';
-sF.metric = 'Isotropy';
-filter = Filter.create(sF);
-filter.updateEpsilon(epsilon);
-rhoEps1 = filter.compute(chi,3);  % Cubic Gauss quadrature to integrate
-rhoEps1.plot();
+% epsilon = 4*h;
+% sF.trial = LagrangianFunction.create(mesh,1,'P1');
+% sF.mesh = mesh;
+% sF.filterType = 'PDE';
+% sF.metric = 'Isotropy';
+% filter = Filter.create(sF);
+% filter.updateEpsilon(epsilon);
+% rhoEps1 = filter.compute(chi,3);  % Cubic Gauss quadrature to integrate
+% rhoEps1.plot();
 
 
 % Filter PDE Anisotropy
 
+epsilon = 4*h;
 
+sF = [];
+sF.trial = LagrangianFunction.create(mesh,1,'P1');
+sF.mesh = mesh;
+sF.filterType = 'PDE';
+sF.boundaryType = 'Neumann';
+sF.metric = 'Anisotropy';
+k = 10;
+A = [k 0; 
+     0  1/k];
+R = 
+
+sF.A = ConstantFunction.create(A,mesh);
+
+filter = Filter.create(sF);
+filter.updateEpsilon(epsilon);
+
+rhoEps2 = filter.compute(chi,3);
+rhoEps2.plot();
 
 % Filter Lump
 
-
+% sF = [];
+% sF.trial = LagrangianFunction.create(mesh,1,'P1');
+% sF.mesh = mesh;
+% sF.filterType = 'LUMP';
+% 
+% filter = Filter.create(sF);
+% 
+% rhoLump = filter.compute(chi,3);
+% rhoLump.plot();
 
 % (Optional) Is not in the tutorial !!   NonLinearFilterSegment
 
+% epsilon = 4*h;
+% 
+% sF = [];
+% sF.filterType = 'Segment';
+% sF.mesh       = mesh;
+% 
+% sF.alpha = 5;
+% sF.beta  = 5;
+% sF.theta = 45;   
+% 
+% filter = Filter.create(sF);
+% filter.updateEpsilon(epsilon);
+% 
+% rhoSeg = filter.compute(chi,3);
+% 
+% figure;
+% rhoSeg.plot();
+% title('Nonlinear Segment Filter');
