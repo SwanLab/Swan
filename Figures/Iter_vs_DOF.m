58fb7bdb366913b4ca13d5ab56401db6fbc83cb0
Abril Bertran Garrigos <abriljinbg@gmail.com>
2026-05-18 10:52:35 +0200

    plots script: iter vs dofs & time vs dofs

--- Figures/Iter_vs_DOF.m
+++ Figures/Iter_vs_DOF.m
@@ -1,5 +1,5 @@
 %% CIRCLE
-
+pos= [543 498 433 315]
 
 % SCALABILITY
 dof = [43968, 116864, 262296, 654480];
@@ -32,3 +32,20 @@ title('Scalability test: iter. vs DOFs for r = 0.8');
 xlabel('DOFs');
 ylabel('Iterations');
 ylim([45,95]);
+
+
+% GENERALIZATION
+dof = [10108, 24034, 43968, 101828, 183688, 576240];
+DS= [13, 18, 24, 34, 44, 55];
+NN= [13, 19, 24, 34, 45, 56];
+
+plot(dof,DS,'LineWidth',1.5);
+hold on
+plot(dof,NN,'LineWidth',1.5);
+legend('Dataset', 'NN');
+% set(gca, 'YScale', 'log')
+title('Iter. vs DOFs');
+xlabel('DOFs');
+ylabel('Iterations');
+
+
--- Figures/TimesComparison.m
+++ Figures/TimesComparison.m
@@ -12,12 +12,12 @@ NN08      = [0.65, 2.77, 8.63, 31.68];
 Direct08  = [0.11, 0.34, 0.87, 2.54];
 
 figure
-plot(x,dat03,'LineWidth',1.5);
+plot(x,dat03,'-+','LineWidth',1.5);
 hold on
-plot(x,NN03,'LineWidth',1.5);
+plot(x,NN03,'-+','LineWidth',1.5);
 hold on
-plot(x,Direct03,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+plot(x,Direct03,'-+','LineWidth',1.5);
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 % set(gca, 'YScale', 'log')
 title('Time vs DOFs for r=0.3');
 xlabel('DOFs');
@@ -25,38 +25,38 @@ ylabel('time [s]');
 ylim([0,15]);
 
 figure
-plot(x,dat08,'LineWidth',1.5);
+plot(x,dat08,'-+','LineWidth',1.5);
 hold on
-plot(x,NN08,'LineWidth',1.5);
+plot(x,NN08,'-+','LineWidth',1.5);
 hold on
-plot(x,Direct08,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+plot(x,Direct08,'-+','LineWidth',1.5);
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 % set(gca, 'YScale', 'log')
 title('Time vs DOFs for r=0.8');
 xlabel('DOFs');
 ylabel('time [s]');
-ylim([0,40]);
+ylim([0,45]);
 
 
 % Mesh generalization
-x=[];
+x= [10108, 24034, 43968, 101828, 183688, 289548];
 
-dat=[];
-NN=[];
-Direct=[];
+dat=[0.05, 0.16, 0.33, 1.07, 2.81, 5.11];
+NN=[0.05, 0.14, 0.32, 1.10, 2.81, 5.28];
+Direct=[0.02, 0.06, 0.17, 0.26, 0.53, 0.91];
 
 figure
-plot(x,dat,'LineWidth',1.5);
+plot(x,dat,'-+','LineWidth',1.5);
 hold on
-plot(x,NN,'LineWidth',1.5);
+plot(x,NN,'-+','LineWidth',1.5);
 hold on
-plot(x,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
-set(gca, 'YScale', 'log')
+plot(x,Direct,'-+','LineWidth',1.5);
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
+% set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
 ylabel('time [s]');
-
+ylim([0,8]);
 
 
 %% LATTICE
@@ -67,12 +67,12 @@ NN=[1.29,4.19,10.98,17.15];
 Direct=[0.28,0.78,2.16,3.44];
 
 figure
-plot(x,dat,'LineWidth',1.5);
+plot(x,dat,'-+','LineWidth',1.5);
 hold on
-plot(x,NN,'LineWidth',1.5);
+plot(x,NN,'-+','LineWidth',1.5);
 hold on
-plot(x,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+plot(x,Direct,'-+','LineWidth',1.5);
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
@@ -87,12 +87,12 @@ NN=[0.06,0.23,0.67,2.39,6.59,13.08];
 Direct=[0.03,0.06,0.10,0.26,0.56,0.90];
 
 figure
-plot(x,dat,'LineWidth',1.5);
+plot(x,dat,'-+','LineWidth',1.5);
 hold on
-plot(x,NN,'LineWidth',1.5);
+plot(x,NN,'-+','LineWidth',1.5);
 hold on
-plot(x,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+plot(x,Direct,'-+','LineWidth',1.5);
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
@@ -115,7 +115,7 @@ hold on
 plot(x1,NN,'LineWidth',1.5);
 hold on
 plot(x2,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
@@ -135,7 +135,7 @@ hold on
 plot(x1,NN,'LineWidth',1.5);
 hold on
 plot(x2,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
@@ -158,7 +158,7 @@ hold on
 plot(x1,NN,'LineWidth',1.5);
 hold on
 plot(x2,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
@@ -178,7 +178,7 @@ hold on
 plot(x1,NN,'LineWidth',1.5);
 hold on
 plot(x2,Direct,'LineWidth',1.5);
-legend('Multiscale Dataset', 'Multiscale NN', 'Direct Solver');
+legend('Multiscale', 'Multiscale with NN', 'Direct Solver');
 set(gca, 'YScale', 'log')
 title('Time vs DOFs');
 xlabel('DOFs');
