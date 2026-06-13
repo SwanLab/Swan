f0eb51a62a433db35922571e5bd534d20c88af42
Gerard Villalta <villaltagerard@gmail.com>
2026-06-13 20:13:20 +0200

    Update TestGeometricalFunction.m

--- TestGeometricalFunction.m
+++ TestGeometricalFunction.m
@@ -1,7 +1,7 @@
 clc,clear,close all
 
 %% Load base mesh
-file = 'Tetradecahedron004';
+file = 'Tetradecahedron003';
 
 TMC = TetradecahedronMeshComputer(file);
 mesh = TMC.getMesh();
@@ -35,8 +35,8 @@ density = f.compute(ls,2);
 %% set material
 s.interpolation  = 'SIMPALL';
 s.dim            = mesh.ndim;
-s.matA.bulk  = IsotropicElasticMaterial.computeKappaFromYoungAndPoisson(1e-12,0.3,mesh.ndim);
-s.matA.shear = IsotropicElasticMaterial.computeMuFromYoungAndPoisson(1e-12,0.3);
+s.matA.bulk  = IsotropicElasticMaterial.computeKappaFromYoungAndPoisson(1e-5,0.3,mesh.ndim);
+s.matA.shear = IsotropicElasticMaterial.computeMuFromYoungAndPoisson(1e-5,0.3);
 s.matB.bulk  = IsotropicElasticMaterial.computeKappaFromYoungAndPoisson(1,0.3,mesh.ndim);
 s.matB.shear = IsotropicElasticMaterial.computeMuFromYoungAndPoisson(1,0.3);
 mI = MaterialInterpolator.create(s);
@@ -59,23 +59,23 @@ sDir{1}.domain    = @(coor) isVertex1(coor);
 sDir{1}.direction = [1,2,3];
 sDir{1}.value     = 0;
 
-isV2X = @(coor) (abs(coor(:,1) - 0.75) < 1e-5);
-isV2Y = @(coor) (abs(coor(:,2) - 0.5) < 1e-5);
-isV2Z = @(coor) (abs(coor(:,3) - 0) < 1e-5);
-isVertex2 = @(coor) isV2X(coor) & isV2Y(coor) & isV2Z(coor);
-
-sDir{2}.domain    = @(coor) isVertex2(coor);
-sDir{2}.direction = [2,3];
-sDir{2}.value     = 0;
-
-isV3X = @(coor) (abs(coor(:,1) - 0.5) < 1e-5);
-isV3Y = @(coor) (abs(coor(:,2) - 0.25) < 1e-5);
-isV3Z = @(coor) (abs(coor(:,3) - 0) < 1e-5);
-isVertex3 = @(coor) isV3X(coor) & isV3Y(coor) & isV3Z(coor);
-
-sDir{3}.domain    = @(coor) isVertex3(coor);
-sDir{3}.direction = [3];
-sDir{3}.value     = 0;
+% isV2X = @(coor) (abs(coor(:,1) - 0.75) < 1e-5);
+% isV2Y = @(coor) (abs(coor(:,2) - 0.5) < 1e-5);
+% isV2Z = @(coor) (abs(coor(:,3) - 0) < 1e-5);
+% isVertex2 = @(coor) isV2X(coor) & isV2Y(coor) & isV2Z(coor);
+% 
+% sDir{2}.domain    = @(coor) isVertex2(coor);
+% sDir{2}.direction = [2];
+% sDir{2}.value     = 0;
+% 
+% isV3X = @(coor) (abs(coor(:,1) - 0.5) < 1e-5);
+% isV3Y = @(coor) (abs(coor(:,2) - 0.25) < 1e-5);
+% isV3Z = @(coor) (abs(coor(:,3) - 0) < 1e-5);
+% isVertex3 = @(coor) isV3X(coor) & isV3Y(coor) & isV3Z(coor);
+% 
+% sDir{3}.domain    = @(coor) isVertex3(coor);
+% sDir{3}.direction = [3];
+% sDir{3}.value     = 0;
 
 dirichletFun = [];
 for i = 1:numel(sDir)
@@ -95,7 +95,33 @@ s.material = material;
 s.scale    = 'MICRO';
 s.dim      = '3D';
 s.boundaryConditions = bc;
-s.solverCase = DirectSolver();
+
+%% Set solver
+BCAp = BCApplier(s);
+
+rigModes = RigidBodyFunction.create(mesh,[0.5,0.5,0.5]);
+RFun = rigModes.projectBasisFunctions('P1');
+for i = 1:length(RFun)
+    Rfull(:,i) = reshape(RFun{i}.fValues',[],1);
+end
+
+for i = 1:size(Rfull,2)
+    R(:,i) = BCAp.fullToReducedVectorDirichlet(Rfull(:,i));
+end
+s.type = 'ELASTIC';
+s.nullSpace = R;
+s.nLevels = 5;
+s.tol = 1e-8;
+s.maxIter = 1;
+p     = pyAMG.create(s);
+
+sS.preconditioner = p;
+sS.tol = 1e-5;
+solver = PCG(sS);
+
+%% Continue problem definition
+
+s.solverCase = solver;%DirectSolver();
 s.solverType = 'REDUCED';
 s.solverMode = 'FLUC';
 fem = ElasticProblemMicro(s);
