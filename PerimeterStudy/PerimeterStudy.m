3727e572da0addc3dc3ec8cfa3401fbc84188c4f
jose-antonio-torres <87600853+jose-antonio-torres@users.noreply.github.com>
2026-07-09 15:31:36 +0200

    perimeter study init

---
+++ PerimeterStudy/PerimeterStudy.m
@@ -0,0 +1,91 @@
+
+%
+clear;
+clc;
+close all;
+%
+
+%% Preprocess
+
+fid = fopen('00001_intensity.txt', 'r');
+i = 1;
+while ~feof(fid)
+    if i<=7
+        line = fgetl(fid);
+        disp(line)
+        i = i+1;
+    else
+        line = fscanf(fid, '%lf');
+        intensity = reshape(line,2,[])';
+        break;
+    end
+end
+fclose(fid);
+
+fid = fopen('mesh_common.txt', 'r');
+i = 1;
+while ~feof(fid)
+    if i<=13
+        line = fgetl(fid);
+        disp(line)
+        i = i+1;
+    elseif i>13 && i<=51542
+        line = fscanf(fid, '%lf');
+        coord = reshape(line,5,[])';
+        break;
+    end
+end
+fclose(fid);
+
+fid = fopen('mesh_common.txt', 'r');
+i = 1;
+while ~feof(fid)
+    if i<=103077
+        line = fgetl(fid);
+        disp(line)
+        i = i+1;
+    else
+        line = fscanf(fid, '%lf');
+        connec = reshape(line,5,[])';
+        break;
+    end
+end
+fclose(fid);
+
+coord = coord(:,2:3);
+connec = connec(:,2:end);
+connec = connec + 1;
+intensity = intensity(:,2);
+
+% coord(:,2) = -coord(:,2);
+% coord(:,2) = coord(:,2) - min(coord(:,2)) + 0.5;
+
+%% Mesh
+
+s.coord = coord;
+s.connec = connec;
+mesh = Mesh.create(s);
+
+intFun = LagrangianFunction.create(mesh,1,'P1');
+intFun.setFValues(intensity);
+
+intFun.plot();
+
+%% Filter
+
+h = mesh.computeMeanCellSize();
+e = 3*h;
+
+sF.mesh = mesh;
+sF.filterType = 'PDE';
+sF.trial = intFun;
+filter = Filter.create(sF);
+filter.updateEpsilon(e);
+
+intEpsFun = filter.compute(intFun,3);
+
+intEpsFun.plot();
+
+%% Perimeter computation
+
+P = (2/e)*Integrator.compute(intFun.*(1-intEpsFun),mesh,2);
