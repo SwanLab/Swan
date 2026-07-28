45c5a5677420ab99a7d07ed58a90e1ef7541ccfc
gmendonca08 <gmendonca@cimne.upc.edu>
2026-07-28 15:28:32 -0300

    NewResults

--- Addicraft/TopOpt_Density_C5Sup.m
+++ Addicraft/TopOpt_Density_C5Sup.m
@@ -164,7 +164,7 @@ classdef TopOpt_Density_C5Sup < handle
             s.rbe3Value(2,2) = 0;
             s.rbe3Value(2,3) = 0;
             s.rbe3Value(3,1) = NaN;
-            s.rbe3Value(3,2) = -8.5;
+            s.rbe3Value(3,2) = NaN;
             s.rbe3Value(3,3) = 50;
             s.interpolationType = 'LINEAR';
             s.solverType = 'REDUCED';
@@ -233,7 +233,7 @@ classdef TopOpt_Density_C5Sup < handle
 
         function createCost(obj)
             s.shapeFunctions{1} = obj.compliance;
-            s.weights           = -1;
+            s.weights           = 1;
             s.Msmooth           = obj.createMassMatrix();
             obj.cost            = Cost(s);
         end
@@ -296,15 +296,15 @@ classdef TopOpt_Density_C5Sup < handle
         function bc = createBoundaryConditions(obj)
             femReader = FemInputReaderGiD();
             s         = femReader.read(obj.filename);
-            % sPL       = obj.computeCondition(s.pointload);
+            sPL       = obj.computeCondition(s.pointload);
 
             dirichletFun = [];
 
             pointloadFun = [];
-            % for i = 1:numel(sPL)
-            %     pl = TractionLoad(obj.mesh, sPL{i}, 'DIRAC');
-            %     pointloadFun = [pointloadFun, pl];
-            % end
+            for i = 1:numel(sPL)
+                pl = TractionLoad(obj.mesh, sPL{i}, 'DIRAC');
+                pointloadFun = [pointloadFun, pl];
+            end
             s.pointloadFun = pointloadFun;
 
             s.periodicFun  = [];
