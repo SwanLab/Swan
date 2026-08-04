024344f7278b96c1a53e7e012ad1e81a1e05d14f
Gerard Villalta <37781783+gervilquin@users.noreply.github.com>
2026-08-04 17:31:19 +0200

    Update Tutorial07_1_PhaseFieldProblemAnalytical.m

--- Tutorials/Tutorial07_1_PhaseFieldProblemAnalytical.m
+++ Tutorials/Tutorial07_1_PhaseFieldProblemAnalytical.m
@@ -2,6 +2,7 @@ classdef Tutorial07_1_PhaseFieldProblemAnalytical < handle
 
     properties (Access = public)
         degType
+        energySplit
         initialGuess
         output
     end
@@ -33,6 +34,7 @@ classdef Tutorial07_1_PhaseFieldProblemAnalytical < handle
         function init(obj)
            close all;
            obj.degType = 'AT'; % 'ATSplit', ...
+           obj.energySplit = false; %In the future may be different types, not just Vol-Dev
         end
 
         function defineCase(obj)
@@ -65,14 +67,12 @@ classdef Tutorial07_1_PhaseFieldProblemAnalytical < handle
         end
 
         function createPhaseFieldFunctional(obj)
-            switch obj.degType
-                case {'AT'}
-                    s.energySplit = false;
-                    s.C = obj.mat.C; s.dC = obj.mat.dC; s.d2C = obj.mat.d2C;
-                case {'ATSplit'}
-                    s.energySplit = true;
-                    s.mu = obj.mat.mu; s.dmu = obj.mat.dmu; s.d2mu = obj.mat.d2mu;
-                    s.k = obj.mat.k; s.dk = obj.mat.dk; s.d2k = obj.mat.d2k;
+            s.energySplit   = obj.energySplit;
+            if obj.energySplit
+                s.mu = obj.mat.mu; s.dmu = obj.mat.dmu; s.d2mu = obj.mat.d2mu;
+                s.k = obj.mat.k; s.dk = obj.mat.dk; s.d2k = obj.mat.d2k;
+            else
+                s.C = obj.mat.C; s.dC = obj.mat.dC; s.d2C = obj.mat.d2C;
             end
             s.mesh          = obj.mesh;
             s.dissipation   = obj.dissipation;
@@ -89,23 +89,32 @@ classdef Tutorial07_1_PhaseFieldProblemAnalytical < handle
             nu0 = ConstantFunction.create(0.3,obj.mesh);
             mu0 = E0./(2*(1+nu0));
             k0  = E0./(N*(1-(N-1)*nu0));
-            [mu,dmu,d2mu] = PhaseFieldInterpolators.compute(obj.degType,mu0);
-            [k,dk,d2k]    = PhaseFieldInterpolators.compute(obj.degType,k0);
-            l   = @(phi) k(phi) - (2/N)*mu(phi);
-            dl  = @(phi) dk(phi) - (2/N)*dmu(phi);
-            d2l = @(phi) d2k(phi) - (2/N)*d2mu(phi);
+            [mu,dmu,d2mu] = PhaseFieldDegradationInterpolator.compute(obj.degType,mu0);
+            [k,dk,d2k]    = PhaseFieldDegradationInterpolator.compute(obj.degType,k0);
             obj.mat.mu = mu; obj.mat.dmu = dmu; obj.mat.d2mu = d2mu;
             obj.mat.k  = k; obj.mat.dk = dk; obj.mat.d2k = d2k;
+            if obj.energySplit
+                trc = @(u) squeezeParticular(trace(SymGrad(u)),1);
+                trcSign = @(u) Heaviside(trc(u));
 
-            mu = @(phi) Expand(mu(phi),4); l = @(phi) Expand(l(phi),4);
-            dmu = @(phi) Expand(dmu(phi),4); dl = @(phi) Expand(dl(phi),4);
-            d2mu = @(phi) Expand(d2mu(phi),4); d2l = @(phi) Expand(d2l(phi),4);
+                obj.mat.k = @(phi,u) k(phi).*trcSign(u) + k0.*(1-trcSign(u));
+                obj.mat.dk = @(phi,u) dk(phi).*trcSign(u);
+                obj.mat.d2k = @(phi,u) d2k(phi).*trcSign(u);
+            else
+                l   = @(phi) LameParametersConverter.computeLambdaFromBulkAndShear(k(phi),mu(phi),N);
+                dl  = @(phi) LameParametersConverter.computeLambdaFromBulkAndShear(dk(phi),dmu(phi),N);
+                d2l = @(phi) LameParametersConverter.computeLambdaFromBulkAndShear(d2k(phi),d2mu(phi),N);
 
-            I           = ConstantFunction.create(eye4D(N),obj.mesh);
-            IxI         = ConstantFunction.create(kronEye(N),obj.mesh);
-            obj.mat.C   = @(phi) 2*mu(phi).*I + l(phi).*IxI;
-            obj.mat.dC  = @(phi) 2*dmu(phi).*I + dl(phi).*IxI;
-            obj.mat.d2C = @(phi) 2*d2mu(phi).*I + d2l(phi).*IxI;
+                muE   = @(phi) Expand(mu(phi),4);   lE   = @(phi) Expand(l(phi),4);
+                dmuE  = @(phi) Expand(dmu(phi),4);  dlE  = @(phi) Expand(dl(phi),4);
+                d2muE = @(phi) Expand(d2mu(phi),4); d2lE = @(phi) Expand(d2l(phi),4);
+
+                I   = ConstantFunction.create(eye4D(N),obj.mesh);
+                IxI = ConstantFunction.create(kronEye(N),obj.mesh);
+                obj.mat.C   = @(phi) 2*muE(phi).*I + lE(phi).*IxI;
+                obj.mat.dC  = @(phi) 2*dmuE(phi).*I + dlE(phi).*IxI;
+                obj.mat.d2C = @(phi) 2*d2muE(phi).*I + d2lE(phi).*IxI;
+            end
         end
 
         function createDissipationInterpolation(obj)
