8b55b5863c4a22b308116d07585ce259f15b25b6
Gerard Villalta <37781783+gervilquin@users.noreply.github.com>
2026-08-05 10:50:12 +0200

    Update EnergyPrincipalDirections.m

--- PhaseFieldExperiments/MainFiles/Rotation/EnergyPrincipalDirections.m
+++ PhaseFieldExperiments/MainFiles/Rotation/EnergyPrincipalDirections.m
@@ -6,6 +6,8 @@ E_eps = zeros(length(xi),length(beta));
 BetaOpt = zeros(1,length(xi));
 phi = [0 0.01 0.05 0.1];
 
+cmap = hsv(length(xi));
+
 c2 = @(x) cos(2*x);
 s2 = @(x) sin(2*x);
 R = @(x) [ (1 + c2(x))/2,  (1 - c2(x))/2,  -s2(x)/sqrt(2);
@@ -44,7 +46,7 @@ for i=1:length(phi)
         [~,pos] = min(E_eps(j,:));
         BetaOpt(j) = beta(pos);
     end
-    figure(2*i-1)
+    figure(4*i-3)
     plot(rad2deg(xi),rad2deg(BetaOpt))
     hold on
     title(['Optimal angle (beta) [phi = ',num2str(phi(i)),']'])
@@ -53,23 +55,58 @@ for i=1:length(phi)
     xlabel('Strain ratio angle (xi) [deg]')
     xlim([0 360])
 
-    figure(2*i)
+    figure(4*i-2)
     surf(rad2deg(beta),rad2deg(xi),E_eps)
     title(['Strain energy (Horizontal) [phi = ',num2str(phi(i)),']'])
     ylabel('Strain ratio angle (xi) [deg]')
     ylim([0 360])
     xlabel('Rotation angle (beta) [deg]')
     xlim([0 180])
+
+    figure(4*i-1)
+    grid on
+    hold on
+    for j=1:length(xi)
+        plot3(repmat(rad2deg(xi(j)),1,length(beta)),rad2deg(beta),E_eps(j,:),'Color',cmap(j,:))
+    end
+    % lgd = legend('0','22.5','45','67.5','90','112.5','135','157.5','180','202.5','225','247.5','270','292.5','315','337.5','360');
+    % lgd.AutoUpdate = 'off'; 
+    for j=1:length(xi)
+        idx = find(abs(E_eps(j,:)-min(E_eps(j,:)))<1e-10);
+        scatter3(repmat(rad2deg(xi(j)),1,length(idx)),rad2deg(beta(idx)),E_eps(j,idx),'o','MarkerEdgeColor',cmap(j,:))
+    end
+    title(['Strain energy (Horizontal) [phi = ',num2str(phi(i)),']'])
+    xlabel('Strain ratio angle (xi) [deg]')
+    xlim([0 360])
+    ylabel('Rotation angle (beta) [deg]')
+    ylim([0 180])
+    view(348,6)
+
+    figure(4*i)
+    grid on
+    hold on
+    for j=1:length(xi)
+        idx = find(abs(E_eps(j,:)-min(E_eps(j,:)))<1e-10);
+        scatter(repmat(rad2deg(xi(j)),1,length(idx)),rad2deg(beta(idx)),'MarkerEdgeColor','k')
+    end   
+    title(['Optimal angle (beta) [phi = ',num2str(phi(i)),']'])
+    ylabel('Rotation angle (beta) [deg]')
+    ylim([0 180])
+    xlabel('Strain ratio angle (xi) [deg]')
+    xlim([0 360])
 end
+
 
 
 %% State (stress)
 clc,clear,close all
-xi = linspace(0,2*pi,300);
+xi = linspace(0,2*pi,100);
 beta = linspace(0,pi,300);
 E_sig = zeros(length(xi),length(beta));
 BetaOpt = zeros(1,length(xi));
 phi = [0 0.01 0.05 0.1];
+
+cmap = hsv(length(xi));
 
 c2 = @(x) cos(2*x);
 s2 = @(x) sin(2*x);
@@ -116,7 +153,7 @@ for i=1:length(phi)
         [~,pos] = max(E_sig(j,:));
         BetaOpt(j) = beta(pos);
     end
-    figure(2*i-1)
+    figure(4*i-3)
     plot(rad2deg(xi),rad2deg(BetaOpt))
     hold on
     title(['Optimal angle (beta) [phi = ',num2str(phi(i)),']'])
@@ -125,11 +162,43 @@ for i=1:length(phi)
     xlabel('Stress ratio angle (xi) [deg]')
     xlim([0 360])
 
-    figure(2*i)
+    figure(4*i-2)
     surf(rad2deg(beta),rad2deg(xi),E_sig)
-    title(['Strain energy derivative (Horizontal) [phi = ',num2str(phi(i)),']'])
+    title(['Stress energy derivative (Horizontal) [phi = ',num2str(phi(i)),']'])
     ylabel('Stress ratio angle (xi) [deg]')
     ylim([0 360])
     xlabel('Rotation angle (beta) [deg]')
     xlim([0 180])
+
+    figure(4*i-1)
+    grid on
+    hold on
+    for j=1:length(xi)
+        plot3(repmat(rad2deg(xi(j)),1,length(beta)),rad2deg(beta),E_sig(j,:),'Color',cmap(j,:))
+    end
+    % lgd = legend('0','22.5','45','67.5','90','112.5','135','157.5','180','202.5','225','247.5','270','292.5','315','337.5','360');
+    % lgd.AutoUpdate = 'off'; 
+    for j=1:length(xi)
+        idx = find(abs(E_sig(j,:)-max(E_sig(j,:)))<1e-10);
+        scatter3(repmat(rad2deg(xi(j)),1,length(idx)),rad2deg(beta(idx)),E_sig(j,idx),'o','MarkerEdgeColor',cmap(j,:))
+    end
+    title(['Stress energy (Horizontal) [phi = ',num2str(phi(i)),']'])
+    xlabel('Stress ratio angle (xi) [deg]')
+    xlim([0 360])
+    ylabel('Rotation angle (beta) [deg]')
+    ylim([0 180])
+    view(348,6)
+
+    figure(4*i)
+    grid on
+    hold on
+    for j=1:length(xi)
+        idx = find(abs(E_sig(j,:)-max(E_sig(j,:)))<1e-10);
+        scatter(repmat(rad2deg(xi(j)),1,length(idx)),rad2deg(beta(idx)),'MarkerEdgeColor','k')
+    end   
+    title(['Optimal angle (beta) [phi = ',num2str(phi(i)),']'])
+    ylabel('Rotation angle (beta) [deg]')
+    ylim([0 180])
+    xlabel('Stress ratio angle (xi) [deg]')
+    xlim([0 360])
 end
